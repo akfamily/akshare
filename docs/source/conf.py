a957e0ad094895a13a16b1c950c6c0879355f774
jindaxiang <jindaxiang@163.com>
2019-10-11 00:57:51 +0800

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ release = '0.0.1'
 # ones.
 extensions = ["recommonmark"]
 
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -57,5 +58,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+master_doc = 'index'
 
 
