9223562e609a8c2228c4a0588cc53d45f7d7e01a
jindaxiang <jindaxiang@163.com>
2019-10-12 17:46:37 +0800

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,11 +28,6 @@ author = 'Albert King'
 release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
-source_parsers = {
-    '.md': 'recommonmark.parser.CommonMarkParser',
-}
-
-source_suffix = ['.rst', '.md']
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
