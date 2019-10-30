8d489253760e0ec4e1b158aecf08e9e7cc1cf9ef
jindaxiang <jindaxiang@163.com>
2019-10-30 21:11:04 +0800

    update us

--- akshare/stock/cons.py
+++ akshare/stock/cons.py
@@ -4,9 +4,92 @@
 Author: Albert King
 date: 2019/10/25 15:56
 contact: jindaxiang@163.com
-desc:
+desc: 股票配置文件
 """
-# hk
+# us-sina
+us_sina_stock_hist_qfq_url = "https://finance.sina.com.cn/us_stock/company/reinstatement/{}_qfq.js"
+us_sina_stock_hist_url = "https://finance.sina.com.cn/us_stock/company/hisdata/klc_kl_{}.js"
+us_sina_stock_list_url = "http://stock.finance.sina.com.cn/usstock/api/jsonp.php/IO.XSRV2.CallbackList[{}]/US_CategoryService.getList"
+us_sina_stock_dict_payload = {
+    "page": "2",
+    "num": "20",
+    "sort": "",
+    "asc": "0",
+    "market": "",
+    "id": ""
+}
+js_hash_text = """
+    function d(s){
+		var a, i, j, c, c0, c1, c2, r;
+		var _s = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_$';
+		var _r64 = function(s, b)
+		{
+			return ((s | (s << 6)) >>> (b % 6)) & 63;
+		};
+		a = [];
+		c = [];
+		for (i = 0; i < s.length; i++)
+		{
+			c0 = s.charCodeAt(i);
+			if (c0 & ~255)
+			{
+				c0 = (c0 >> 8) ^ c0;
+			}
+			c.push(c0);
+			if (c.length == 3 || i == s.length - 1)
+			{
+				while(c.length < 3)
+				{
+					c.push(0);
+				}
+				a.push((c[0] >> 2) & 63);
+				a.push(((c[1] >> 4) | (c[0] << 6)) & 63);
+				a.push(((c[1] << 4) | (c[2] >> 2)) & 63);
+				a.push(c[2] & 63);
+				c = [];
+			}
+		}
+		while (a.length < 16)
+		{
+			a.push(0);
+		}
+		r = 0;
+		for (i = 0; i < a.length; i++)
+		{
+			r ^= (_r64(a[i] ^ (r | i), i) ^ _r64(i, r)) & 63;
+		}
+		for (i = 0; i < a.length; i++)
+		{
+			a[i] = (_r64((r | i & a[i]), r) ^ a[i]) & 63;
+			r += a[i];
+		}
+		for (i = 16; i < a.length; i++)
+		{
+			a[i % 16] ^= (a[i] + (i >>> 4)) & 63;
+		}
+		for (i = 0; i < 16; i++)
+		{
+			a[i] = _s.substr(a[i], 1);
+		}
+		a = a.slice(0, 16).join('');
+		return a;
+	}
+"""
+# hk-sina
+hk_sina_stock_hist_hfq_url = "https://finance.sina.com.cn/stock/hkstock/{}/hfq.js"
+hk_sina_stock_hist_qfq_url = "https://finance.sina.com.cn/stock/hkstock/{}/qfq.js"
+hk_sina_stock_hist_url = "https://finance.sina.com.cn/stock/hkstock/{}/klc_kl.js"
+hk_sina_stock_list_url = "http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHKStockData"
+hk_sina_stock_dict_payload = {
+    "page": "1",
+    "num": "3000",
+    "sort": "symbol",
+    "asc": "1",
+    "node": "qbgg_hk",
+    "_s_r_a": "page"
+}
+
+# hk-tx
 hk_url = "http://stock.gtimg.cn/data/hk_rank.php"
 hk_headers = {
     "Referer": "http://stockapp.finance.qq.com/mstats/",
@@ -20,7 +103,6 @@ hk_payload = {
     "order": "decs",
     "var_name": "list_data"
 }
-
 
 # usa
 url_usa_daily = "http://webusstock.hermes.hexun.com/usa/kline"
@@ -80,3 +162,293 @@ params = {
     "page": "8",
     "callback": "hxbase_json11571989979887",
 }
+
+hk_js_decode = """
+function d(t) {
+    var e, i, n, r, a, o, s, l = (arguments,
+            864e5), u = 7657, c = [], h = [], d = ~(3 << 30), f = 1 << 30,
+        p = [0, 3, 5, 6, 9, 10, 12, 15, 17, 18, 20, 23, 24, 27, 29, 30], m = Math, g = function () {
+            var l, u;
+            for (l = 0; 64 > l; l++)
+                h[l] = m.pow(2, l),
+                26 > l && (c[l] = v(l + 65),
+                    c[l + 26] = v(l + 97),
+                10 > l && (c[l + 52] = v(l + 48)));
+            for (c.push("+", "/"),
+                     c = c.join(""),
+                     i = t.split(""),
+                     n = i.length,
+                     l = 0; n > l; l++)
+                i[l] = c.indexOf(i[l]);
+            return r = {},
+                e = o = 0,
+                a = {},
+                u = w([12, 6]),
+                s = 63 ^ u[1],
+            {
+                _1479: T,
+                _136: _,
+                _200: S,
+                _139: k,
+                _197: _mi_run
+            }["_" + u[0]] || function () {
+                return []
+            }
+        }, v = String.fromCharCode, b = function (t) {
+            return t === {}._
+        }, N = function () {
+            var t, e;
+            for (t = y(),
+                     e = 1; ;) {
+                if (!y())
+                    return e * (2 * t - 1);
+                e++
+            }
+        }, y = function () {
+            var t;
+            return e >= n ? 0 : (t = i[e] & 1 << o,
+                o++,
+            o >= 6 && (o -= 6,
+                e++),
+                !!t)
+        }, w = function (t, r, a) {
+            var s, l, u, c, d;
+            for (l = [],
+                     u = 0,
+                 r || (r = []),
+                 a || (a = []),
+                     s = 0; s < t.length; s++)
+                if (c = t[s],
+                    u = 0,
+                    c) {
+                    if (e >= n)
+                        return l;
+                    if (t[s] <= 0)
+                        u = 0;
+                    else if (t[s] <= 30) {
+                        for (; d = 6 - o,
+                                   d = c > d ? d : c,
+                                   u |= (i[e] >> o & (1 << d) - 1) << t[s] - c,
+                                   o += d,
+                               o >= 6 && (o -= 6,
+                                   e++),
+                                   c -= d,
+                                   !(0 >= c);)
+                            ;
+                        r[s] && u >= h[t[s] - 1] && (u -= h[t[s]])
+                    } else
+                        u = w([30, t[s] - 30], [0, r[s]]),
+                        a[s] || (u = u[0] + u[1] * h[30]);
+                    l[s] = u
+                } else
+                    l[s] = 0;
+            return l
+        }, x = function (t) {
+            var e, i, n;
+            for (t > 1 && (e = 0),
+                     e = 0; t > e; e++)
+                r.d++,
+                    n = r.d % 7,
+                (3 == n || 4 == n) && (r.d += 5 - n);
+            return i = new Date,
+                i.setTime((u + r.d) * l),
+                i
+        }, S = function () {
+            var t, i, a, o, l;
+            if (s >= 1)
+                return [];
+            for (r.d = w([18], [1])[0] - 1,
+                     a = w([3, 3, 30, 6]),
+                     r.p = a[0],
+                     r.ld = a[1],
+                     r.cd = a[2],
+                     r.c = a[3],
+                     r.m = m.pow(10, r.p),
+                     r.pc = r.cd / r.m,
+                     i = [],
+                     t = 0; o = {
+                d: 1
+            },
+                 y() && (a = w([3])[0],
+                     0 == a ? o.d = w([6])[0] : 1 == a ? (r.d = w([18])[0],
+                         o.d = 0) : o.d = a),
+                     l = {
+                         day: x(o.d)
+                     },
+                 y() && (r.ld += N()),
+                     a = w([3 * r.ld], [1]),
+                     r.cd += a[0],
+                     l.close = r.cd / r.m,
+                     i.push(l),
+                 !(e >= n) && (e != n - 1 || 63 & (r.c ^ t + 1)); t++)
+                ;
+            return i[0].prevclose = r.pc,
+                i
+        }, _ = function () {
+            var t, i, a, o, l, u, c, h, d, f, p;
+            if (s > 2)
+                return [];
+            for (c = [],
+                     d = {
+                         v: "volume",
+                         p: "price",
+                         a: "avg_price"
+                     },
+                     r.d = w([18], [1])[0] - 1,
+                     h = {
+                         day: x(1)
+                     },
+                     a = w(1 > s ? [3, 3, 4, 1, 1, 1, 5] : [4, 4, 4, 1, 1, 1, 3]),
+                     t = 0; 7 > t; t++)
+                r[["la", "lp", "lv", "tv", "rv", "zv", "pp"][t]] = a[t];
+            for (r.m = m.pow(10, r.pp),
+                     s >= 1 ? (a = w([3, 3]),
+                         r.c = a[0],
+                         a = a[1]) : (a = 5,
+                         r.c = 2),
+                     r.pc = w([6 * a])[0],
+                     h.pc = r.pc / r.m,
+                     r.cp = r.pc,
+                     r.da = 0,
+                     r.sa = r.sv = 0,
+                     t = 0; !(e >= n) && (e != n - 1 || 7 & (r.c ^ t)); t++) {
+                for (l = {},
+                         o = {},
+                         f = r.tv ? y() : 1,
+                         i = 0; 3 > i; i++)
+                    if (p = ["v", "p", "a"][i],
+                    (f ? y() : 0) && (a = N(),
+                        r["l" + p] += a),
+                        u = "v" == p && r.rv ? y() : 1,
+                        a = w([3 * r["l" + p] + ("v" == p ? 7 * u : 0)], [!!i])[0] * (u ? 1 : 100),
+                        o[p] = a,
+                    "v" == p) {
+                        if (!(l[d[p]] = a) && (s > 1 || 241 > t) && (r.zv ? !y() : 1)) {
+                            o.p = 0;
+                            break
+                        }
+                    } else
+                        "a" == p && (r.da = (1 > s ? 0 : r.da) + o.a);
+                r.sv += o.v,
+                    l[d.p] = (r.cp += o.p) / r.m,
+                    r.sa += o.v * r.cp,
+                    l[d.a] = b(o.a) ? t ? c[t - 1][d.a] : l[d.p] : r.sv ? ((m.floor((r.sa * (2e3 / r.m) + r.sv) / r.sv) >> 1) + r.da) / 1e3 : l[d.p] + r.da / 1e3,
+                    c.push(l)
+            }
+            return c[0].date = h.day,
+                c[0].prevclose = h.pc,
+                c
+        }, T = function () {
+            var t, e, i, n, a, o, l;
+            if (s >= 1)
+                return [];
+            for (r.lv = 0,
+                     r.ld = 0,
+                     r.cd = 0,
+                     r.cv = [0, 0],
+                     r.p = w([6])[0],
+                     r.d = w([18], [1])[0] - 1,
+                     r.m = m.pow(10, r.p),
+                     a = w([3, 3]),
+                     r.md = a[0],
+                     r.mv = a[1],
+                     t = []; a = w([6]),
+                     a.length;) {
+                if (i = {
+                    c: a[0]
+                },
+                    n = {},
+                    i.d = 1,
+                32 & i.c)
+                    for (; ;) {
+                        if (a = w([6])[0],
+                        63 == (16 | a)) {
+                            l = 16 & a ? "x" : "u",
+                                a = w([3, 3]),
+                                i[l + "_d"] = a[0] + r.md,
+                                i[l + "_v"] = a[1] + r.mv;
+                            break
+                        }
+                        if (32 & a) {
+                            o = 8 & a ? "d" : "v",
+                                l = 16 & a ? "x" : "u",
+                                i[l + "_" + o] = (7 & a) + r["m" + o];
+                            break
+                        }
+                        if (o = 15 & a,
+                            0 == o ? i.d = w([6])[0] : 1 == o ? (r.d = o = w([18])[0],
+                                i.d = 0) : i.d = o,
+                            !(16 & a))
+                            break
+                    }
+                n.date = x(i.d);
+                for (o in {
+                    v: 0,
+                    d: 0
+                })
+                    b(i["x_" + o]) || (r["l" + o] = i["x_" + o]),
+                    b(i["u_" + o]) && (i["u_" + o] = r["l" + o]);
+                for (i.l_l = [i.u_d, i.u_d, i.u_d, i.u_d, i.u_v],
+                         l = p[15 & i.c],
+                     1 & i.u_v && (l = 31 - l),
+                     16 & i.c && (i.l_l[4] += 2),
+                         e = 0; 5 > e; e++)
+                    l & 1 << 4 - e && i.l_l[e]++,
+                        i.l_l[e] *= 3;
+                i.d_v = w(i.l_l, [1, 0, 0, 1, 1], [0, 0, 0, 0, 1]),
+                    o = r.cd + i.d_v[0],
+                    n.open = o / r.m,
+                    n.high = (o + i.d_v[1]) / r.m,
+                    n.low = (o - i.d_v[2]) / r.m,
+                    n.close = (o + i.d_v[3]) / r.m,
+                    a = i.d_v[4],
+                "number" == typeof a && (a = [a, a >= 0 ? 0 : -1]),
+                    r.cd = o + i.d_v[3],
+                    l = r.cv[0] + a[0],
+                    r.cv = [l & d, r.cv[1] + a[1] + !!((r.cv[0] & d) + (a[0] & d) & f)],
+                    n.volume = (r.cv[0] & f - 1) + r.cv[1] * f,
+                    t.push(n)
+            }
+            return t
+        }, k = function () {
+            var t, e, i, n;
+            if (s > 1)
+                return [];
+            for (r.l = 0,
+                     n = -1,
+                     r.d = w([18])[0] - 1,
+                     i = w([18])[0]; r.d < i;)
+                e = x(1),
+                    0 >= n ? (y() && (r.l += N()),
+                        n = w([3 * r.l], [0])[0] + 1,
+                    t || (t = [e],
+                        n--)) : t.push(e),
+                    n--;
+            return t
+        };
+    return _mi_run = function () {
+        var t, i, a, o;
+        if (s >= 1)
+            return [];
+        for (r.f = w([6])[0],
+                 r.c = w([6])[0],
+                 a = [],
+                 r.dv = [],
+                 r.dl = [],
+                 t = 0; t < r.f; t++)
+            r.dv[t] = 0,
+                r.dl[t] = 0;
+        for (t = 0; !(e >= n) && (e != n - 1 || 7 & (r.c ^ t)); t++) {
+            for (o = [],
+                     i = 0; i < r.f; i++)
+                y() && (r.dl[i] += N()),
+                    r.dv[i] += w([3 * r.dl[i]], [1])[0],
+                    o[i] = r.dv[i];
+            a.push(o)
+        }
+        return a
+    }
+        ,
+        g()()
+}
+"""
---
+++ akshare/stock/us_stock_sina.py
@@ -0,0 +1,71 @@
+# -*- coding:utf-8 -*-
+# /usr/bin/env python
+"""
+Author: Albert King
+date: 2019/10/30 18:47
+contact: jindaxiang@163.com
+desc: 获取新浪财经-美股实时数据和历史数据
+"""
+import requests
+import json
+import pandas as pd
+import execjs
+
+from akshare.stock.cons import (js_hash_text,
+                                us_sina_stock_list_url,
+                                us_sina_stock_dict_payload,
+                                us_sina_stock_hist_url,
+                                hk_js_decode,
+                                us_sina_stock_hist_qfq_url)
+
+
+def get_us_current_stock_price():
+    big_df = pd.DataFrame()
+    for page in range(1, 489):
+        # page = "1"
+        print("正在抓取第{}页的美股数据".format(page))
+        us_js_decode = "US_CategoryService.getList?page={}&num=20&sort=&asc=0&market=&id=".format(page)
+        js_code = execjs.compile(js_hash_text)
+        dict_list = js_code.call('d', us_js_decode)  # 执行js解密代码
+        us_sina_stock_dict_payload.update({"page": "{}".format(page)})
+        res = requests.get(us_sina_stock_list_url.format(dict_list), params=us_sina_stock_dict_payload)
+        data_json = json.loads(res.text[res.text.find("({")+1: res.text.rfind(");")])
+        big_df = big_df.append(pd.DataFrame(data_json["data"]), ignore_index=True)
+    return big_df
+
+
+def get_us_stock_name():
+    big_df = pd.DataFrame()
+    for page in range(1, 489):
+        # page = "1"
+        print("正在抓取第{}页的美股数据".format(page))
+        us_js_decode = "US_CategoryService.getList?page={}&num=20&sort=&asc=0&market=&id=".format(page)
+        js_code = execjs.compile(js_hash_text)
+        dict_list = js_code.call('d', us_js_decode)  # 执行js解密代码
+        us_sina_stock_dict_payload.update({"page": "{}".format(page)})
+        res = requests.get(us_sina_stock_list_url.format(dict_list), params=us_sina_stock_dict_payload)
+        data_json = json.loads(res.text[res.text.find("({")+1: res.text.rfind(");")])
+        big_df = big_df.append(pd.DataFrame(data_json["data"]), ignore_index=True)
+    return big_df
+
+
+def get_hk_stock_hist_data(symbol="BRK.A"):
+    res = requests.get(us_sina_stock_hist_url.format(symbol))
+    js_code = execjs.compile(hk_js_decode)
+    dict_list = js_code.call('d', res.text.split("=")[1].split(";")[0].replace('"', ""))  # 执行js解密代码
+    data_df = pd.DataFrame(dict_list)
+    data_df["date"] = data_df["date"].str.split("T", expand=True).iloc[:, 0]
+    data_df.index = pd.to_datetime(data_df["date"])
+    del data_df["date"]
+    data_df.astype("float")
+    res = requests.get(us_sina_stock_hist_qfq_url.format(symbol))
+    qfq_factor_df = pd.DataFrame(eval(res.text.split("=")[1].split("\n")[0])['data'])
+    return data_df, qfq_factor_df
+
+
+if __name__ == "__main__":
+    df = get_us_stock_name()
+    print(df)
+    original_df, fq_df = get_hk_stock_hist_data(symbol="AMZN")
+    print(original_df)
+    print(fq_df)
