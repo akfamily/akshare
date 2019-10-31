8bd88302d7b61685ef869e954ec903ea0b5479c5
jindaxiang <jindaxiang@163.com>
2019-10-31 23:59:19 +0800

    0.1.98 修正版本更新错误

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -216,9 +216,11 @@ PCE物价指数年率报告
 修正新浪财经-期货-实时数据接口-返回 current_price 字段为实时数据
 0.1.97
 修正新浪财经-期货-实时数据接口-返回 current_price 和 ask_price 字段为实时数据
+0.1.98
+修正版本更新错误
 """
 
-__version__ = '0.1.97'
+__version__ = '0.1.98'
 __author__ = 'Albert King'
 
 """
