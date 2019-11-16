5189a0969b63eae66674d1569490f1bca37c759d
jindaxiang <jindaxiang@163.com>
2019-11-16 17:32:02 +0800

    0.2.31 修正期货-国内-金融期货实时行情接口订阅问题

--- akshare/futures/zh_futures_sina.py
+++ akshare/futures/zh_futures_sina.py
@@ -59,58 +59,122 @@ def match_main_contract(exchange="dce"):
     return ','.join(["nf_" + item for item in subscribe_cffex_list])
 
 
-def futures_zh_spot(subscribe_list="nf_V2001,nf_P2001"):
+def futures_zh_spot(subscribe_list="nf_IF1912,nf_TF1912,nf_IH1912,nf_IC1912", market="CF"):
     url = f"https://hq.sinajs.cn/rn={round(time.time() * 1000)}&list={subscribe_list}"
     res = requests.get(url)
     data_df = pd.DataFrame([item.strip().split("=")[1].split(
         ",") for item in res.text.split(";") if item.strip() != ""])
     data_df.iloc[:, 0] = data_df.iloc[:, 0].str.replace('"', "")
     data_df.iloc[:, -1] = data_df.iloc[:, -1].str.replace('"', "")
-    data_df.columns = [
-        "symbol",
-        "time",
-        "open",
-        "high",
-        "low",
-        "last_close",
-        "bid_price",
-        "ask_price",
-        "current_price",
-        "avg_price",
-        "last_settle_price",
-        "buy_vol",
-        "sell_vol",
-        "hold",
-        "volume",
-        "_",
-        "_",
-        "_",
-        "_",
-        "_",
-        "_",
-        "_",
-        "_",
-        "_",
-        "_",
-        "_",
-        "_",
-        "_"]
-    return data_df[["symbol",
-                    "time",
-                    "open",
-                    "high",
-                    "low",
-                    "current_price",
-                    "bid_price",
-                    "ask_price",
-                    "buy_vol",
-                    "sell_vol",
-                    "hold",
-                    "volume",
-                    "avg_price",
-                    "last_close",
-                    "last_settle_price",
-                    ]]
+    if market == "CF":
+        data_df.columns = [
+            "symbol",
+            "time",
+            "open",
+            "high",
+            "low",
+            "last_close",
+            "bid_price",
+            "ask_price",
+            "current_price",
+            "avg_price",
+            "last_settle_price",
+            "buy_vol",
+            "sell_vol",
+            "hold",
+            "volume",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_"]
+        return data_df[["symbol",
+                        "time",
+                        "open",
+                        "high",
+                        "low",
+                        "current_price",
+                        "bid_price",
+                        "ask_price",
+                        "buy_vol",
+                        "sell_vol",
+                        "hold",
+                        "volume",
+                        "avg_price",
+                        "last_close",
+                        "last_settle_price",
+                        ]]
+    else:
+        data_df.columns = [
+            "open",
+            "high",
+            "low",
+            "current_price",
+            "volume",
+            "amount",
+            "hold",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_"
+            "_",
+            "time",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "_",
+            "symbol"]
+        return data_df[["symbol",
+                        "time",
+                        "open",
+                        "high",
+                        "low",
+                        "current_price",
+                        "hold",
+                        "volume",
+                        "amount"
+                        ]]
 
 
 if __name__ == "__main__":
@@ -123,3 +187,11 @@ if __name__ == "__main__":
         data = futures_zh_spot(
             subscribe_list=",".join([dce_text, czce_text, shfe_text]))
         print(data)
+
+    # 金融期货单独订阅
+    # cffex_text = match_main_contract(exchange="cffex")
+    #
+    # while True:
+    #     time.sleep(3)
+    #     data = futures_zh_spot(subscribe_list=cffex_text, market="FF")
+    #     print(data)
