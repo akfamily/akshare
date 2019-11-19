c2575194bb398be09c1c9eeccd2466d5a2c16ff9
jindaxiang <jindaxiang@163.com>
2019-11-19 17:48:38 +0800

    hhhh

---
+++ docs/source/_themes/sphinx_rtd_theme/__init__.py
@@ -0,0 +1,30 @@
+"""
+Sphinx Read the Docs theme.
+
+From https://github.com/ryan-roemer/sphinx-bootstrap-theme.
+"""
+
+from os import path
+
+import sphinx
+
+
+__version__ = '0.4.3.dev0'
+__version_full__ = __version__
+
+
+def get_html_theme_path():
+    """Return list of HTML theme paths."""
+    cur_dir = path.abspath(path.dirname(path.dirname(__file__)))
+    return cur_dir
+
+
+# See http://www.sphinx-doc.org/en/stable/theming.html#distribute-your-theme-as-a-python-package
+def setup(app):
+    app.add_html_theme('sphinx_rtd_theme', path.abspath(path.dirname(__file__)))
+
+    if sphinx.version_info >= (1, 8, 0):
+        # Add Sphinx message catalog for newer versions of Sphinx
+        # See http://www.sphinx-doc.org/en/master/extdev/appapi.html#sphinx.application.Sphinx.add_message_catalog
+        rtd_locale_path = path.join(path.abspath(path.dirname(__file__)), 'locale')
+        app.add_message_catalog('sphinx', rtd_locale_path)
