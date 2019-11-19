282eaf6b1201067d2b31e10102cda1f77cbab845
jindaxiang <jindaxiang@163.com>
2019-11-20 00:26:01 +0800

    update hyperlink

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,9 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import recommonmark
 from recommonmark.transform import AutoStructify
+import sphinx_rtd_theme
 
 html_theme_path = ["_themes", ]
 
