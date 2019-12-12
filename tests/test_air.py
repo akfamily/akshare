0821944079e18083538101236ef345d41cc4cd4d
jindaxiang <jindaxiang@163.com>
2019-12-12 19:04:37 +0800

    update test file

--- tests/test_air.py
+++ tests/test_air.py
@@ -4,7 +4,7 @@
 Author: Albert King
 date: 2019/12/12 18:16
 contact: jindaxiang@163.com
-desc: 
+desc: 测试文件
 """
 from akshare.weather.aqi_study import air_hourly
 
@@ -12,3 +12,7 @@ from akshare.weather.aqi_study import air_hourly
 def test_air_hourly():
     df_hourly = air_hourly('成都', '2019-12-10')
     assert len(df_hourly) >= 10
+
+
+if __name__ == "__main__":
+    test_air_hourly()
