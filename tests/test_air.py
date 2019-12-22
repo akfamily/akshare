f14dda45164ba20ccaecd4dae5d84b76d838098c
jindaxiang <jindaxiang@163.com>
2019-12-22 19:24:53 +0800

    update travis

--- tests/test_air.py
+++ tests/test_air.py
@@ -4,7 +4,7 @@
 Author: Albert King
 date: 2019/12/12 18:16
 contact: jindaxiang@163.com
-desc: 测试文件
+desc: 用于自动测试的文件, 这里写相关的自动测试接口函数
 """
 from akshare.air.aqi_study import air_hourly
 from akshare.index.index_weibo import weibo_index
