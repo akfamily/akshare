c031938984e7b925bccd2769450b5dc48634379a
jindaxiang <jindaxiang@163.com>
2020-02-25 16:41:05 +0800

    update README.md

--- akshare/stock/us_stock_sina.py
+++ akshare/stock/us_stock_sina.py
@@ -9,26 +9,32 @@ desc: 获取新浪财经-美股实时行情数据和历史行情数据
 """
 import json
 
-import requests
-import pandas as pd
 import execjs
+import pandas as pd
+import requests
 
-from akshare.stock.cons import (js_hash_text,
-                                hk_js_decode,
-                                us_sina_stock_list_url,
-                                us_sina_stock_dict_payload,
-                                us_sina_stock_hist_url,
-                                us_sina_stock_hist_qfq_url)
+from akshare.stock.cons import (
+    js_hash_text,
+    hk_js_decode,
+    us_sina_stock_list_url,
+    us_sina_stock_dict_payload,
+    us_sina_stock_hist_url,
+    us_sina_stock_hist_qfq_url,
+)
 
 
 def get_us_page_count():
     page = "1"
-    us_js_decode = "US_CategoryService.getList?page={}&num=20&sort=&asc=0&market=&id=".format(page)
+    us_js_decode = "US_CategoryService.getList?page={}&num=20&sort=&asc=0&market=&id=".format(
+        page
+    )
     js_code = execjs.compile(js_hash_text)
-    dict_list = js_code.call('d', us_js_decode)  # 执行js解密代码
+    dict_list = js_code.call("d", us_js_decode)  # 执行js解密代码
     us_sina_stock_dict_payload.update({"page": "{}".format(page)})
-    res = requests.get(us_sina_stock_list_url.format(dict_list), params=us_sina_stock_dict_payload)
-    data_json = json.loads(res.text[res.text.find("({") + 1: res.text.rfind(");")])
+    res = requests.get(
+        us_sina_stock_list_url.format(dict_list), params=us_sina_stock_dict_payload
+    )
+    data_json = json.loads(res.text[res.text.find("({") + 1 : res.text.rfind(");")])
     if not isinstance(int(data_json["count"]) / 20, int):
         page_count = int(int(data_json["count"]) / 20) + 1
     else:
@@ -39,15 +45,19 @@ def get_us_page_count():
 def stock_us_spot():
     big_df = pd.DataFrame()
     page_count = get_us_page_count()
-    for page in range(1, page_count+1):
+    for page in range(1, page_count + 1):
         # page = "1"
         print("正在抓取第{}页的美股数据".format(page))
-        us_js_decode = "US_CategoryService.getList?page={}&num=20&sort=&asc=0&market=&id=".format(page)
+        us_js_decode = "US_CategoryService.getList?page={}&num=20&sort=&asc=0&market=&id=".format(
+            page
+        )
         js_code = execjs.compile(js_hash_text)
-        dict_list = js_code.call('d', us_js_decode)  # 执行js解密代码
+        dict_list = js_code.call("d", us_js_decode)  # 执行js解密代码
         us_sina_stock_dict_payload.update({"page": "{}".format(page)})
-        res = requests.get(us_sina_stock_list_url.format(dict_list), params=us_sina_stock_dict_payload)
-        data_json = json.loads(res.text[res.text.find("({") + 1: res.text.rfind(");")])
+        res = requests.get(
+            us_sina_stock_list_url.format(dict_list), params=us_sina_stock_dict_payload
+        )
+        data_json = json.loads(res.text[res.text.find("({") + 1 : res.text.rfind(");")])
         big_df = big_df.append(pd.DataFrame(data_json["data"]), ignore_index=True)
     return big_df
 
@@ -55,14 +65,16 @@ def stock_us_spot():
 def stock_us_daily(symbol="BRK.A", factor=""):
     res = requests.get(us_sina_stock_hist_url.format(symbol))
     js_code = execjs.compile(hk_js_decode)
-    dict_list = js_code.call('d', res.text.split("=")[1].split(";")[0].replace('"', ""))  # 执行js解密代码
+    dict_list = js_code.call(
+        "d", res.text.split("=")[1].split(";")[0].replace('"', "")
+    )  # 执行js解密代码
     data_df = pd.DataFrame(dict_list)
     data_df["date"] = data_df["date"].str.split("T", expand=True).iloc[:, 0]
     data_df.index = pd.to_datetime(data_df["date"])
     del data_df["date"]
     data_df.astype("float")
     res = requests.get(us_sina_stock_hist_qfq_url.format(symbol))
-    qfq_factor_df = pd.DataFrame(eval(res.text.split("=")[1].split("\n")[0])['data'])
+    qfq_factor_df = pd.DataFrame(eval(res.text.split("=")[1].split("\n")[0])["data"])
     qfq_factor_df.columns = ["date", "qfq_factor"]
     if factor == "qfq":
         return qfq_factor_df
@@ -71,8 +83,7 @@ def stock_us_daily(symbol="BRK.A", factor=""):
 
 
 if __name__ == "__main__":
-    df = stock_us_spot()
-    print(df)
-    df = stock_us_daily(symbol="AMZN", factor="qfq")
-    print(df)
-
+    stock_us_spot_df = stock_us_spot()
+    print(stock_us_spot_df)
+    stock_us_daily_df = stock_us_daily(symbol="AMZN", factor="qfq")
+    print(stock_us_daily_df)
