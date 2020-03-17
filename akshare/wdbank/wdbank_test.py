de1d55245ec08aa340481a527eb2bc3d1f91797e
jindaxiang <jindaxiang@163.com>
2020-03-17 21:48:38 +0800

    feat(wdbank.py): add test wdbank.py
    add test wdbank.py

---
+++ akshare/wdbank/wdbank_test.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+# /usr/bin/env python
+"""
+Author: Albert King
+date: 2020/3/17 20:57
+contact: jindaxiang@163.com
+desc: 
+"""
+from akshare import wdbank
+
+wdbank.get_source()
+wdbank.get_data('NY.GDP.PCAP.PP.CD', country = ['CHN', 'USA'])
+
