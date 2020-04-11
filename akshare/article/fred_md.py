406afe43fde43de0821fe964c74b320d120d14c1
jindaxiang <jindaxiang@163.com>
2020-04-12 02:06:30 +0800

    docs(docs): docs
    docs

--- akshare/article/fred_md.py
+++ akshare/article/fred_md.py
@@ -11,45 +11,13 @@ import requests
 
 def fred_md(date="2020-01"):
     url = f"https://s3.amazonaws.com/files.fred.stlouisfed.org/fred-md/monthly/{date}.csv"
-    headers = {
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
-        "Cache-Control": "no-cache",
-        "Connection": "keep-alive",
-        "Host": "s3.amazonaws.com",
-        "Pragma": "no-cache",
-        "Referer": "https://research.stlouisfed.org/econ/mccracken/fred-databases/",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "cross-site",
-        "Sec-Fetch-User": "?1",
-        "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
-    }
-    r = requests.get(url, headers=headers)
+    r = requests.get(url)
     temp_df = pd.read_csv(BytesIO(r.content))
     return temp_df
 
 
 def fred_qd(date="2020-03"):
     url = f"https://s3.amazonaws.com/files.fred.stlouisfed.org/fred-md/quarterly/{date}.csv"
-    headers = {
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
-        "Cache-Control": "no-cache",
-        "Connection": "keep-alive",
-        "Host": "s3.amazonaws.com",
-        "Pragma": "no-cache",
-        "Referer": "https://research.stlouisfed.org/econ/mccracken/fred-databases/",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "cross-site",
-        "Sec-Fetch-User": "?1",
-        "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
-    }
     temp_df = pd.read_csv(url)
     return temp_df
 
