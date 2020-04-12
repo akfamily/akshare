8a655ac577b89d6040d524b4491b994ebb157302
jindaxiang <jindaxiang@163.com>
2020-04-12 20:32:02 +0800

    docs(docs): docs
    docs

--- akshare/article/epu_index.py
+++ akshare/article/epu_index.py
@@ -1,22 +1,30 @@
 # -*- coding:utf-8 -*-
 # /usr/bin/env python
 """
-Author: Albert King
-date: 2019/11/30 13:14
-contact: jindaxiang@163.com
-desc: 获取 http://www.policyuncertainty.com/index.html 网站的经济政策不确定性指数
+Date: 2019/11/30 13:14
+Desc: 网站的经济政策不确定性指数
+http://www.policyuncertainty.com/index.html
 """
 import pandas as pd
 
 
-def article_epu_index(index="China"):
+def article_epu_index(index: str = "China") -> pd.DataFrame:
+    """
+    网站的经济政策不确定性指数
+    :param index: 指定的国家名称, e.g. “China”
+    :type index: str
+    :return: 指定 index 的数据
+    :rtype: pandas.DataFrame
+    """
     if index == "China New":
         index = "China"
     if index == "USA":
         index = "US"
     if index == "Hong Kong":
         index = "HK"
-        epu_df = pd.read_excel(f"http://www.policyuncertainty.com/media/{index}_EPU_Data_Annotated.xlsx")
+        epu_df = pd.read_excel(
+            f"http://www.policyuncertainty.com/media/{index}_EPU_Data_Annotated.xlsx"
+        )
         return epu_df
     if index in ["Germany", "France", "Italy"]:  # 欧洲
         index = "Europe"
@@ -25,10 +33,14 @@ def article_epu_index(index="China"):
     if index == "Spain New":
         index = "Spain"
     if index in ["Ireland", "Chile", "Colombia", "Netherlands", "Singapore", "Sweden"]:
-        epu_df = pd.read_excel(f"http://www.policyuncertainty.com/media/{index}_Policy_Uncertainty_Data.xlsx")
+        epu_df = pd.read_excel(
+            f"http://www.policyuncertainty.com/media/{index}_Policy_Uncertainty_Data.xlsx"
+        )
         return epu_df
     if index == "Greece":
-        epu_df = pd.read_excel(f"http://www.policyuncertainty.com/media/FKT_{index}_Policy_Uncertainty_Data.xlsx")
+        epu_df = pd.read_excel(
+            f"http://www.policyuncertainty.com/media/FKT_{index}_Policy_Uncertainty_Data.xlsx"
+        )
         return epu_df
     url = f"http://www.policyuncertainty.com/media/{index}_Policy_Uncertainty_Data.csv"
     epu_df = pd.read_csv(url)
@@ -36,5 +48,5 @@ def article_epu_index(index="China"):
 
 
 if __name__ == "__main__":
-    epu_index_df = article_epu_index(index="China")
-    print(epu_index_df)
+    article_epu_index_df = article_epu_index(index="China")
+    print(article_epu_index_df)
--- akshare/article/risk_rv.py
+++ akshare/article/risk_rv.py
@@ -17,13 +17,14 @@ from bs4 import BeautifulSoup
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-def article_oman_rv(symbol="FTSE", index="rk_th2", plot=True):
+def article_oman_rv(symbol: str = "FTSE", index: str = "rk_th2", plot: bool = True) -> pd.DataFrame:
     """
-    获取 Oxford-Man Institute of Quantitative Finance Realized Library 的数据
+    Oxford-Man Institute of Quantitative Finance Realized Library 的数据
     :param symbol: str ['AEX', 'AORD', 'BFX', 'BSESN', 'BVLG', 'BVSP', 'DJI', 'FCHI', 'FTMIB', 'FTSE', 'GDAXI', 'GSPTSE', 'HSI', 'IBEX', 'IXIC', 'KS11', 'KSE', 'MXX', 'N225', 'NSEI', 'OMXC20', 'OMXHPI', 'OMXSPI', 'OSEAX', 'RUT', 'SMSI', 'SPX', 'SSEC', 'SSMI', 'STI', 'STOXX50E']
     :param index: str 指标 ['medrv', 'rk_twoscale', 'bv', 'rv10', 'rv5', 'rk_th2', 'rv10_ss', 'rsv', 'rv5_ss', 'bv_ss', 'rk_parzen', 'rsv_ss']
     :param plot: Bool 是否画图
     :return: pandas.DataFrame
+
     The Oxford-Man Institute's "realised library" contains daily non-parametric measures of how volatility financial assets or indexes were in the past. Each day's volatility measure depends solely on financial data from that day. They are driven by the use of the latest innovations in econometric modelling and theory to design them, while we draw our high frequency data from the Thomson Reuters DataScope Tick History database. Realised measures are not volatility forecasts. However, some researchers use these measures as an input into forecasting models. The aim of this line of research is to make financial markets more transparent by exposing how volatility changes through time.
 
     This Library is used as the basis of some of our own research, which effects its scope, and is made available here to encourage the more widespread exploitation of these methods. It is given 'as is' and solely for informational purposes, please read the disclaimer.
@@ -87,12 +88,13 @@ def article_oman_rv(symbol="FTSE", index="rk_th2", plot=True):
         return temp_df
 
 
-def article_oman_rv_short(symbol="FTSE", plot=True):
+def article_oman_rv_short(symbol: str = "FTSE", plot: str = True) -> pd.DataFrame:
     """
-    获取 Oxford-Man Institute of Quantitative Finance Realized Library 的数据
+    Oxford-Man Institute of Quantitative Finance Realized Library 的数据
     :param symbol: str FTSE: FTSE 100, GDAXI: DAX, RUT: Russel 2000, SPX: S&P 500 Index, STOXX50E: EURO STOXX 50, SSEC: Shanghai Composite Index, N225: Nikkei 225
     :param plot: Bool 是否画图
     :return: pandas.DataFrame
+
     The Oxford-Man Institute's "realised library" contains daily non-parametric measures of how volatility financial assets or indexes were in the past. Each day's volatility measure depends solely on financial data from that day. They are driven by the use of the latest innovations in econometric modelling and theory to design them, while we draw our high frequency data from the Thomson Reuters DataScope Tick History database. Realised measures are not volatility forecasts. However, some researchers use these measures as an input into forecasting models. The aim of this line of research is to make financial markets more transparent by exposing how volatility changes through time.
 
     This Library is used as the basis of some of our own research, which effects its scope, and is made available here to encourage the more widespread exploitation of these methods. It is given 'as is' and solely for informational purposes, please read the disclaimer.
@@ -136,9 +138,9 @@ def article_oman_rv_short(symbol="FTSE", plot=True):
         return temp_df
 
 
-def article_rlab_rv(symbol="39693", plot=True):
+def article_rlab_rv(symbol: str = "39693", plot: bool = True) -> pd.DataFrame:
     """
-    获取修大成主页-Risk Lab-Realized Volatility
+    修大成主页-Risk Lab-Realized Volatility
     :param symbol: str 股票代码
     :param plot: Bool 是否绘图
     :return: pandas.DataFrame
@@ -155,8 +157,7 @@ def article_rlab_rv(symbol="39693", plot=True):
     2019-11-08    0.199549
     Name: RV, Length: 5810, dtype: float64
 
-
-    Website:
+    Website
     https://dachxiu.chicagobooth.edu/
 
     Objective
@@ -174,7 +175,6 @@ def article_rlab_rv(symbol="39693", plot=True):
     3. “How Often to Sample A Continuous-time Process in the Presence of Market Microstructure Noise”, by Yacine Aït-Sahalia, Per Mykland, and Lan Zhang. Review of Financial Studies, 18 (2005), 351–416.
     4. “The Distribution of Exchange Rate Volatility”, by Torben Andersen, Tim Bollerslev, Francis X. Diebold, and Paul Labys. Journal of the American Statistical Association, 96 (2001), 42-55.
     5. “Econometric Analysis of Realized Volatility and Its Use in Estimating Stochastic Volatility Models”, by Ole E Barndorff‐Nielsen and Neil Shephard. Journal of the Royal Statistical Society: Series B, 64 (2002), 253-280.
-
     """
     print("由于服务器在国外, 请稍后, 如果访问失败, 请使用代理工具")
     url = "https://dachxiu.chicagobooth.edu/data.php"
