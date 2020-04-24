1f9e32e7bbc44fe33465f66bbf853ecd15ab7a4e
jindaxiang <jindaxiang@163.com>
2020-04-24 12:46:38 +0800

    fix(stock_zh_a_sina.py): stock_zh_a and stock_zh_kcb return adjusted stock price
    stock_zh_a and stock_zh_kcb return adjusted stock price

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -915,9 +915,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.4.87: fix: jinshi data update
 0.4.88: fix: macro_china
 0.4.89: fix: macro_other
+0.4.90: fix: stock_zh_a and stock_zh_kcb return adjusted stock price
 """
 
-__version__ = "0.4.89"
+__version__ = "0.4.90"
 __author__ = "Albert King"
 
 """
