0da35c378325cb35620b4225d7700a45fe9ecd03
jindaxiang <jindaxiang@163.com>
2020-04-29 19:59:08 +0800

    fix(interbank_rate_em.py): interbank_rate_em.py add need_page parameter to control update content
    interbank_rate_em.py add need_page parameter to control update content

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -922,9 +922,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.4.94: fix: air_quality path
 0.4.95: add: js file
 0.4.96: fix: format air interface
+0.4.97: fix: interbank_rate_em.py add need_page parameter to control update content
 """
 
-__version__ = "0.4.96"
+__version__ = "0.4.97"
 __author__ = "Albert King"
 
 """
