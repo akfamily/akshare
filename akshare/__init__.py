31ff5643b6a944df48ede407419526dd82df6253
jindaxiang <jindaxiang@163.com>
2020-05-11 17:28:27 +0800

    fix(zh_stock_ah_tx.py): fix zh_stock_ah_tx return fields
    fix zh_stock_ah_tx return fields

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -935,9 +935,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.5.7: fix: import akshare only load functions
 0.5.8: add: macro_china_money_supply
 0.5.9: add: macro_china_new_house_price, macro_china_enterprise_boom_index, macro_china_national_tax_receipts
+0.5.10: fix: zh_stock_ah_tx
 """
 
-__version__ = "0.5.9"
+__version__ = "0.5.10"
 __author__ = "Albert King"
 
 """
