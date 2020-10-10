9a11047e76e32f7bf3101de29c7d72858e352bee
jindaxiang <jindaxiang@163.com>
2020-10-10 16:03:03 +0800

    fix(sunrise_daily): fix sunrise_daily interface
    fix sunrise_daily interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1086,9 +1086,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.6.59: add: add macro_china_passenger_load_factor interface
 0.6.60: fix: fix stock_sina_lhb_ggtj interface
 0.6.61: fix: fix option_czce_hist interface
+0.6.62: fix: fix sunrise_daily interface
 """
 
-__version__ = "0.6.61"
+__version__ = "0.6.62"
 __author__ = "Albert King"
 
 """
