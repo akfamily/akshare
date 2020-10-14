8c3c6e0d5d68d7610327534930bfcdb5ade6256c
jindaxiang <jindaxiang@163.com>
2020-10-14 12:27:26 +0800

    fix(bond_investing_global): fix bond_investing_global interface
    fix bond_investing_global interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1094,9 +1094,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.6.67: add: add macro_china_foreign_exchange_gold interface
 0.6.68: add: add macro_china_retail_price_index interface
 0.6.69: fix: fix box_office_spot interface
+0.6.70: fix: fix bond_investing_global interface
 """
 
-__version__ = "0.6.69"
+__version__ = "0.6.70"
 __author__ = "Albert King"
 
 """
--- docs/changelog.md
+++ docs/changelog.md
@@ -1607,4 +1607,6 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.6.68: add: add macro_china_retail_price_index interface
 
 0.6.69: fix: fix box_office_spot interface
+
+0.6.70: fix: fix bond_investing_global interface
 ```
