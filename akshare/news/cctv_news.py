157255acfe7235ec9e0899259d49202050f6d3f1
jindaxiang <jindaxiang@163.com>
2020-12-03 15:27:06 +0800

    docs(docs): docs
    docs

--- akshare/news/cctv_news.py
+++ akshare/news/cctv_news.py
@@ -56,5 +56,5 @@ def news_cctv(date: str = "20200902") -> pd.DataFrame:
 
 
 if __name__ == "__main__":
-    news_cctv_df = news_cctv(date="20201104")
+    news_cctv_df = news_cctv(date="20201201")
     print(news_cctv_df)
