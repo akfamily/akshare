58eb8f12e3dc4b8b6b6c195cdd6e829031f3296c
jindaxiang <jindaxiang@163.com>
2020-12-10 20:08:07 +0800

    fix(stock_hk_daily): fix stock_hk_daily interface
    fix stock_hk_daily interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1162,9 +1162,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.7.41: add: add stock_em_yzxdr interface
 0.7.42: fix: fix fund_em_etf_fund_daily interface
 0.7.43: fix: fix match_main_contract interface
+0.7.44: fix: fix stock_hk_daily interface
 """
 
-__version__ = "0.7.43"
+__version__ = "0.7.44"
 __author__ = "Albert King"
 
 import sys
--- docs/changelog.md
+++ docs/changelog.md
@@ -1748,4 +1748,6 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.7.42: fix: fix fund_em_etf_fund_daily interface
 
 0.7.43: fix: fix match_main_contract interface
+
+0.7.44: fix: fix stock_hk_daily interface
 ```
