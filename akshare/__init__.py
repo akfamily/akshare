77286067db73d10206d9e0d1431785f7c886bd3d
jindaxiang <jindaxiang@163.com>
2020-12-16 17:30:09 +0800

    feat(futures_dce_position_rank): fix futures_dce_position_rank interface
    fix futures_dce_position_rank interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1166,9 +1166,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.7.45: fix: fix stock_em_yzxdr interface
 0.7.46: fix: fix option_czce_hist interface
 0.7.47: fix: fix bond_zh_cov interface
+0.7.48: fix: fix futures_dce_position_rank interface
 """
 
-__version__ = "0.7.47"
+__version__ = "0.7.48"
 __author__ = "Albert King"
 
 import sys
