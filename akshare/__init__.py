b7cce0fb33ff8381c3dea8fb61a501e2d892f5bc
jindaxiang <jindaxiang@163.com>
2021-01-04 20:23:24 +0800

    fix(qq group): fix QQ group interface
    fix QQ group interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1180,9 +1180,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.7.59: fix: fix read_excel support
 0.7.60: fix: fix fund_em_open_fund_daily interface
 0.7.61: fix: fix calendar.json interface
+0.7.62: fix: fix QQ group interface
 """
 
-__version__ = "0.7.61"
+__version__ = "0.7.62"
 __author__ = "Albert King"
 
 import sys
