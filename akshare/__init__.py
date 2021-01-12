b7fe5745dfaabc113b8dc1205cd14a2d00785821
jindaxiang <jindaxiang@163.com>
2021-01-12 12:17:34 +0800

    fix(stock_zh_a_minute): fix stock_zh_a_minute interface
    fix stock_zh_a_minute interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1192,9 +1192,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.7.71: fix: fix stock_us_daily interface
 0.7.72: fix: fix get_ine_daily interface
 0.7.73: add: add macro_china_money_supply interface
+0.7.74: fix: fix stock_zh_a_minute interface
 """
 
-__version__ = "0.7.73"
+__version__ = "0.7.74"
 __author__ = "Albert King"
 
 import sys
--- docs/changelog.md
+++ docs/changelog.md
@@ -1808,4 +1808,6 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.7.72: fix: fix get_ine_daily interface
 
 0.7.73: add: add macro_china_money_supply interface
+
+0.7.74: fix: fix stock_zh_a_minute interface
 ```
