01f0859f629d9560b8e8ee531ce92b57d05c3d95
jindaxiang <jindaxiang@163.com>
2021-02-01 17:48:18 +0800

    feat(stock_zh_a_tick_163_now): add stock_zh_a_tick_163_now interface
    add stock_zh_a_tick_163_now interface

--- akshare/stock/zh_stock_a_tick_tx_163.py
+++ akshare/stock/zh_stock_a_tick_tx_163.py
@@ -1,18 +1,27 @@
 # -*- coding:utf-8 -*-
 # /usr/bin/env python
 """
-Date: 2021/1/26 16:39
-Desc: 腾讯-股票-实时行情-成交明细
+Date: 2021/2/1 16:39
+Desc: 腾讯-网易-股票-实时行情-成交明细
 成交明细-每个交易日 16:00 提供当日数据
 港股报价延时 15 分钟
 """
 from io import StringIO, BytesIO
+from tqdm import tqdm
 
 import pandas as pd
 import requests
 
 
 def stock_zh_a_tick_tx_js(code: str = "sz000001") -> pd.DataFrame:
+    """
+    腾讯财经-历史分笔数据
+    http://gu.qq.com/sz300494/gp/detail
+    :param code: 股票代码
+    :type code: str
+    :return: 股票代码
+    :rtype: pandas.DataFrame
+    """
     big_df = pd.DataFrame()
     page = 0
     while True:
@@ -61,7 +70,7 @@ def stock_zh_a_tick_tx(code: str = "sh600848", trade_date: str = "20201111") -> 
     return temp_df
 
 
-def stock_zh_a_tick_163(code: str = "sh600848", trade_date: str = "20200410") -> pd.DataFrame:
+def stock_zh_a_tick_163(code: str = "sh600848", trade_date: str = "20210128") -> pd.DataFrame:
     """
     成交明细-每个交易日 22:00 提供当日数据
     http://quotes.money.163.com/trade/cjmx_000001.html#01b05
@@ -74,16 +83,80 @@ def stock_zh_a_tick_163(code: str = "sh600848", trade_date: str = "20200410") ->
     """
     name_code_map = {"sh": "0", "sz": 1}
     url = f"http://quotes.money.163.com/cjmx/{trade_date[:4]}/{trade_date}/{name_code_map[code[:2]]}{code[2:]}.xls"
-    res = requests.get(url)
-    res.encoding = "utf-8"
-    try:
-        temp_df = pd.read_excel(BytesIO(res.content))
-        return temp_df
-    except:
-        return "无当前交易日数据，请稍后再试"
+    r = requests.get(url)
+    r.encoding = "utf-8"
+    temp_df = pd.read_excel(BytesIO(r.content))
+    temp_df.columns = [
+        "成交时间",
+        "成交价",
+        "价格变动",
+        "成交量",
+        "成交额",
+        "性质",
+    ]
+    return temp_df
+
+
+def stock_zh_a_tick_163_now(code: str = "000001") -> pd.DataFrame:
+    """
+    成交明细-收盘后获取, 补充 stock_zh_a_tick_163 接口, 用来尽快获取数据
+    http://quotes.money.163.com/trade/cjmx_000001.html#01b05
+    :param code: 带市场标识的股票代码
+    :type code: str
+    :return: 返回当日股票成交明细的数据
+    :rtype: pandas.DataFrame
+    """
+    time_list_one = [item.isoformat().split("T")[1] for item in pd.date_range('09:30:00', '11:30:00', freq='5min').tolist()][1:]
+    time_list_two = [item.isoformat().split("T")[1] for item in pd.date_range('13:00:00', '15:00:00', freq='5min').tolist()][1:]
+    time_list_one.extend(time_list_two)
+    big_df = pd.DataFrame()
+    for item in tqdm(time_list_one):
+        url = "http://quotes.money.163.com/service/zhubi_ajax.html"
+        params = {
+            "symbol": code,
+            "end": item
+        }
+        r = requests.get(url, params=params)
+        data_json = r.json()
+        temp_df = pd.DataFrame(data_json["zhubi_list"])
+        del temp_df["_id"]
+        del temp_df["TRADE_TYPE"]
+        del temp_df["DATE"]
+        temp_df.reset_index(inplace=True)
+        temp_df.sort_values(by="index", ascending=False, ignore_index=True, inplace=True)
+        big_df = big_df.append(temp_df)
+    del big_df["index"]
+    big_df.columns = [
+        "_",
+        "成交量",
+        "成交价",
+        "成交额",
+        "价格变动",
+        "成交时间",
+        "性质",
+    ]
+    big_df = big_df[[
+        "成交时间",
+        "成交价",
+        "价格变动",
+        "成交量",
+        "成交额",
+        "性质",
+    ]]
+    big_df["成交量"] = big_df["成交量"] / 100
+    return big_df
 
 
 if __name__ == "__main__":
+    stock_zh_a_tick_163_df = stock_zh_a_tick_163(code="sh600848", trade_date="20210127")
+    print(stock_zh_a_tick_163_df)
+
+    stock_zh_a_tick_163_now_df = stock_zh_a_tick_163_now(code="000001")
+    print(stock_zh_a_tick_163_now_df)
+
+    stock_zh_a_tick_tx_js_df = stock_zh_a_tick_tx_js(code="sz000001")
+    print(stock_zh_a_tick_tx_js_df)
+
     date_list = pd.date_range(start="20201111", end="20201130").tolist()
     date_list = [item.strftime("%Y%m%d") for item in date_list]
     for item in date_list:
@@ -91,8 +164,3 @@ if __name__ == "__main__":
         data = stock_zh_a_tick_tx(code="sz000001", trade_date=f"{item}")
         if not data.empty:
             print(data)
-    stock_zh_a_tick_163_df = stock_zh_a_tick_163(code="sh600848", trade_date="20210125")
-    print(stock_zh_a_tick_163_df)
-
-    stock_zh_a_tick_tx_js_df = stock_zh_a_tick_tx_js(code="sz000001")
-    print(stock_zh_a_tick_tx_js_df)
