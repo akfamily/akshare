ce6e8e4fcd10f616ff4929e136084e3d60ff8833
jindaxiang <jindaxiang@163.com>
2021-04-12 16:03:32 +0800

    fix(stock_zh_a_tick_163): fix stock_zh_a_tick_163 interface
    fix stock_zh_a_tick_163 interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1279,9 +1279,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.8.59: fix: fix get_sector_futures interface
 0.8.60: fix: fix currency_hist interface
 0.8.61: fix: fix stock_em_hsgt_hold_stock interface
+0.8.62: fix: fix stock_zh_a_tick_163 interface
 """
 
-__version__ = "0.8.61"
+__version__ = "0.8.62"
 __author__ = "Albert King"
 
 import sys
