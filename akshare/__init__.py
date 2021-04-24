702bf9cea23bcce18cf37bb7d6dbbd2bf9a4da3e
jindaxiang <jindaxiang@163.com>
2021-04-24 22:31:47 +0800

    feat(stock_fund_flow_industry): add stock_fund_flow_industry interface
    add stock_fund_flow_industry interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1305,9 +1305,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.8.85: fix: fix rename amac_person_org_list to amac_person_fund_org_list interface
 0.8.86: add: add amac_person_bond_org_list interface
 0.8.87: add: add stock_fund_flow_concept interface
+0.8.88: add: add stock_fund_flow_industry interface
 """
 
-__version__ = "0.8.87"
+__version__ = "0.8.88"
 __author__ = "Albert King"
 
 import sys
