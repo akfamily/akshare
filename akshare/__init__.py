33e4d669d1d7adbab24b34bf1ab0385a266b0d52
jindaxiang <jindaxiang@163.com>
2021-04-25 16:40:42 +0800

    feat(stock_fund_flow_big_deal): add stock_fund_flow_big_deal interface
    add stock_fund_flow_big_deal interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1307,9 +1307,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.8.87: add: add stock_fund_flow_concept interface
 0.8.88: add: add stock_fund_flow_industry interface
 0.8.89: add: add stock_fund_flow_individual interface
+0.8.90: add: add stock_fund_flow_big_deal interface
 """
 
-__version__ = "0.8.89"
+__version__ = "0.8.90"
 __author__ = "Albert King"
 
 import sys
