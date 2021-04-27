5e8b5af9a84bd31c1680612d0e9f5e58d5cb5c80
jindaxiang <jindaxiang@163.com>
2021-04-27 14:42:09 +0800

    fix(docs): docs
    docs

--- akshare/energy/energy_oil.py
+++ akshare/energy/energy_oil.py
@@ -33,9 +33,9 @@ def energy_oil_hist() -> pd.DataFrame:
     r = requests.get(url, params=params)
     data_text = r.text
     data_json = json.loads(data_text[data_text.find("{"): -1])
-    data_df = pd.DataFrame(data_json["result"]["data"])
-    data_df.columns = ["日期", "汽油价格", "柴油价格", "汽油涨幅", "柴油涨幅"]
-    return data_df
+    temp_df = pd.DataFrame(data_json["result"]["data"])
+    temp_df.columns = ["日期", "汽油价格", "柴油价格", "汽油涨幅", "柴油涨幅"]
+    return temp_df
 
 
 def energy_oil_detail(date: str = "2020-03-19") -> pd.DataFrame:
@@ -62,12 +62,13 @@ def energy_oil_detail(date: str = "2020-03-19") -> pd.DataFrame:
     r = requests.get(url, params=params)
     data_text = r.text
     data_json = json.loads(data_text[data_text.find("{"): -1])
-    data_df = pd.DataFrame(data_json["result"]["data"]).iloc[:, 1:]
-    return data_df
+    temp_df = pd.DataFrame(data_json["result"]["data"]).iloc[:, 1:]
+    return temp_df
 
 
 if __name__ == "__main__":
     energy_oil_hist_df = energy_oil_hist()
     print(energy_oil_hist_df)
-    energy_oil_detail_df = energy_oil_detail(date="2020-09-19")
+
+    energy_oil_detail_df = energy_oil_detail(date="2021-04-01")
     print(energy_oil_detail_df)
