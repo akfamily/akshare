762696830538637782d6b1668516cf58d27284d1
jindaxiang <jindaxiang@163.com>
2021-05-18 16:50:23 +0800

    feat(bond_investing_global): fix bond_investing_global interface
    fix bond_investing_global interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1348,9 +1348,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.9.29: add: add stock_em_qbzf interface
 0.9.30: add: add stock_em_pg interface
 0.9.31: fix: fix index_investing_global interface
+0.9.32: fix: fix bond_investing_global interface
 """
 
-__version__ = "0.9.31"
+__version__ = "0.9.32"
 __author__ = "Albert King"
 
 import sys
