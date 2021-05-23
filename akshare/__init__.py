1013dac62ddb52d56c4d6504cfc352ac1c8172fa
jindaxiang <jindaxiang@163.com>
2021-05-23 16:57:13 +0800

    fix(stock_em_zt_pool_strong): fix stock_em_zt_pool_strong interface
    fix stock_em_zt_pool_strong interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1359,9 +1359,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.9.40: fix: fix bond_cov_comparison interface
 0.9.41: fix: fix stock_em_zt_pool_previous interface
 0.9.42: add: add stock_em_zt_pool_strong interface
+0.9.43: fix: fix stock_em_zt_pool_strong interface
 """
 
-__version__ = "0.9.42"
+__version__ = "0.9.43"
 __author__ = "Albert King"
 
 import sys
