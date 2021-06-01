7b450c50f6996dec68b452418a44bfafbaf2ea0b
jindaxiang <jindaxiang@163.com>
2021-06-01 16:45:00 +0800

    fix(macro_fx_sentiment): fix macro_fx_sentiment interface
    fix macro_fx_sentiment interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1372,9 +1372,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.9.53: add: add stock_zh_a_alerts_cls interface
 0.9.54: fix: fix bond_zh_us_rate interface
 0.9.55: fix: fix index_vix interface
+0.9.56: fix: fix macro_fx_sentiment interface
 """
 
-__version__ = "0.9.55"
+__version__ = "0.9.56"
 __author__ = "Albert King"
 
 import sys
--- docs/changelog.md
+++ docs/changelog.md
@@ -2160,4 +2160,6 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.9.54: fix: fix bond_zh_us_rate interface
 
 0.9.55: fix: fix index_vix interface
+
+0.9.56: fix: fix macro_fx_sentiment interface
 ```
