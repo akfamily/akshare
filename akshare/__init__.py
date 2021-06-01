0fc569d2d11f789ca2a625c3005bc9b7aa1b6050
jindaxiang <jindaxiang@163.com>
2021-06-01 17:22:18 +0800

    fix(stock_zh_a_alerts_cls): fix stock_zh_a_alerts_cls interface
    fix stock_zh_a_alerts_cls interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1373,9 +1373,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.9.54: fix: fix bond_zh_us_rate interface
 0.9.55: fix: fix index_vix interface
 0.9.56: fix: fix macro_fx_sentiment interface
+0.9.57: fix: fix stock_zh_a_alerts_cls interface
 """
 
-__version__ = "0.9.56"
+__version__ = "0.9.57"
 __author__ = "Albert King"
 
 import sys
