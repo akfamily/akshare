4ee2b7f96f5a6d4398a28a94c2f962906c7a95e9
jindaxiang <jindaxiang@163.com>
2021-07-08 22:29:46 +0800

    feat(macro_usa_phs): add macro_usa_phs interface
    add macro_usa_phs interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1419,9 +1419,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 0.9.97: fix: fix urllib3 version at 1.25.8
 0.9.98: fix: fix js_news interface
 0.9.99: fix: fix news_cctv interface
+1.0.1: add: add macro_usa_phs interface
 """
 
-__version__ = "0.9.99"
+__version__ = "1.0.1"
 __author__ = "Albert King"
 
 import sys
