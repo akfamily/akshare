bca4b41f4857308227e80a35f61a6e769711ec8c
jindaxiang <jindaxiang@163.com>
2021-07-08 21:49:04 +0800

    fix(news_cctv): fix news_cctv interface
    fix news_cctv interface

--- akshare/news/news_cctv.py
+++ akshare/news/news_cctv.py
@@ -1,61 +1,44 @@
 # -*- coding:utf-8 -*-
 # /usr/bin/env python
 """
-Date: 2021/6/29 12:54
+Date: 2021/7/8 21:44
 Desc: 新闻联播文字稿
-http://www.xwlbo.com/date-2020-7-11.html
+https://tv.cctv.com/lm/xwlb/?spm=C52056131267.P4y8I53JvSWE.0.0
 """
-import re
-
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
-# TODO 部分日期数据缺失
 
 
-def news_cctv(date: str = "20210628") -> pd.DataFrame:
+def news_cctv(date: str = "20210708") -> pd.DataFrame:
     """
     新闻联播文字稿
-    http://www.xwlbo.com/date-2020-7-11.html
-    :param date: 需要获取数据的日期; 目前 2018 年后
+    https://tv.cctv.com/lm/xwlb/?spm=C52056131267.P4y8I53JvSWE.0.0
+    :param date: 需要获取数据的日期; 目前 20160330 年后
     :type date: str
     :return: 新闻联播文字稿
     :rtype: pandas.DataFrame
     """
-    url = f"http://www.xwlbo.com/date-{date[:4]}-{int(date[4:6])}-{date[6:]}.html"
+    url = f'https://tv.cctv.com/lm/xwlb/day/{date}.shtml'
     r = requests.get(url)
+    r.encoding = 'utf-8'
     soup = BeautifulSoup(r.text, "lxml")
-    pattern = re.compile(f"{date[:4]}年{int(date[4:6])}月{int(date[6:])}日新闻联播")
-    url = soup.find("a", text=pattern)["href"]
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "lxml")
-    news_list = soup.find("div", attrs={"class": "text_content"}).find_all("a")
-    title_list = [item.get_text() for item in news_list]
-    href_list = [item["href"] for item in news_list]
-    news_content_list = []
-    for href in tqdm(href_list):
-        if "http://www.xwlbo.com" in href:
-            url = href
-        else:
-            url = f"http://www.xwlbo.com/{href}"
-        r = requests.get(url)
+    page_url = [item.find("a")['href'] for item in soup.find_all("li")[1:]]
+    title_list = []
+    content_list = []
+    for page in tqdm(page_url, leave=False):
+        r = requests.get(page)
+        r.encoding = 'utf-8'
         soup = BeautifulSoup(r.text, "lxml")
-        content_list = (
-            soup.find(attrs={"id": "tab_con2"})
-            .find("div", attrs={"class": "text_content"})
-            .find_all("p")
-        )
-        content = " ".join([item.get_text() for item in content_list])
-        pure_content = content.strip("央视网消息（新闻联播文字版）：").strip()
-        news_content_list.append(pure_content)
-    temp_df = pd.DataFrame(
-        [[date] * len(title_list), title_list, news_content_list],
-        index=["date", "title", "content"],
-    ).T
+        title = soup.find('h3').text
+        content = soup.find('div', attrs={"class": 'cnt_bd'}).text
+        title_list.append(title.strip("[视频]").strip().replace("\n", " "))
+        content_list.append(content.strip("央视网消息（新闻联播）：").strip("(新闻联播)：").strip().replace("\n", " "))
+    temp_df = pd.DataFrame([[date]*len(title_list), title_list, content_list], index=["date", "title", "content"]).T
     return temp_df
 
 
 if __name__ == "__main__":
-    news_cctv_df = news_cctv(date="20210622")
+    news_cctv_df = news_cctv(date="20210708")
     print(news_cctv_df)
