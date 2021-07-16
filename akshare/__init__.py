ffb9dbd191e4188cb14b91ba9b6921da3545b056
jindaxiang <jindaxiang@163.com>
2021-07-16 19:01:58 +0800

    fix(stock_em_dxsyl): fix stock_em_dxsyl interface
    fix stock_em_dxsyl interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1441,9 +1441,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.0.20: fix: fix bond_zh_hs_cov_daily interface
 1.0.21: fix: fix charity_china interface
 1.0.22: fix: fix stock_em_xgsglb interface
+1.0.23: fix: fix stock_em_dxsyl interface
 """
 
-__version__ = "1.0.22"
+__version__ = "1.0.23"
 __author__ = "Albert King"
 
 import sys
