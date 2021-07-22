6a91abfc4ac8a56c6ad8bc76f2e32ecf9f0b9f27
jindaxiang <jindaxiang@163.com>
2021-07-22 14:18:32 +0800

    fix(bond_china_close_return): fix bond_china_close_return interface
    fix bond_china_close_return interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1451,9 +1451,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.0.30: fix: fix option_czce_hist interface
 1.0.31: fix: fix futures_foreign_commodity_realtime interface
 1.0.32: fix: fix covid_19_baidu interface
+1.0.33: fix: fix bond_china_close_return interface
 """
 
-__version__ = "1.0.32"
+__version__ = "1.0.33"
 __author__ = "Albert King"
 
 import sys
