71fdf8256325e9be423a9ddb95b5e0387e03d40a
jindaxiang <jindaxiang@163.com>
2021-08-23 21:15:35 +0800

    fix(bond_zh_hs_cov_spot): fix bond_zh_hs_cov_spot interface
    fix bond_zh_hs_cov_spot interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1492,9 +1492,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.0.71: fix: remove watch_argus interface
 1.0.72: fix: fix amac_fund_abs interface
 1.0.73: add: add bond_cash_summary_sse interface
+1.0.74: fix: fix bond_zh_hs_cov_spot interface
 """
 
-__version__ = "1.0.73"
+__version__ = "1.0.74"
 __author__ = "Albert King"
 
 import sys
