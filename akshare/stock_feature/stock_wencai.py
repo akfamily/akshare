1d65931828764bdd079d65a2551f5b827b4ea03d
jindaxiang <jindaxiang@163.com>
2021-09-08 16:41:12 +0800

    docs(docs): docs
    docs

--- akshare/stock_feature/stock_wencai.py
+++ akshare/stock_feature/stock_wencai.py
@@ -119,5 +119,5 @@ def stock_wc_hot_rank(date: str = "20210430") -> pd.DataFrame:
 
 
 if __name__ == "__main__":
-    stock_wc_hot_rank_df = stock_wc_hot_rank(date="20210521")
+    stock_wc_hot_rank_df = stock_wc_hot_rank(date="20210907")
     print(stock_wc_hot_rank_df)
