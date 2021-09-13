d27e868d01e8a96170999832fcaee10db16d0a92
jindaxiang <jindaxiang@163.com>
2021-09-13 20:35:01 +0800

    fix(covid_19_trace): fix covid_19_trace interface
    fix covid_19_trace interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1524,9 +1524,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.1.4: fix: fix stock_average_position_legu interface
 1.1.5: add: add stock_rank_forecast_cninfo interface
 1.1.6: fix: fix futures_zh_minute_sina interface
+1.1.7: fix: fix covid_19_trace interface
 """
 
-__version__ = "1.1.6"
+__version__ = "1.1.7"
 __author__ = "Albert King"
 
 import sys
