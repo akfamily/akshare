678385490a27d2b3f01f0076ab79706336fae69d
jindaxiang <jindaxiang@163.com>
2021-09-17 17:12:10 +0800

    fix(stock_em_jgdy_detail): fix stock_em_jgdy_detail interface
    fix stock_em_jgdy_detail interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1533,9 +1533,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.1.13: add: add stock_dividents_cninfo interface
 1.1.14: fix: fix stock_fund_flow_concept interface
 1.1.15: add: add stock_new_gh_cninfo interface
+1.1.16: fix: fix stock_em_jgdy_detail interface
 """
 
-__version__ = "1.1.15"
+__version__ = "1.1.16"
 __author__ = "Albert King"
 
 import sys
--- akshare/stock_feature/stock_em_jgdy.py
+++ akshare/stock_feature/stock_em_jgdy.py
@@ -1,19 +1,17 @@
 # -*- coding:utf-8 -*-
 # /usr/bin/env python
 """
-date: 2021/8/2 16:02
+date: 2021/9/17 16:02
 desc: 东方财富网-数据中心-特色数据-机构调研
 http://data.eastmoney.com/jgdy/
 东方财富网-数据中心-特色数据-机构调研-机构调研统计: http://data.eastmoney.com/jgdy/tj.html
 东方财富网-数据中心-特色数据-机构调研-机构调研详细: http://data.eastmoney.com/jgdy/xx.html
 """
-import json
-
 import pandas as pd
 import requests
 
 
-def stock_em_jgdy_tj():
+def stock_em_jgdy_tj() -> pd.DataFrame:
     """
     东方财富网-数据中心-特色数据-机构调研-机构调研统计
     http://data.eastmoney.com/jgdy/tj.html
@@ -21,7 +19,6 @@ def stock_em_jgdy_tj():
     """
     url = "http://datainterface3.eastmoney.com/EM_DataCenter_V3/api/JGDYHZ/GetJGDYMX"
     params = {
-        "js": "datatable8554018",
         "tkn": "eastmoney",
         "secuCode": "",
         "sortfield": "0",
@@ -34,8 +31,7 @@ def stock_em_jgdy_tj():
         "_": "1605855456546",
     }
     r = requests.get(url, params=params)
-    data_text = r.text
-    data_json = json.loads(data_text[data_text.find("{") : -1])
+    data_json = r.json()
     temp_df = pd.DataFrame([item.split("|") for item in data_json["Data"][0]["Data"]])
     temp_df.reset_index(inplace=True)
     temp_df["index"] = list(range(1, len(temp_df) + 1))
@@ -81,6 +77,8 @@ def stock_em_jgdy_tj():
     temp_df['最新价'] = pd.to_numeric(temp_df['最新价'])
     temp_df['涨跌幅'] = pd.to_numeric(temp_df['涨跌幅'])
     temp_df['接待机构数量'] = pd.to_numeric(temp_df['接待机构数量'])
+    temp_df['接待日期'] = pd.to_datetime(temp_df['接待日期']).dt.date
+    temp_df['公告日期'] = pd.to_datetime(temp_df['公告日期']).dt.date
     return temp_df
 
 
@@ -93,7 +91,6 @@ def stock_em_jgdy_detail():
     """
     url = "http://datainterface3.eastmoney.com/EM_DataCenter_V3/api/JGDYMX/GetJGDYMX"
     params = {
-        "js": "datatable8554018",
         "tkn": "eastmoney",
         "secuCode": "",
         "dateTime": "",
@@ -107,8 +104,7 @@ def stock_em_jgdy_detail():
         "_": "1605855456546",
     }
     r = requests.get(url, params=params)
-    data_text = r.text
-    data_json = json.loads(data_text[data_text.find("{") : -1])
+    data_json = r.json()
     temp_df = pd.DataFrame([item.split("|") for item in data_json["Data"][0]["Data"]])
     temp_df.reset_index(inplace=True)
     temp_df["index"] = list(range(1, len(temp_df) + 1))
@@ -133,6 +129,7 @@ def stock_em_jgdy_detail():
         "_",
         "涨跌幅",
         "最新价",
+        "_",
     ]
 
     temp_df = temp_df[
@@ -154,6 +151,8 @@ def stock_em_jgdy_detail():
     ]
     temp_df['最新价'] = pd.to_numeric(temp_df['最新价'])
     temp_df['涨跌幅'] = pd.to_numeric(temp_df['涨跌幅'])
+    temp_df['调研日期'] = pd.to_datetime(temp_df['调研日期']).dt.date
+    temp_df['公告日期'] = pd.to_datetime(temp_df['公告日期']).dt.date
     return temp_df
 
 
