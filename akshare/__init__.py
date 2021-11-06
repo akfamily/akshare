90fca62c22890ac7c97f6fbd7e3aaf7c89dcd1ff
ak_quant <akshare.akfamily@gmail.com>
2021-11-06 19:45:51 +0800

    feat(stock_rank_xxtp_ths): add stock_rank_xxtp_ths interface
    add stock_rank_xxtp_ths interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1627,9 +1627,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.2.3 add: add stock_rank_xstp_ths interface
 1.2.4 fix: fix fund_portfolio_hold_em interface
 1.2.5 fix: fix index_stock_hist interface
+1.2.6 add: add stock_rank_xxtp_ths interface
 """
 
-__version__ = "1.2.5"
+__version__ = "1.2.6"
 __author__ = "Albert King"
 
 import sys
