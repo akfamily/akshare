5d1ce8dbbfa6db3660b3b0cc496da7f477897628
ak_quant <akshare.akfamily@gmail.com>
2021-11-09 16:43:28 +0800

    feat(stock_rank_xzjp_ths): add stock_rank_xzjp_ths interface
    add stock_rank_xzjp_ths interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1634,9 +1634,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.2.10 fix: fix bond_zh_hs_daily interface
 1.2.11 add: add stock_zh_a_gdhs_detail_em interface
 1.2.12 fix: fix stock_zh_a_gdhs interface
+1.2.13 add: add stock_rank_xzjp_ths interface
 """
 
-__version__ = "1.2.12"
+__version__ = "1.2.13"
 __author__ = "Albert King"
 
 import sys
@@ -1661,6 +1662,7 @@ from akshare.stock_feature.stock_technology_ths import (
     stock_rank_xxtp_ths,
     stock_rank_ljqd_ths,
     stock_rank_ljqs_ths,
+    stock_rank_xzjp_ths,
 )
 
 """
