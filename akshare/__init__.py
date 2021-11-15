2b35bf8c080855d10adde1ee4cc0cc9ca1e15d1b
ak_quant <akshare.akfamily@gmail.com>
2021-11-15 19:49:18 +0800

    fix(stock_zh_a_hist): fix stock_zh_a_hist interface
    fix stock_zh_a_hist interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1642,9 +1642,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.2.18 fix: fix amac interface
 1.2.19 fix: fix amac cons.py interface
 1.2.20 fix: fix stock_zh_a_spot_em interface
+1.2.21 fix: fix stock_zh_a_hist interface
 """
 
-__version__ = "1.2.20"
+__version__ = "1.2.21"
 __author__ = "Albert King"
 
 import sys
