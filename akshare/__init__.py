ffa450fe9a256440ef5c4d66faec590a1ce3c708
ak_quant <akshare.akfamily@gmail.com>
2021-12-14 16:35:40 +0800

    fix(sw_index_spot): fix sw_index_spot interface
    fix sw_index_spot interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1691,9 +1691,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.2.67 fix: fix air_quality_hebei interface
 1.2.68 fix: fix stock_financial_hk_analysis_indicator_em interface
 1.2.69 fix: fix get_futures_daily interface
+1.2.70 fix: fix sw_index_spot interface
 """
 
-__version__ = "1.2.69"
+__version__ = "1.2.70"
 __author__ = "Albert King"
 
 import sys
--- docs/changelog.md
+++ docs/changelog.md
@@ -2797,4 +2797,6 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.2.68 fix: fix stock_financial_hk_analysis_indicator_em interface
 
 1.2.69 fix: fix get_futures_daily interface
+
+1.2.70 fix: fix sw_index_spot interface
 ```
