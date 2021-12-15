31ef4a2aac8183120554034aec574d88aa2371ae
ak_quant <akshare.akfamily@gmail.com>
2021-12-15 15:39:42 +0800

    docs(docs): dosc
    docs

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1696,9 +1696,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.2.72 add: add sw_index_third_cons interface
 1.2.73 fix: fix sw_index_third_cons interface
 1.2.74 fix: fix macro_australia and macro_canada interface
+1.2.75 fix: fix docs interface
 """
 
-__version__ = "1.2.73"
+__version__ = "1.2.75"
 __author__ = "Albert King"
 
 import sys
