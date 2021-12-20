167f8a10a58c0e2b026acfb74272f6189c2424ae
ak_quant <akshare.akfamily@gmail.com>
2021-12-20 16:35:19 +0800

    fix(futures_index_cscidx): fix futures_index_cscidx interface
    fix futures_index_cscidx interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1710,9 +1710,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.2.86 add: add stock_a_pe_and_pb interface
 1.2.87 add: add stock_hk_ggt_components_em interface
 1.2.88 fix: fix futures_nh_index interface
+1.2.89 fix: fix futures_index_cscidx interface
 """
 
-__version__ = "1.2.88"
+__version__ = "1.2.89"
 __author__ = "Albert King"
 
 import sys
