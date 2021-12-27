c265c7440c89a28bc9da120456d39de6e6410c5e
ak_quant <akshare.akfamily@gmail.com>
2021-12-27 16:19:56 +0800

    fix(index_cflp_price): fix index_cflp_price interface
    fix index_cflp_price interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1732,9 +1732,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.3.8 fix: fix stock_info_change_name interface
 1.3.9 add: add index_sugar_msweet interface
 1.3.10 fix: fix index_sugar_msweet interface
+1.3.11 fix: fix index_cflp_price interface
 """
 
-__version__ = "1.3.10"
+__version__ = "1.3.11"
 __author__ = "Albert King"
 
 import sys
