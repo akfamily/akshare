06b324acfc9d961e02e875011c0c14a28b3eecc6
ak_quant <akshare.akfamily@gmail.com>
2021-12-31 14:08:33 +0800

    fix(index_cni_xx): fix index_cni_xx interface
    fix index_cni_xx interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1741,9 +1741,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.3.17 fix: fix stock_sse_deal_daily interface
 1.3.18 fix: fix stock_sse_summary interface
 1.3.19 add: add index_stock_cons_weight_csindex interface
+1.3.20 fix: fix index_cni_xx interface
 """
 
-__version__ = "1.3.19"
+__version__ = "1.3.20"
 __author__ = "Albert King"
 
 import sys
--- docs/changelog.md
+++ docs/changelog.md
@@ -2895,4 +2895,6 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.3.18 fix: fix stock_sse_summary interface
 
 1.3.19 add: add index_stock_cons_weight_csindex interface
+
+1.3.20 fix: fix index_cni_xx interface
 ```
