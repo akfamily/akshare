d399a53a2040d0b97635ecaa3de268e6fce27663
ak_quant <akshare.akfamily@gmail.com>
2021-12-31 15:06:13 +0800

    fix(hf_sp_500): fix hf_sp_500 interface
    fix hf_sp_500 interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1743,9 +1743,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.3.19 add: add index_stock_cons_weight_csindex interface
 1.3.20 fix: fix index_cni_xx interface
 1.3.21 fix: fix index_detail_hist_adjust_cni interface
+1.3.22 fix: fix hf_sp_500 interface
 """
 
-__version__ = "1.3.21"
+__version__ = "1.3.22"
 __author__ = "Albert King"
 
 import sys
