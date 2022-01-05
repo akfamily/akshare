3d4b6068f91d94d387b87c5d421cea35e0ab4611
ak_quant <akshare.akfamily@gmail.com>
2022-01-05 17:55:25 +0800

    fix(stock_dzjy_yybph): fix stock_dzjy_yybph interface
    fix stock_dzjy_yybph interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1754,9 +1754,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.3.30 fix: fix stock_zh_index_daily_em interface
 1.3.31 fix: fix stock_dzjy_mrmx interface
 1.3.32 fix: fix stock_dzjy_mrtj interface
+1.3.33 fix: fix stock_dzjy_yybph interface
 """
 
-__version__ = "1.3.32"
+__version__ = "1.3.33"
 __author__ = "Albert King"
 
 import sys
