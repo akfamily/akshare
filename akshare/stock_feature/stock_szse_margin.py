6d02788fac521c987f6a84c7a3edc0c4fdc5eee1
ak_quant <akshare.akfamily@gmail.com>
2022-01-19 14:29:12 +0800

    fix(stock_margin_detail_szse): fix stock_margin_detail_szse interface
    fix stock_margin_detail_szse interface

--- akshare/stock_feature/stock_szse_margin.py
+++ akshare/stock_feature/stock_szse_margin.py
@@ -102,38 +102,34 @@ def stock_margin_szse(date: str = "20210727") -> pd.DataFrame:
     return temp_df
 
 
-def stock_margin_detail_szse(date: str = "20210728") -> pd.DataFrame:
+def stock_margin_detail_szse(date: str = "20220118") -> pd.DataFrame:
     """
-    深证证券交易所-融资融券数据-融资融券明细
+    深证证券交易所-融资融券数据-融资融券交易明细
     http://www.szse.cn/disclosure/margin/margin/index.html
     :param date: 交易日期
     :type date: str
     :return: 融资融券明细
     :rtype: pandas.DataFrame
     """
-    url = "http://www.szse.cn/api/report/ShowReport/data"
+    url = "http://www.szse.cn/api/report/ShowReport"
     params = {
-        "SHOWTYPE": "JSON",
-        "CATALOGID": "1837_xxpl",
-        "txtDate": "-".join([date[:4], date[4:6], date[6:]]),
-        "tab2PAGENO": "1",
-        "random": "0.7425245522795993",
+        'SHOWTYPE': 'xlsx',
+        'CATALOGID': '1837_xxpl',
+        'txtDate': "-".join([date[:4], date[4:6], date[6:]]),
+        'tab2PAGENO': '1',
+        'random': '0.24279342734085696',
+        'TABKEY': 'tab2',
     }
     headers = {
         "Referer": "http://www.szse.cn/disclosure/margin/margin/index.html",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36",
     }
     r = requests.get(url, params=params, headers=headers)
-    data_json = r.json()
-    total_page = data_json[1]["metadata"]["pagecount"]
-    big_df = pd.DataFrame()
-    for page in tqdm(range(1, total_page + 1), leave=False):
-        params.update({"tab2PAGENO": page})
-        r = requests.get(url, params=params, headers=headers)
-        data_json = r.json()
-        temp_df = pd.DataFrame(data_json[1]["data"])
-        big_df = big_df.append(temp_df, ignore_index=True)
-    big_df.columns = [
+    import warnings
+    with warnings.catch_warnings(record=True):
+        warnings.simplefilter("always")
+        temp_df = pd.read_excel(r.content, engine="openpyxl", dtype={'证券代码': str})
+    temp_df.columns = [
         "证券代码",
         "证券简称",
         "融资买入额",
@@ -143,20 +139,20 @@ def stock_margin_detail_szse(date: str = "20210728") -> pd.DataFrame:
         "融券余额",
         "融资融券余额",
     ]
-    big_df["证券简称"] = big_df["证券简称"].str.replace("&nbsp;", "")
-    big_df["融资买入额"] = big_df["融资买入额"].str.replace(",", "")
-    big_df["融资买入额"] = pd.to_numeric(big_df["融资买入额"])
-    big_df["融资余额"] = big_df["融资余额"].str.replace(",", "")
-    big_df["融资余额"] = pd.to_numeric(big_df["融资余额"])
-    big_df["融券卖出量"] = big_df["融券卖出量"].str.replace(",", "")
-    big_df["融券卖出量"] = pd.to_numeric(big_df["融券卖出量"])
-    big_df["融券余量"] = big_df["融券余量"].str.replace(",", "")
-    big_df["融券余量"] = pd.to_numeric(big_df["融券余量"])
-    big_df["融券余额"] = big_df["融券余额"].str.replace(",", "")
-    big_df["融券余额"] = pd.to_numeric(big_df["融券余额"])
-    big_df["融资融券余额"] = big_df["融资融券余额"].str.replace(",", "")
-    big_df["融资融券余额"] = pd.to_numeric(big_df["融资融券余额"])
-    return big_df
+    temp_df["证券简称"] = temp_df["证券简称"].str.replace("&nbsp;", "")
+    temp_df["融资买入额"] = temp_df["融资买入额"].str.replace(",", "")
+    temp_df["融资买入额"] = pd.to_numeric(temp_df["融资买入额"])
+    temp_df["融资余额"] = temp_df["融资余额"].str.replace(",", "")
+    temp_df["融资余额"] = pd.to_numeric(temp_df["融资余额"])
+    temp_df["融券卖出量"] = temp_df["融券卖出量"].str.replace(",", "")
+    temp_df["融券卖出量"] = pd.to_numeric(temp_df["融券卖出量"])
+    temp_df["融券余量"] = temp_df["融券余量"].str.replace(",", "")
+    temp_df["融券余量"] = pd.to_numeric(temp_df["融券余量"])
+    temp_df["融券余额"] = temp_df["融券余额"].str.replace(",", "")
+    temp_df["融券余额"] = pd.to_numeric(temp_df["融券余额"])
+    temp_df["融资融券余额"] = temp_df["融资融券余额"].str.replace(",", "")
+    temp_df["融资融券余额"] = pd.to_numeric(temp_df["融资融券余额"])
+    return temp_df
 
 
 if __name__ == "__main__":
