12af7bcb68a3cc50c93e85ad2653ba7b0d8e25cd
ak_quant <akshare.akfamily@gmail.com>
2022-01-20 16:40:40 +0800

    feat(covid_19_risk_area): add covid_19_risk_area interface
    add covid_19_risk_area interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1780,9 +1780,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.3.56 fix: fix stock_hsgt_board_rank_em interface
 1.3.57 fix: fix futures_main_sina interface
 1.3.58 fix: fix stock_margin_detail_szse interface
+1.3.59 add: add covid_19_risk_area interface
 """
 
-__version__ = "1.3.58"
+__version__ = "1.3.59"
 __author__ = "Albert King"
 
 import sys
--- docs/changelog.md
+++ docs/changelog.md
@@ -2973,4 +2973,6 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.3.57 fix: fix futures_main_sina interface
 
 1.3.58 fix: fix stock_margin_detail_szse interface
+
+1.3.59 add: add covid_19_risk_area interface
 ```
