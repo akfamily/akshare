40a1da646ec4e93a4a484d269ed595b2ff621733
ak_quant <akshare.akfamily@gmail.com>
2022-01-22 16:28:57 +0800

    fix(futures_foreign_commodity_realtime): fix futures_foreign_commodity_realtime interface
    fix futures_foreign_commodity_realtime interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1785,9 +1785,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.3.61 fix: fix futures_zh_spot interface
 1.3.62 fix: fix option_sina_sse_spot_price interface
 1.3.63 fix: fix option_sina_finance_minute interface
+1.3.64 fix: fix futures_foreign_commodity_realtime interface
 """
 
-__version__ = "1.3.63"
+__version__ = "1.3.64"
 __author__ = "Albert King"
 
 import sys
