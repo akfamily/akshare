d51eb496e527a81199b4e715e26705a4e0a55238
ak_quant <akshare.akfamily@gmail.com>
2022-01-26 20:38:33 +0800

    fix(build.yml): fix build.yml
    fix build.yml

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1807,9 +1807,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.3.83 fix: fix dockerfile-jupyter
 1.3.84 fix: fix release_and_deploy
 1.3.85 fix: fix release_and_deploy.yml
+1.3.86 fix: fix build.yml
 """
 
-__version__ = "1.3.85"
+__version__ = "1.3.86"
 __author__ = "Albert King"
 
 import sys
