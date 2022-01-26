9d2a68ddf5064494ffc2b824ee86f6f79914f55a
ak_quant <akshare.akfamily@gmail.com>
2022-01-26 13:14:47 +0800

    fix(sw_index_daily_indicator): fix sw_index_daily_indicator interface
    fix sw_index_daily_indicator interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1798,9 +1798,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.3.74 fix: fix option_value_analysis_em interface
 1.3.75 add: add option_risk_analysis_em interface
 1.3.76 add: add option_premium_analysis_em interface
+1.3.77 fix: fix sw_index_daily_indicator interface
 """
 
-__version__ = "1.3.76"
+__version__ = "1.3.77"
 __author__ = "Albert King"
 
 import sys
