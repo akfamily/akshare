e9dc086764ab6bf6f2128abc4ba56057b0f42e04
ak_quant <akshare.akfamily@gmail.com>
2022-01-26 20:56:50 +0800

    fix(master_develop_check.yml): fix master_develop_check.yml
    fix master_develop_check.yml

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1809,9 +1809,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.3.85 fix: fix release_and_deploy.yml
 1.3.86 fix: fix master_develop_check.yml
 1.3.87 fix: fix master_develop_check.yml
+1.3.88 fix: fix master_develop_check.yml
 """
 
-__version__ = "1.3.87"
+__version__ = "1.3.88"
 __author__ = "Albert King"
 
 import sys
--- docs/changelog.md
+++ docs/changelog.md
@@ -3031,4 +3031,6 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.3.86 fix: fix build.yml
 
 1.3.87 fix: fix master_develop_check.yml
+
+1.3.88 fix: fix master_develop_check.yml
 ```
