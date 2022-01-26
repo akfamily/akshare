1fb22a3af4a6b60a8d0d60e28e0eb32c81c6206d
ak_quant <akshare.akfamily@gmail.com>
2022-01-26 17:30:12 +0800

    feat(dockerfile): add dockerfile for aktools interface
    add dockerfile for aktools interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1800,9 +1800,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.3.76 add: add option_premium_analysis_em interface
 1.3.77 fix: fix sw_index_daily_indicator interface
 1.3.78 fix: fix forbes_rank interface
+1.3.79 add: add dockerfile for aktools interface
 """
 
-__version__ = "1.3.78"
+__version__ = "1.3.79"
 __author__ = "Albert King"
 
 import sys
--- docs/changelog.md
+++ docs/changelog.md
@@ -3013,4 +3013,6 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.3.77 fix: fix sw_index_daily_indicator interface
 
 1.3.78 fix: fix forbes_rank interface
+
+1.3.79 add: add dockerfile for aktools interface
 ```
