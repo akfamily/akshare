ff26116493d323df320226d1191a3193fa973322
ak_quant <akshare.akfamily@gmail.com>
2022-02-02 23:24:46 +0800

    fix(bond_china_close_return): fix bond_china_close_return interface
    fix bond_china_close_return interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1817,9 +1817,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.3.93 fix: fix docs
 1.3.94 fix: fix akdocker.md
 1.3.95 fix: fix covid_19_risk_area interface
+1.3.96 fix: fix bond_china_close_return interface
 """
 
-__version__ = "1.3.95"
+__version__ = "1.3.96"
 __author__ = "Albert King"
 
 import sys
--- docs/changelog.md
+++ docs/changelog.md
@@ -3047,4 +3047,6 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.3.94 fix: fix akdocker.md
 
 1.3.95 fix: fix covid_19_risk_area interface
+
+1.3.96 fix: fix bond_china_close_return interface
 ```
