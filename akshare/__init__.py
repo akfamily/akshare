72993549e7b470893bf535ad1c342f51fa31bfda
ak_quant <akshare.akfamily@gmail.com>
2022-03-04 16:22:33 +0800

    fix(stock_hot_rank_relate_em): fix stock_hot_rank_relate_em interface
    fix stock_hot_rank_relate_em interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1874,9 +1874,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.4.51 add: add stock_hot_keyword_em interface
 1.4.52 add: add stock_hot_rank_latest_em interface
 1.4.53 add: add stock_hot_rank_relate_em interface
+1.4.54 fix: fix stock_hot_rank_relate_em interface
 """
 
-__version__ = "1.4.53"
+__version__ = "1.4.54"
 __author__ = "AKFamily"
 
 import sys
