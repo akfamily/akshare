01c17cd3f95070bb65f2fc8a1f22ef7645c37730
ak_quant <akshare.akfamily@gmail.com>
2022-03-15 16:32:41 +0800

    fix(crypto_hist): fix crypto_hist interface
    fix crypto_hist interface

--- akshare/crypto/crypto_hist_investing.py
+++ akshare/crypto/crypto_hist_investing.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2020/10/23 13:52
+Date: 2022/3/15 13:52
 Desc: 加密货币
 https://cn.investing.com/crypto/currencies
 高频数据
@@ -12,11 +12,10 @@ import re
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
-pd.set_option("mode.chained_assignment", None)
 
-
-def crypto_name_map() -> dict:
+def crypto_name_url_table() -> pd.DataFrame:
     """
     加密货币名称
     https://cn.investing.com/crypto/ethereum/historical-data
@@ -28,25 +27,35 @@ def crypto_name_map() -> dict:
         "X-Requested-With": "XMLHttpRequest",
     }
     url = "https://cn.investing.com/crypto/Service/LoadCryptoCurrencies"
+    big_df = pd.DataFrame()
     payload = {"lastRowId": "0", 'page': '1'}
-    r = requests.post(url, data=payload, headers=headers)
-    soup = BeautifulSoup(r.text, "lxml")
-    crypto_url_list = [
-        "https://cn.investing.com/" + item["href"].split("/")[1].strip(r"\\") + '/' + item["href"].split("/")[2][:-2]
-        + "/historical-data"
-        for item in soup.find_all("a")
-        if "-" not in item["href"]
-    ]
-    crypto_name_list = [
-        item["href"].split("/")[2][:-2]
-        for item in soup.find_all("a")
-        if "-" not in item["href"]
-    ]
-    name_url_dict = dict(zip(crypto_name_list, crypto_url_list))
-    temp_df = pd.DataFrame.from_dict(name_url_dict, orient="index")
-    temp_df.reset_index(inplace=True)
-    temp_df.columns = ["name", "url"]
-    return temp_df
+    for page in tqdm(range(1, 40), leave=False):
+        try:
+            payload.update({
+                "lastRowId": (page-1)*100,
+                'page': page
+            })
+            r = requests.post(url, data=payload, headers=headers)
+            soup = BeautifulSoup(r.text, "lxml")
+            crypto_url_list = [
+                "https://cn.investing.com/" + item["href"].split("/")[1].strip(r"\\") + '/' + item["href"].split("/")[2][:-2]
+                + "/historical-data"
+                for item in soup.find_all("a")
+                if "-" not in item["href"]
+            ]
+            crypto_name_list = [
+                item["href"].split("/")[2][:-2]
+                for item in soup.find_all("a")
+                if "-" not in item["href"]
+            ]
+            name_url_dict = dict(zip(crypto_name_list, crypto_url_list))
+            temp_df = pd.DataFrame.from_dict(name_url_dict, orient="index")
+            temp_df.reset_index(inplace=True)
+            temp_df.columns = ["name", "url"]
+            big_df = pd.concat([big_df, temp_df], ignore_index=True)
+        except:
+            break
+    return big_df
 
 
 def crypto_hist(
@@ -76,7 +85,7 @@ def crypto_hist(
     period_map = {"每日": "Daily", "每周": "Weekly", "每月": "Monthly"}
     start_date = "/".join([start_date[:4], start_date[4:6], start_date[6:]])
     end_date = "/".join([end_date[:4], end_date[4:6], end_date[6:]])
-    name_url_df = crypto_name_map()
+    name_url_df = crypto_name_url_table()
     temp_url = name_url_df[name_url_df["name"] == symbol]["url"].values[0]
     res = requests.post(temp_url, headers=headers)
     soup = BeautifulSoup(res.text, "lxml")
@@ -96,7 +105,7 @@ def crypto_hist(
     }
     r = requests.post(url, data=payload, headers=headers)
     temp_df = pd.read_html(r.text)[0]
-    df_data = temp_df
+    df_data = temp_df.copy()
     if period == "每月":
         df_data.index = pd.to_datetime(df_data["日期"], format="%Y年%m月")
     else:
@@ -138,14 +147,33 @@ def crypto_hist(
         )
     )
     del df_data["日期"]
+    df_data.reset_index(inplace=True)
+    df_data = df_data[[
+        "日期",
+        "收盘",
+        "开盘",
+        "高",
+        "低",
+        "交易量",
+        "涨跌幅",
+    ]]
+    df_data['日期'] = pd.to_datetime(df_data['日期']).dt.date
+    df_data['收盘'] = pd.to_numeric(df_data['收盘'])
+    df_data['开盘'] = pd.to_numeric(df_data['开盘'])
+    df_data['高'] = pd.to_numeric(df_data['高'])
+    df_data['低'] = pd.to_numeric(df_data['低'])
+    df_data['交易量'] = pd.to_numeric(df_data['交易量'])
+    df_data['涨跌幅'] = pd.to_numeric(df_data['涨跌幅'])
+    df_data.sort_values('日期', inplace=True)
+    df_data.reset_index(inplace=True, drop=True)
     return df_data
 
 
 if __name__ == "__main__":
-    crypto_name_map_df = crypto_name_map()
-    print(crypto_name_map_df)
+    crypto_name_url_table_df = crypto_name_url_table()
+    print(crypto_name_url_table_df)
 
     crypto_hist_df = crypto_hist(
-        symbol="bitcoin", period="每日", start_date="20151020", end_date="20210407"
+        symbol="bitcoin", period="每日", start_date="20151020", end_date="20220315"
     )
     print(crypto_hist_df)
