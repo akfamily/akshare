6e45a9ebd686706325b9a2192f7c3346f862bc45
ak_quant <akshare.akfamily@gmail.com>
2022-05-13 12:20:59 +0800

    fix(stock_notice_report): fix stock_notice_report interface
    fix stock_notice_report interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -1992,9 +1992,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.5.70 fix: fix stock_zh_a_spot interface
 1.5.71 fix: fix import path problem
 1.5.72 add: add news_economic_baidu interface
+1.5.73 fix: fix stock_notice_report interface
 """
 
-__version__ = "1.5.72"
+__version__ = "1.5.73"
 __author__ = "AKFamily"
 
 import sys
