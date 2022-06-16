e67ca6074c8676df5a2ea024b134924338c97a80
ak_quant <akshare.akfamily@gmail.com>
2022-06-16 14:50:07 +0800

    fix(stock_zh_a_spot_em): fix stock_zh_a_spot_em interface
    fix stock_zh_a_spot_em interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2036,9 +2036,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.6.15 fix: fix bond_zh_cov_info interface
 1.6.16 fix: fix baidu_search_index interface
 1.6.17 fix: fix stock_new_a_spot_em interface
+1.6.18 fix: fix stock_zh_a_spot_em interface
 """
 
-__version__ = "1.6.17"
+__version__ = "1.6.18"
 __author__ = "AKFamily"
 
 import sys
