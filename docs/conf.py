398e6ca927dbe72612ded4019d29967668899460
ak_quant <akshare.akfamily@gmail.com>
2022-07-15 23:44:15 +0800

    docs(docs): docs
    docs

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,7 @@ release = get_version_string()
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["recommonmark", "sphinx_markdown_tables", "sphinx_rtd_theme"]
+extensions = ["recommonmark", "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 
