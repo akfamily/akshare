617c9537f752988e14a17362f149cdac729055d4
ak_quant <akshare.akfamily@gmail.com>
2022-08-15 16:59:02 +0800

    fix(index_vix): fix index_vix interface
    fix index_vix interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2111,9 +2111,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.6.90 fix: fix macro_china_cpi_yearly interface
 1.6.91 fix: fix forbes_rank interface
 1.6.92 fix: fix hurun_rank interface
+1.6.93 fix: fix index_vix interface
 """
 
-__version__ = "1.6.92"
+__version__ = "1.6.93"
 __author__ = "AKFamily"
 
 import sys
