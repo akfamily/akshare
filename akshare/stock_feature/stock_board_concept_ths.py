2c90fbf38683b8303f51c263bff7177e904a4697
jindaxiang <jindaxiang@163.com>
2022-08-17 23:43:13 +0800

    docs(docs): docs
    docs

--- akshare/stock_feature/stock_board_concept_ths.py
+++ akshare/stock_feature/stock_board_concept_ths.py
@@ -383,7 +383,7 @@ if __name__ == "__main__":
     print(stock_board_concept_name_ths_df)
 
     stock_board_concept_cons_ths_df = stock_board_concept_cons_ths(
-        symbol="WiFi 6"
+        symbol="苹果概念"
     )
     print(stock_board_concept_cons_ths_df)
 
