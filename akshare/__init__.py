9f1a481afee05f221cf076cbeadb54e2d01a675b
ak_quant <akshare.akfamily@gmail.com>
2022-08-29 15:02:33 +0800

    fix(stock_repurchase_em): fix stock_repurchase_em interface
    fix stock_repurchase_em interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2129,9 +2129,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.7.9 add: add index_market_representation_hist_sw interface
 1.7.10 fix: fix option_czce_daily interface
 1.7.11 fix: fix bond_cash_summary_sse interface
+1.7.12 fix: fix stock_repurchase_em interface
 """
 
-__version__ = "1.7.11"
+__version__ = "1.7.12"
 __author__ = "AKFamily"
 
 import sys
--- akshare/stock/stock_repurchase_em.py
+++ akshare/stock/stock_repurchase_em.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 # !/usr/bin/env python
 """
-Date: 2022/5/10 17:30
+Date: 2022/8/29 14:20
 Desc: 东方财富网-数据中心-股票回购-股票回购数据
 https://data.eastmoney.com/gphg/hglist.html
 """
@@ -17,15 +17,14 @@ def stock_repurchase_em() -> pd.DataFrame:
     :return: 股票回购数据
     :rtype: pandas.DataFrame
     """
-    url = "https://datacenter-web.eastmoney.com/api/data/get"
+    url = "https://datacenter-web.eastmoney.com/api/data/v1/get"
     params = {
-        "st": "dim_scode",
-        "sr": "-1",
-        "ps": "500",
-        "p": "1",
-        # "type": "RPTA_WEB_GETHGLIST",
-        "type": "RPTA_WEB_GETHGLIST_NEW",
-        "sty": "ALL",
+        "sortColumns": "UPD,DIM_DATE,DIM_SCODE",
+        "sortTypes": "-1,-1,-1",
+        "pageSize": "500",
+        "pageNumber": "1",
+        "reportName": "RPTA_WEB_GETHGLIST_NEW",
+        "columns": "ALL",
         "source": "WEB",
     }
     r = requests.get(url, params=params)
@@ -33,7 +32,7 @@ def stock_repurchase_em() -> pd.DataFrame:
     total_page = data_json["result"]["pages"]
     big_df = pd.DataFrame()
     for page in tqdm(range(1, int(total_page) + 1), leave=False):
-        params.update({"p": page})
+        params.update({"pageNumber": page})
         r = requests.get(url, params=params)
         data_json = r.json()
         temp_df = pd.DataFrame(data_json["result"]["data"])
@@ -43,16 +42,20 @@ def stock_repurchase_em() -> pd.DataFrame:
             "DIM_SCODE": "股票代码",
             "SECURITYSHORTNAME": "股票简称",
             "NEWPRICE": "最新价",
-            "JHJG_VAG": "计划回购价格区间(元)",
-            "JHSL_VAG": "计划回购数量区间(股)",
-            "ZJSZBL": "占公告前一日总股本比例(%)",
-            "JHJE_VAG": "计划回购金额区间(元)",
-            "REPURSTARTDATE": "回购起始时间",
+            "REPURPRICECAP": "计划回购价格区间",
+            "REPURNUMLOWER": "计划回购数量区间-下限",
+            "REPURNUMCAP": "计划回购数量区间-上限",
+            "ZSZXX": "占公告前一日总股本比例-下限",
+            "ZSZSX": "占公告前一日总股本比例-上限",
+            "JEXX": "计划回购金额区间-下限",
+            "JESX": "计划回购金额区间-上限",
+            "DIM_TRADEDATE": "回购起始时间",
             "REPURPROGRESS": "实施进度",
-            "HGJG_VAG": "已回购股份价格区间(元)",
-            "REPURNUM": "已回购股份数量(股)",
-            "REPURAMOUNT": "已回购金额(元)",
-            "UPD": "最新公告日期"
+            "REPURPRICELOWER1": "已回购股份价格区间-下限",
+            "REPURPRICECAP1": "已回购股份价格区间-上限",
+            "REPURNUM": "已回购股份数量",
+            "REPURAMOUNT": "已回购金额",
+            "UPDATEDATE": "最新公告日期",
         },
         axis="columns",
         inplace=True,
@@ -62,16 +65,20 @@ def stock_repurchase_em() -> pd.DataFrame:
             "股票代码",
             "股票简称",
             "最新价",
-            "计划回购价格区间(元)",
-            "计划回购数量区间(股)",
-            "占公告前一日总股本比例(%)",
-            "计划回购金额区间(元)",
+            "计划回购价格区间",
+            "计划回购数量区间-下限",
+            "计划回购数量区间-上限",
+            "占公告前一日总股本比例-下限",
+            "占公告前一日总股本比例-上限",
+            "计划回购金额区间-下限",
+            "计划回购金额区间-上限",
             "回购起始时间",
             "实施进度",
-            "已回购股份价格区间(元)",
-            "已回购股份数量(股)",
-            "已回购金额(元)",
-            "最新公告日期"
+            "已回购股份价格区间-下限",
+            "已回购股份价格区间-上限",
+            "已回购股份数量",
+            "已回购金额",
+            "最新公告日期",
         ]
     ]
     big_df.reset_index(inplace=True)
@@ -94,6 +101,18 @@ def stock_repurchase_em() -> pd.DataFrame:
     big_df["实施进度"] = big_df["实施进度"].map(process_map)
     big_df["回购起始时间"] = pd.to_datetime(big_df["回购起始时间"]).dt.date
     big_df["最新公告日期"] = pd.to_datetime(big_df["最新公告日期"]).dt.date
+    big_df["最新价"] = pd.to_numeric(big_df["最新价"])
+    big_df["计划回购价格区间"] = pd.to_numeric(big_df["计划回购价格区间"])
+    big_df["计划回购数量区间-下限"] = pd.to_numeric(big_df["计划回购数量区间-下限"])
+    big_df["计划回购数量区间-上限"] = pd.to_numeric(big_df["计划回购数量区间-上限"])
+    big_df["占公告前一日总股本比例-上限"] = pd.to_numeric(big_df["占公告前一日总股本比例-上限"])
+    big_df["占公告前一日总股本比例-下限"] = pd.to_numeric(big_df["占公告前一日总股本比例-下限"])
+    big_df["计划回购金额区间-上限"] = pd.to_numeric(big_df["计划回购金额区间-上限"])
+    big_df["计划回购金额区间-下限"] = pd.to_numeric(big_df["计划回购金额区间-下限"])
+    big_df["已回购股份价格区间-下限"] = pd.to_numeric(big_df["已回购股份价格区间-下限"])
+    big_df["已回购股份价格区间-上限"] = pd.to_numeric(big_df["已回购股份价格区间-上限"])
+    big_df["已回购股份数量"] = pd.to_numeric(big_df["已回购股份数量"])
+    big_df["已回购金额"] = pd.to_numeric(big_df["已回购金额"])
     return big_df
 
 
