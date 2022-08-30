6dc9eedddcc87c43ca107eb1ab84fb38936adb74
ak_quant <akshare.akfamily@gmail.com>
2022-08-30 16:24:15 +0800

    fix(stock_balance_sheet_by_yearly_em): fix stock_balance_sheet_by_yearly_em interface
    fix stock_balance_sheet_by_yearly_em interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2130,9 +2130,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.7.10 fix: fix option_czce_daily interface
 1.7.11 fix: fix bond_cash_summary_sse interface
 1.7.12 fix: fix stock_repurchase_em interface
+1.7.13 fix: fix stock_balance_sheet_by_yearly_em interface
 """
 
-__version__ = "1.7.12"
+__version__ = "1.7.13"
 __author__ = "AKFamily"
 
 import sys
