dbff9e471c0b5ac918fad1c554cd881f22df76bb
jindaxiang <jindaxiang@163.com>
2022-10-02 19:15:32 +0800

    feat(stock_sgt_reference_exchange_rate_szse): add stock_sgt_reference_exchange_rate_szse interface
    add stock_sgt_reference_exchange_rate_szse interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2159,9 +2159,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.7.39 add: add stock_sgt_settlement_exchange_rate_szse interface
 1.7.40 add: add stock_sgt_settlement_exchange_rate_sse interface
 1.7.41 add: add stock_sgt_reference_exchange_rate_sse interface
+1.7.42 add: add stock_sgt_reference_exchange_rate_szse interface
 """
 
-__version__ = "1.7.41"
+__version__ = "1.7.42"
 __author__ = "AKFamily"
 
 import sys
