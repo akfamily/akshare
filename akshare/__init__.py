2876dd42d48e8a586b0ae2b2e3247771eb7abc34
jindaxiang <jindaxiang@163.com>
2022-10-30 22:05:59 +0800

    fix(fortune_hurun.py): fix hurun_rank interface
    fix hurun_rank interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2185,9 +2185,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.7.65 fix: fix futures_rule interface
 1.7.66 add: add index_realtime_sw interface
 1.7.67 fix: fix xincaifu_rank interface
+1.7.68 fix: fix hurun_rank interface
 """
 
-__version__ = "1.7.67"
+__version__ = "1.7.68"
 __author__ = "AKFamily"
 
 import sys
