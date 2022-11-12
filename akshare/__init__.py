687eccb63a85cd2cda8140f9c42f13d75304b213
jindaxiang <jindaxiang@163.com>
2022-11-12 18:09:32 +0800

    fix(macro_uk.py): fix macro_uk interface
    fix macro_uk interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2204,9 +2204,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.7.84 add: add bond_info_cm interface
 1.7.85 fix: fix stock_board_industry_hist_em interface
 1.7.86 fix: fix bond_info_cm interface
+1.7.87 fix: fix macro_uk interface
 """
 
-__version__ = "1.7.86"
+__version__ = "1.7.87"
 __author__ = "AKFamily"
 
 import sys
