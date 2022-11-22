dfc5593327ab99c7b219acf4da1ff218d0aaf9b9
jindaxiang <jindaxiang@163.com>
2022-11-22 12:33:18 +0800

    fix(stock_a_ttm_lyr.py): fix stock_a_ttm_lyr interface
    fix stock_a_ttm_lyr interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2225,9 +2225,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.8.6 fix: fix stock_market_activity_legu interface
 1.8.7 fix: fix stock_a_below_net_asset_statistics interface
 1.8.8 fix: fix macro_china_gdp interface
+1.8.9 fix: fix stock_a_ttm_lyr interface
 """
 
-__version__ = "1.8.8"
+__version__ = "1.8.9"
 __author__ = "AKFamily"
 
 import sys
