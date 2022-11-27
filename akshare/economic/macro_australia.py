961006b7ae20c779e5c6dadbfc029bd869e284be
jindaxiang <jindaxiang@163.com>
2022-11-27 20:51:59 +0800

    fix(macro_china.py): fix macro_china_fdi interface
    fix macro_china_fdi interface

--- akshare/economic/macro_australia.py
+++ akshare/economic/macro_australia.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2021/12/15 15:26
+Date: 2022/11/27 20:26
 Desc: 东方财富-经济数据-澳大利亚
-http://data.eastmoney.com/cjsj/foreign_5_0.html
+https://data.eastmoney.com/cjsj/foreign_5_0.html
 """
 import pandas as pd
 import requests
@@ -15,36 +15,49 @@ from akshare.utils import demjson
 def macro_australia_retail_rate_monthly() -> pd.DataFrame:
     """
     东方财富-经济数据-澳大利亚-零售销售月率
-    http://data.eastmoney.com/cjsj/foreign_5_0.html
+    https://data.eastmoney.com/cjsj/foreign_5_0.html
     :return: 零售销售月率
     :rtype: pandas.DataFrame
     """
-    url = "http://datainterface.eastmoney.com/EM_DataCenter/JS.aspx"
+    url = "https://datacenter-web.eastmoney.com/api/data/v1/get"
     params = {
-        "type": "GJZB",
-        "sty": "HKZB",
-        "js": "({data:[(x)],pages:(pc)})",
+        "reportName": "RPT_ECONOMICVALUE_AUSTRALIA",
+        "columns": "ALL",
+        "filter": '(INDICATOR_ID="EMG00152903")',
+        "pageNumber": "1",
+        "pageSize": "2000",
+        "sortColumns": "REPORT_DATE",
+        "sortTypes": "-1",
+        "source": "WEB",
+        "client": "WEB",
         "p": "1",
-        "ps": "2000",
-        "mkt": "5",
-        "stat": "0",
         "pageNo": "1",
         "pageNum": "1",
-        "_": "1625474966006",
+        "_": "1669047266881",
     }
     r = requests.get(url, params=params)
-    data_text = r.text
-    data_json = demjson.decode(data_text[1:-1])
-    temp_df = pd.DataFrame([item.split(",") for item in data_json["data"]])
+    data_json = r.json()
+    temp_df = pd.DataFrame(data_json["result"]["data"])
     temp_df.columns = [
+        "-",
+        "-",
+        "-",
+        "时间",
+        "-",
+        "发布日期",
+        "现值",
+        "前值",
+
+
+    ]
+    temp_df = temp_df[[
         "时间",
         "前值",
         "现值",
         "发布日期",
-    ]
-    temp_df['时间'] = pd.to_datetime(temp_df['时间']).dt.date
-    temp_df["前值"] = pd.to_numeric(temp_df["前值"])
-    temp_df["现值"] = pd.to_numeric(temp_df["现值"])
+    ]]
+    temp_df["前值"] = pd.to_numeric(temp_df["前值"], errors="coerce")
+    temp_df["现值"] = pd.to_numeric(temp_df["现值"], errors="coerce")
     temp_df['发布日期'] = pd.to_datetime(temp_df['发布日期']).dt.date
     return temp_df
 
@@ -53,36 +66,48 @@ def macro_australia_retail_rate_monthly() -> pd.DataFrame:
 def macro_australia_trade() -> pd.DataFrame:
     """
     东方财富-经济数据-澳大利亚-贸易帐
-    http://data.eastmoney.com/cjsj/foreign_5_1.html
+    https://data.eastmoney.com/cjsj/foreign_5_1.html
     :return: 贸易帐
     :rtype: pandas.DataFrame
     """
-    url = "http://datainterface.eastmoney.com/EM_DataCenter/JS.aspx"
+    url = "https://datacenter-web.eastmoney.com/api/data/v1/get"
     params = {
-        "type": "GJZB",
-        "sty": "HKZB",
-        "js": "({data:[(x)],pages:(pc)})",
+        "reportName": "RPT_ECONOMICVALUE_AUSTRALIA",
+        "columns": "ALL",
+        "filter": '(INDICATOR_ID="EMG00152793")',
+        "pageNumber": "1",
+        "pageSize": "2000",
+        "sortColumns": "REPORT_DATE",
+        "sortTypes": "-1",
+        "source": "WEB",
+        "client": "WEB",
         "p": "1",
-        "ps": "2000",
-        "mkt": "5",
-        "stat": "1",
         "pageNo": "1",
         "pageNum": "1",
-        "_": "1625474966006",
+        "_": "1669047266881",
     }
     r = requests.get(url, params=params)
-    data_text = r.text
-    data_json = demjson.decode(data_text[1:-1])
-    temp_df = pd.DataFrame([item.split(",") for item in data_json["data"]])
+    data_json = r.json()
+    temp_df = pd.DataFrame(data_json["result"]["data"])
     temp_df.columns = [
+        "-",
+        "-",
+        "-",
+        "时间",
+        "-",
+        "发布日期",
+        "现值",
+        "前值",
+
+    ]
+    temp_df = temp_df[[
         "时间",
         "前值",
         "现值",
         "发布日期",
-    ]
-    temp_df['时间'] = pd.to_datetime(temp_df['时间']).dt.date
-    temp_df["前值"] = pd.to_numeric(temp_df["前值"])
-    temp_df["现值"] = pd.to_numeric(temp_df["现值"])
+    ]]
+    temp_df["前值"] = pd.to_numeric(temp_df["前值"], errors="coerce")
+    temp_df["现值"] = pd.to_numeric(temp_df["现值"], errors="coerce")
     temp_df['发布日期'] = pd.to_datetime(temp_df['发布日期']).dt.date
     return temp_df
 
@@ -91,34 +116,48 @@ def macro_australia_trade() -> pd.DataFrame:
 def macro_australia_unemployment_rate() -> pd.DataFrame:
     """
     东方财富-经济数据-澳大利亚-失业率
-    http://data.eastmoney.com/cjsj/foreign_5_2.html
+    https://data.eastmoney.com/cjsj/foreign_5_2.html
     :return: 失业率
     :rtype: pandas.DataFrame
     """
-    url = "http://datainterface.eastmoney.com/EM_DataCenter/JS.aspx"
+    url = "https://datacenter-web.eastmoney.com/api/data/v1/get"
     params = {
-        "type": "GJZB",
-        "sty": "HKZB",
-        "js": "({data:[(x)],pages:(pc)})",
+        "reportName": "RPT_ECONOMICVALUE_AUSTRALIA",
+        "columns": "ALL",
+        "filter": '(INDICATOR_ID="EMG00101141")',
+        "pageNumber": "1",
+        "pageSize": "2000",
+        "sortColumns": "REPORT_DATE",
+        "sortTypes": "-1",
+        "source": "WEB",
+        "client": "WEB",
         "p": "1",
-        "ps": "2000",
-        "mkt": "5",
-        "stat": "2",
-        "_": "1625474966006",
+        "pageNo": "1",
+        "pageNum": "1",
+        "_": "1669047266881",
     }
     r = requests.get(url, params=params)
-    data_text = r.text
-    data_json = demjson.decode(data_text[1:-1])
-    temp_df = pd.DataFrame([item.split(",") for item in data_json["data"]])
+    data_json = r.json()
+    temp_df = pd.DataFrame(data_json["result"]["data"])
     temp_df.columns = [
+        "-",
+        "-",
+        "-",
+        "时间",
+        "-",
+        "发布日期",
+        "现值",
+        "前值",
+
+    ]
+    temp_df = temp_df[[
         "时间",
         "前值",
         "现值",
         "发布日期",
-    ]
-    temp_df['时间'] = pd.to_datetime(temp_df['时间']).dt.date
-    temp_df["前值"] = pd.to_numeric(temp_df["前值"])
-    temp_df["现值"] = pd.to_numeric(temp_df["现值"])
+    ]]
+    temp_df["前值"] = pd.to_numeric(temp_df["前值"], errors="coerce")
+    temp_df["现值"] = pd.to_numeric(temp_df["现值"], errors="coerce")
     temp_df['发布日期'] = pd.to_datetime(temp_df['发布日期']).dt.date
     return temp_df
 
@@ -127,36 +166,48 @@ def macro_australia_unemployment_rate() -> pd.DataFrame:
 def macro_australia_ppi_quarterly() -> pd.DataFrame:
     """
     东方财富-经济数据-澳大利亚-生产者物价指数季率
-    http://data.eastmoney.com/cjsj/foreign_5_3.html
+    https://data.eastmoney.com/cjsj/foreign_5_3.html
     :return: 生产者物价指数季率
     :rtype: pandas.DataFrame
     """
-    url = "http://datainterface.eastmoney.com/EM_DataCenter/JS.aspx"
+    url = "https://datacenter-web.eastmoney.com/api/data/v1/get"
     params = {
-        "type": "GJZB",
-        "sty": "HKZB",
-        "js": "({data:[(x)],pages:(pc)})",
+        "reportName": "RPT_ECONOMICVALUE_AUSTRALIA",
+        "columns": "ALL",
+        "filter": '(INDICATOR_ID="EMG00152722")',
+        "pageNumber": "1",
+        "pageSize": "2000",
+        "sortColumns": "REPORT_DATE",
+        "sortTypes": "-1",
+        "source": "WEB",
+        "client": "WEB",
         "p": "1",
-        "ps": "2000",
-        "mkt": "5",
-        "stat": "3",
-        'pageNo': '1',
-        'pageNum': '1',
-        "_": "1625474966006",
+        "pageNo": "1",
+        "pageNum": "1",
+        "_": "1669047266881",
     }
     r = requests.get(url, params=params)
-    data_text = r.text
-    data_json = demjson.decode(data_text[1:-1])
-    temp_df = pd.DataFrame([item.split(",") for item in data_json["data"]])
+    data_json = r.json()
+    temp_df = pd.DataFrame(data_json["result"]["data"])
     temp_df.columns = [
+        "-",
+        "-",
+        "-",
+        "时间",
+        "-",
+        "发布日期",
+        "现值",
+        "前值",
+
+    ]
+    temp_df = temp_df[[
         "时间",
         "前值",
         "现值",
         "发布日期",
-    ]
-    temp_df['时间'] = pd.to_datetime(temp_df['时间']).dt.date
-    temp_df["前值"] = pd.to_numeric(temp_df["前值"])
-    temp_df["现值"] = pd.to_numeric(temp_df["现值"])
+    ]]
+    temp_df["前值"] = pd.to_numeric(temp_df["前值"], errors="coerce")
+    temp_df["现值"] = pd.to_numeric(temp_df["现值"], errors="coerce")
     temp_df['发布日期'] = pd.to_datetime(temp_df['发布日期']).dt.date
     return temp_df
 
@@ -169,32 +220,44 @@ def macro_australia_cpi_quarterly() -> pd.DataFrame:
     :return: 消费者物价指数季率
     :rtype: pandas.DataFrame
     """
-    url = "http://datainterface.eastmoney.com/EM_DataCenter/JS.aspx"
+    url = "https://datacenter-web.eastmoney.com/api/data/v1/get"
     params = {
-        "type": "GJZB",
-        "sty": "HKZB",
-        "js": "({data:[(x)],pages:(pc)})",
+        "reportName": "RPT_ECONOMICVALUE_AUSTRALIA",
+        "columns": "ALL",
+        "filter": '(INDICATOR_ID="EMG00101104")',
+        "pageNumber": "1",
+        "pageSize": "2000",
+        "sortColumns": "REPORT_DATE",
+        "sortTypes": "-1",
+        "source": "WEB",
+        "client": "WEB",
         "p": "1",
-        "ps": "2000",
-        "mkt": "5",
-        "stat": "4",
-        'pageNo': '1',
-        'pageNum': '1',
-        "_": "1625474966006",
+        "pageNo": "1",
+        "pageNum": "1",
+        "_": "1669047266881",
     }
     r = requests.get(url, params=params)
-    data_text = r.text
-    data_json = demjson.decode(data_text[1:-1])
-    temp_df = pd.DataFrame([item.split(",") for item in data_json["data"]])
+    data_json = r.json()
+    temp_df = pd.DataFrame(data_json["result"]["data"])
     temp_df.columns = [
+        "-",
+        "-",
+        "-",
+        "时间",
+        "-",
+        "发布日期",
+        "现值",
+        "前值",
+
+    ]
+    temp_df = temp_df[[
         "时间",
         "前值",
         "现值",
         "发布日期",
-    ]
-    temp_df['时间'] = pd.to_datetime(temp_df['时间']).dt.date
-    temp_df["前值"] = pd.to_numeric(temp_df["前值"])
-    temp_df["现值"] = pd.to_numeric(temp_df["现值"])
+    ]]
+    temp_df["前值"] = pd.to_numeric(temp_df["前值"], errors="coerce")
+    temp_df["现值"] = pd.to_numeric(temp_df["现值"], errors="coerce")
     temp_df['发布日期'] = pd.to_datetime(temp_df['发布日期']).dt.date
     return temp_df
 
@@ -203,36 +266,48 @@ def macro_australia_cpi_quarterly() -> pd.DataFrame:
 def macro_australia_cpi_yearly() -> pd.DataFrame:
     """
     东方财富-经济数据-澳大利亚-消费者物价指数年率
-    http://data.eastmoney.com/cjsj/foreign_5_5.html
+    https://data.eastmoney.com/cjsj/foreign_5_5.html
     :return: 消费者物价指数年率
     :rtype: pandas.DataFrame
     """
-    url = "http://datainterface.eastmoney.com/EM_DataCenter/JS.aspx"
+    url = "https://datacenter-web.eastmoney.com/api/data/v1/get"
     params = {
-        "type": "GJZB",
-        "sty": "HKZB",
-        "js": "({data:[(x)],pages:(pc)})",
+        "reportName": "RPT_ECONOMICVALUE_AUSTRALIA",
+        "columns": "ALL",
+        "filter": '(INDICATOR_ID="EMG00101093")',
+        "pageNumber": "1",
+        "pageSize": "2000",
+        "sortColumns": "REPORT_DATE",
+        "sortTypes": "-1",
+        "source": "WEB",
+        "client": "WEB",
         "p": "1",
-        "ps": "2000",
-        "mkt": "5",
-        "stat": "5",
-        'pageNo': '1',
-        'pageNum': '1',
-        "_": "1625474966006",
+        "pageNo": "1",
+        "pageNum": "1",
+        "_": "1669047266881",
     }
     r = requests.get(url, params=params)
-    data_text = r.text
-    data_json = demjson.decode(data_text[1:-1])
-    temp_df = pd.DataFrame([item.split(",") for item in data_json["data"]])
+    data_json = r.json()
+    temp_df = pd.DataFrame(data_json["result"]["data"])
     temp_df.columns = [
+        "-",
+        "-",
+        "-",
+        "时间",
+        "-",
+        "发布日期",
+        "现值",
+        "前值",
+
+    ]
+    temp_df = temp_df[[
         "时间",
         "前值",
         "现值",
         "发布日期",
-    ]
-    temp_df['时间'] = pd.to_datetime(temp_df['时间']).dt.date
-    temp_df["前值"] = pd.to_numeric(temp_df["前值"])
-    temp_df["现值"] = pd.to_numeric(temp_df["现值"])
+    ]]
+    temp_df["前值"] = pd.to_numeric(temp_df["前值"], errors="coerce")
+    temp_df["现值"] = pd.to_numeric(temp_df["现值"], errors="coerce")
     temp_df['发布日期'] = pd.to_datetime(temp_df['发布日期']).dt.date
     return temp_df
 
@@ -241,36 +316,48 @@ def macro_australia_cpi_yearly() -> pd.DataFrame:
 def macro_australia_bank_rate() -> pd.DataFrame:
     """
     东方财富-经济数据-澳大利亚-央行公布利率决议
-    http://data.eastmoney.com/cjsj/foreign_5_6.html
+    https://data.eastmoney.com/cjsj/foreign_5_6.html
     :return: 央行公布利率决议
     :rtype: pandas.DataFrame
     """
-    url = "http://datainterface.eastmoney.com/EM_DataCenter/JS.aspx"
+    url = "https://datacenter-web.eastmoney.com/api/data/v1/get"
     params = {
-        "type": "GJZB",
-        "sty": "HKZB",
-        "js": "({data:[(x)],pages:(pc)})",
+        "reportName": "RPT_ECONOMICVALUE_AUSTRALIA",
+        "columns": "ALL",
+        "filter": '(INDICATOR_ID="EMG00342255")',
+        "pageNumber": "1",
+        "pageSize": "2000",
+        "sortColumns": "REPORT_DATE",
+        "sortTypes": "-1",
+        "source": "WEB",
+        "client": "WEB",
         "p": "1",
-        "ps": "2000",
-        "mkt": "5",
-        "stat": "6",
-        'pageNo': '1',
-        'pageNum': '1',
-        "_": "1625474966006",
+        "pageNo": "1",
+        "pageNum": "1",
+        "_": "1669047266881",
     }
     r = requests.get(url, params=params)
-    data_text = r.text
-    data_json = demjson.decode(data_text[1:-1])
-    temp_df = pd.DataFrame([item.split(",") for item in data_json["data"]])
+    data_json = r.json()
+    temp_df = pd.DataFrame(data_json["result"]["data"])
     temp_df.columns = [
+        "-",
+        "-",
+        "-",
+        "时间",
+        "-",
+        "发布日期",
+        "现值",
+        "前值",
+
+    ]
+    temp_df = temp_df[[
         "时间",
         "前值",
         "现值",
         "发布日期",
-    ]
-    temp_df['时间'] = pd.to_datetime(temp_df['时间']).dt.date
-    temp_df["前值"] = pd.to_numeric(temp_df["前值"])
-    temp_df["现值"] = pd.to_numeric(temp_df["现值"])
+    ]]
+    temp_df["前值"] = pd.to_numeric(temp_df["前值"], errors="coerce")
+    temp_df["现值"] = pd.to_numeric(temp_df["现值"], errors="coerce")
     temp_df['发布日期'] = pd.to_datetime(temp_df['发布日期']).dt.date
     return temp_df
 
--- docs/changelog.md
+++ docs/changelog.md
@@ -42,6 +42,10 @@
 
 ## 更新说明
 
+1.8.17 fix: fix macro_china_fdi interface
+
+    1. 修复 macro_china_fdi 接口
+
 1.8.16 fix: fix macro_china_swap_rate interface
 
     1. 修复 macro_china_swap_rate 接口
@@ -1593,6 +1597,8 @@
     3. 修改 air_quality_hist 接口的请求日期格式，从 '2022-01-01' 为 '20220101'
 
 ## 版本更新说明
+
+1.8.17 fix: fix macro_china_fdi interface
 
 1.8.16 fix: fix macro_china_swap_rate interface
 
