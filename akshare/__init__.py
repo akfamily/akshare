91cfb6b4d37991b98b2a330d2f6116827e43633d
jindaxiang <jindaxiang@163.com>
2022-11-30 13:23:13 +0800

    fix(stock_szse_margin.py): fix stock_margin_detail_szse interface
    fix stock_margin_detail_szse interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2238,9 +2238,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.8.19 fix: fix stock_balance_sheet_by_yearly_em interface
 1.8.20 fix: fix stock_board_concept_hist_em interface
 1.8.21 fix: fix stock_board_concept_hist_em interface
+1.8.22 fix: fix stock_margin_detail_szse interface
 """
 
-__version__ = "1.8.21"
+__version__ = "1.8.22"
 __author__ = "AKFamily"
 
 import sys
