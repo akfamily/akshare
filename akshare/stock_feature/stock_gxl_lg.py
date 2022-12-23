70782f97e7027f317dd3fb3ff81ea4b070640e48
jindaxiang <jindaxiang@163.com>
2022-12-23 17:58:38 +0800

    feat(stock_gxl_lg.py): add stock_hk_gxl_lg interface
    add stock_hk_gxl_lg interface

--- akshare/stock_feature/stock_gxl_lg.py
+++ akshare/stock_feature/stock_gxl_lg.py
@@ -17,7 +17,7 @@ def stock_a_gxl_lg(symbol: str = "上证A股") -> pd.DataFrame:
     https://legulegu.com/stockdata/guxilv
     :param symbol: choice of {"上证A股", "深证A股", "创业板", "科创板"}
     :type symbol: str
-    :return: A股股息率
+    :return: A 股股息率
     :rtype: pandas.DataFrame
     """
     symbol_map = {
@@ -42,6 +42,32 @@ def stock_a_gxl_lg(symbol: str = "上证A股") -> pd.DataFrame:
     return temp_df
 
 
+def stock_hk_gxl_lg() -> pd.DataFrame:
+    """
+    乐咕乐股-股息率-恒生指数股息率
+    https://legulegu.com/stockdata/market/hk/dv/hsi
+    :return: 恒生指数股息率
+    :rtype: pandas.DataFrame
+    """
+    url = "https://legulegu.com/api/stockdata/hs"
+    token = get_token_lg()
+    params = {"token": token, "indexCode": "HSI"}
+    r = requests.get(url, params=params)
+    data_json = r.json()
+    temp_df = pd.DataFrame(data_json)
+    temp_df['date'] = (pd.to_datetime(temp_df['date'], unit="ms") + pd.Timedelta(hours=8)).dt.date
+    temp_df.rename(columns={"addDvRatio": "股息率", "date": "日期"}, inplace=True)
+    temp_df = temp_df[[
+        '日期',
+        '股息率',
+    ]]
+    temp_df['股息率'] = pd.to_numeric(temp_df['股息率'], errors="coerce")
+    return temp_df
+
+
 if __name__ == "__main__":
     stock_a_gxl_lg_df = stock_a_gxl_lg(symbol="上证A股")
     print(stock_a_gxl_lg_df)
+
+    stock_hk_gxl_lg_df = stock_hk_gxl_lg()
+    print(stock_hk_gxl_lg_df)
