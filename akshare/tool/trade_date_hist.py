96a8e9c9b8736a4d40241e2a1de577439c3b7225
jindaxiang <jindaxiang@163.com>
2023-01-04 10:39:34 +0800

    feat(futures_roll_yield.py): fix get_roll_yield_bar interface
    fix get_roll_yield_bar interface

--- akshare/tool/trade_date_hist.py
+++ akshare/tool/trade_date_hist.py
@@ -41,5 +41,4 @@ def tool_trade_date_hist_sina() -> pd.DataFrame:
 
 if __name__ == "__main__":
     tool_trade_date_hist_df = tool_trade_date_hist_sina()
-    print(list(tool_trade_date_hist_df['trade_date']))
-    [item.isoformat() for item in tool_trade_date_hist_df['trade_date']]
+    print(tool_trade_date_hist_df)
