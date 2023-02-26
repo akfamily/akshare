b7f01f7db776132fc61fdd3c9d0b55fe4973adbc
jindaxiang <jindaxiang@163.com>
2023-02-26 16:51:00 +0800

    fix(stock_info.py): fix stock_info_sz_delist interface
    fix stock_info_sz_delist interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2314,9 +2314,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.8.95 fix: fix index_zh_a_hist interface
 1.8.96 fix: fix option_finance_board interface
 1.8.97 fix: fix futures_egg_price_yearly interface
+1.8.98 fix: fix stock_info_sz_delist interface
 """
 
-__version__ = "1.8.97"
+__version__ = "1.8.98"
 __author__ = "AKFamily"
 
 import sys
