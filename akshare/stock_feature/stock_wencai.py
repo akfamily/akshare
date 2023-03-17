97c12868ac3f9a849218d68c1dd9ff8839f97f42
jindaxiang <jindaxiang@163.com>
2023-03-17 21:52:27 +0800

    fix(stock_wencai.py): fix stock_hot_rank_wc interface
    fix stock_hot_rank_wc interface

--- akshare/stock_feature/stock_wencai.py
+++ akshare/stock_feature/stock_wencai.py
@@ -1,33 +1,16 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2023/1/11 16:11
+Date: 2023/3/17 21:11
 Desc: 问财-热门股票排名
 https://www.iwencai.com/unifiedwap/home/index
 """
 import pandas as pd
 import requests
-from py_mini_racer import py_mini_racer
 from tqdm import tqdm
 
-from akshare.datasets import get_ths_js
 
-
-def _get_file_content_ths(file: str = "ths.js") -> str:
-    """
-    获取 JS 文件的内容
-    :param file:  JS 文件名
-    :type file: str
-    :return: 文件内容
-    :rtype: str
-    """
-    setting_file_path = get_ths_js(file)
-    with open(setting_file_path) as f:
-        file_data = f.read()
-    return file_data
-
-
-def stock_hot_rank_wc(date: str = "20210430") -> pd.DataFrame:
+def stock_hot_rank_wc(date: str = "20230316") -> pd.DataFrame:
     """
     问财-热门股票排名
     https://www.iwencai.com/unifiedwap/result?w=%E7%83%AD%E9%97%A85000%E8%82%A1%E7%A5%A8&querytype=stock&issugs&sign=1620126514335
@@ -36,41 +19,27 @@ def stock_hot_rank_wc(date: str = "20210430") -> pd.DataFrame:
     :return: 热门股票排名
     :rtype: pandas.DataFrame
     """
-    url = "http://www.iwencai.com/unifiedwap/unified-wap/v2/result/get-robot-data"
-    js_code = py_mini_racer.MiniRacer()
-    js_content = _get_file_content_ths("ths.js")
-    js_code.eval(js_content)
-    v_code = js_code.call("v")
-    headers = {
-        "hexin-v": v_code,
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
-    }
+    url = "https://www.iwencai.com/gateway/urp/v7/landing/getDataList"
     params = {
-        "question": f"{date}热门5000股票",
-        "perpage": "5000",
+        "query": f"{date}热门5000股票",
+        "perpage": "100",
         "page": "1",
-        "secondary_intent": "",
-        "log_info": '{"input_type":"click"}',
-        "source": "Ths_iwencai_Xuangu",
-        "version": "2.0",
-        "query_area": "",
-        "block_list": "",
-        "add_info": '{"urp":{"scene":1,"company":1,"business":1},"contentType":"json"}',
+        "comp_id": "6651918",
+        "uuid": "18369",
+    }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
     }
     big_df = pd.DataFrame()
-    for page in tqdm(range(1, 11), leave=False):
+    for page in tqdm(range(1, 51), leave=False):
         params.update(
             {
                 "page": page,
             }
         )
-        r = requests.post(url, data=params, headers=headers)
+        r = requests.get(url, params=params, headers=headers)
         data_json = r.json()
-        temp_df = pd.DataFrame(
-            data_json["data"]["answer"][0]["txt"][0]["content"]["components"][0][
-                "data"
-            ]["datas"]
-        )
+        temp_df = pd.DataFrame(data_json["answer"]["components"][0]["data"]["datas"])
         big_df = pd.concat([big_df, temp_df], ignore_index=True)
 
     big_df.reset_index(inplace=True)
@@ -113,5 +82,5 @@ def stock_hot_rank_wc(date: str = "20210430") -> pd.DataFrame:
 
 
 if __name__ == "__main__":
-    stock_hot_rank_wc_df = stock_hot_rank_wc(date="20230316")
+    stock_hot_rank_wc_df = stock_hot_rank_wc(date="20230317")
     print(stock_hot_rank_wc_df)
