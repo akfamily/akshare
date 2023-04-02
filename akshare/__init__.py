97121ef8913cc31d6423b85b96088db53309b412
jindaxiang <jindaxiang@163.com>
2023-04-02 16:50:24 +0800

    fix(stock_a_indicator.py): fix stock_a_lg_indicator interface
    fix stock_a_lg_indicator interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2344,9 +2344,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.9.26 fix: fix bond_cb_jsl interface
 1.9.27 fix: fix fund_exchange_rank_em interface
 1.9.28 fix: fix stock_financial_report_sina interface
+1.9.29 fix: fix stock_a_lg_indicator interface
 """
 
-__version__ = "1.9.28"
+__version__ = "1.9.29"
 __author__ = "AKFamily"
 
 import sys
