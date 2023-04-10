e23be13e775b41fdf42975c3210cf56cf59a0f67
jindaxiang <jindaxiang@163.com>
2023-04-10 16:16:40 +0800

    fix(option_commodity.py): fix option_dce_daily interface
    fix option_dce_daily interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2362,9 +2362,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.9.44 fix: fix stock_a_high_low_statistics interface
 1.9.45 add: add stock_fhps_detail_ths interface
 1.9.46 fix: fix stock_a_gxl_lg interface
+1.9.47 fix: fix option_dce_daily interface
 """
 
-__version__ = "1.9.46"
+__version__ = "1.9.47"
 __author__ = "AKFamily"
 
 import sys
