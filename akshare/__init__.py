bb8f4a5421cc7c7cd2b8ffbcec9e23cfd85f0c20
jindaxiang <jindaxiang@163.com>
2023-05-26 20:53:44 +0800

    fix(stock_industry_cninfo.py): fix stock_industry_category_cninfo interface
    fix stock_industry_category_cninfo interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2412,9 +2412,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.9.94 fix: fix stock_a_indicator_lg interface
 1.9.95 fix: fix stock_zh_index_hist_csindex interface
 1.9.96 fix: fix stock_hold_control_cninfo interface
+1.9.97 fix: fix stock_industry_category_cninfo interface
 """
 
-__version__ = "1.9.96"
+__version__ = "1.9.97"
 __author__ = "AKFamily"
 
 import sys
