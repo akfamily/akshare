95c0b7f9119f82334b6adb85a6a7f67efe0bf27b
jindaxiang <jindaxiang@163.com>
2023-05-31 16:51:48 +0800

    fix(futures_index_return_nh.py): fix futures_return_index_nh interface
    fix futures_return_index_nh interface

--- akshare/futures_derivative/futures_index_price_nh.py
+++ akshare/futures_derivative/futures_index_price_nh.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2022/11/13 14:25
+Date: 2023/5/31 16:25
 Desc: 南华期货-商品指数历史走势-价格指数-数值
 https://www.nanhua.net/nhzc/varietytrend.html
 1000 点开始, 用收益率累计
@@ -9,22 +9,22 @@ https://www.nanhua.net/ianalysis/varietyindex/price/A.json?t=1574932974280
 """
 import time
 
-import requests
 import pandas as pd
+import requests
 
 
 def futures_index_symbol_table_nh() -> pd.DataFrame:
     """
     南华期货-南华指数所有品种一览表
-    http://www.nanhua.net/ianalysis/varietyindex/price/A.json?t=1574932974280
+    https://www.nanhua.net/ianalysis/varietyindex/price/A.json?t=1574932974280
     :return: 南华指数所有品种一览表
     :rtype: pandas.DataFrame
     """
-    url = "http://www.nanhua.net/ianalysis/plate-variety.json"
+    url = "https://www.nanhua.net/ianalysis/plate-variety.json"
     r = requests.get(url)
     data_json = r.json()
     temp_df = pd.DataFrame(data_json)
-    temp_df['firstday'] = pd.to_datetime(temp_df['firstday']).dt.date
+    temp_df["firstday"] = pd.to_datetime(temp_df["firstday"]).dt.date
     return temp_df
 
 
@@ -41,12 +41,15 @@ def futures_price_index_nh(symbol: str = "A") -> pd.DataFrame:
     symbol_list = symbol_df["code"].tolist()
     if symbol in symbol_list:
         t = time.time()
-        url = f"http://www.nanhua.net/ianalysis/varietyindex/price/{symbol}.json?t={int(round(t * 1000))}"
+        url = f"https://www.nanhua.net/ianalysis/varietyindex/price/{symbol}.json?t={int(round(t * 1000))}"
         r = requests.get(url)
         data_json = r.json()
         temp_df = pd.DataFrame(data_json)
         temp_df.columns = ["date", "value"]
-        temp_df['date'] = (pd.to_datetime(temp_df["date"], unit='ms') - pd.Timedelta(hours=-8)).dt.date
+        temp_df["date"] = pd.to_datetime(temp_df["date"], unit="ms")
+        temp_df["date"] = temp_df["date"].dt.tz_localize("UTC")
+        temp_df["date"] = temp_df["date"].dt.tz_convert("Asia/Shanghai").dt.date
+        temp_df["value"] = pd.to_numeric(temp_df["value"], errors="coerce")
         return temp_df
 
 
