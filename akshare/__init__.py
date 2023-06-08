ba5e0bb874c582b81e7596fd1b6bcc0b308e2ff4
jindaxiang <jindaxiang@163.com>
2023-06-08 14:54:01 +0800

    fix bond_cb_adj_logs_jsl interface

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2426,9 +2426,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.10.9 fix: fix bond_zh_hs_cov_min interface
 1.10.10 fix: fix stock_zh_a_hist interface
 1.10.11 fix: fix stock_zh_a_hist_pre_min_em interface
+1.10.12 fix: fix bond_cb_adj_logs_jsl interface
 """
 
-__version__ = "1.10.11"
+__version__ = "1.10.12"
 __author__ = "AKFamily"
 
 import sys
