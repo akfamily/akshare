156b6b11cfb8259632906769311cb7d3d909a685
Albert King <jindaxiang@163.com>
2023-06-09 18:30:33 +0800

    Stock (#3862)
    * add version
    
    * add version
    
    * fix stock_allotment_cninfo

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2431,9 +2431,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.10.14 fix: fix bond_zh_hs_cov_min interface
 1.10.15 fix: fix bond_zh_hs_cov_pre_min interface
 1.10.16 fix: fix bond_zh_hs_cov_min interface
+1.10.17 fix: fix bond_zh_hs_cov_min interface
 """
 
-__version__ = "1.10.16"
+__version__ = "1.10.17"
 __author__ = "AKFamily"
 
 import sys
--- akshare/stock/stock_allotment_cninfo.py
+++ akshare/stock/stock_allotment_cninfo.py
@@ -1,45 +1,29 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2022/10/8 18:11
+Date: 2023/6/9 18:11
 Desc: 巨潮资讯-数据浏览器-筹资指标-公司配股实施方案
 http://webapi.cninfo.com.cn/#/dataBrowse
 """
-import time
-
 import pandas as pd
 import requests
 from py_mini_racer import py_mini_racer
 
-js_str = """
-    function mcode(input) {  
-                var keyStr = "ABCDEFGHIJKLMNOP" + "QRSTUVWXYZabcdef" + "ghijklmnopqrstuv"   + "wxyz0123456789+/" + "=";  
-                var output = "";  
-                var chr1, chr2, chr3 = "";  
-                var enc1, enc2, enc3, enc4 = "";  
-                var i = 0;  
-                do {  
-                    chr1 = input.charCodeAt(i++);  
-                    chr2 = input.charCodeAt(i++);  
-                    chr3 = input.charCodeAt(i++);  
-                    enc1 = chr1 >> 2;  
-                    enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);  
-                    enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);  
-                    enc4 = chr3 & 63;  
-                    if (isNaN(chr2)) {  
-                        enc3 = enc4 = 64;  
-                    } else if (isNaN(chr3)) {  
-                        enc4 = 64;  
-                    }  
-                    output = output + keyStr.charAt(enc1) + keyStr.charAt(enc2)  
-                            + keyStr.charAt(enc3) + keyStr.charAt(enc4);  
-                    chr1 = chr2 = chr3 = "";  
-                    enc1 = enc2 = enc3 = enc4 = "";  
-                } while (i < input.length);  
-          
-                return output;  
-            }  
-"""
+from akshare.datasets import get_ths_js
+
+
+def _get_file_content_cninfo(file: str = "cninfo.js") -> str:
+    """
+    获取 JS 文件的内容
+    :param file:  JS 文件名
+    :type file: str
+    :return: 文件内容
+    :rtype: str
+    """
+    setting_file_path = get_ths_js(file)
+    with open(setting_file_path) as f:
+        file_data = f.read()
+    return file_data
 
 
 def stock_allotment_cninfo(
@@ -67,18 +51,18 @@ def stock_allotment_cninfo(
         if not end_date
         else f"{end_date[0:4]}-{end_date[4:6]}-{end_date[6:8]}",
     }
-    random_time_str = str(int(time.time()))
     js_code = py_mini_racer.MiniRacer()
-    js_code.eval(js_str)
-    mcode = js_code.call("mcode", random_time_str)
+    js_content = _get_file_content_cninfo("cninfo.js")
+    js_code.eval(js_content)
+    mcode = js_code.call("getResCode1")
     headers = {
         "Accept": "*/*",
+        "Accept-Enckey": mcode,
         "Accept-Encoding": "gzip, deflate",
         "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
         "Cache-Control": "no-cache",
         "Content-Length": "0",
         "Host": "webapi.cninfo.com.cn",
-        "mcode": mcode,
         "Origin": "http://webapi.cninfo.com.cn",
         "Pragma": "no-cache",
         "Proxy-Connection": "keep-alive",
@@ -206,5 +190,7 @@ def stock_allotment_cninfo(
 
 
 if __name__ == "__main__":
-    stock_allotment_cninfo_df = stock_allotment_cninfo(symbol="600030", start_date="19900101", end_date="20221008")
+    stock_allotment_cninfo_df = stock_allotment_cninfo(
+        symbol="600030", start_date="19900101", end_date="20221008"
+    )
     print(stock_allotment_cninfo_df)
