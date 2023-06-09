277a770c5a1acd6c4e4179f963d3507290421ee9
jindaxiang <jindaxiang@163.com>
2023-06-09 18:53:27 +0800

    add version

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2431,7 +2431,7 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.10.14 fix: fix bond_zh_hs_cov_min interface
 1.10.15 fix: fix bond_zh_hs_cov_pre_min interface
 1.10.16 fix: fix bond_zh_hs_cov_min interface
-1.10.17 fix: fix bond_zh_hs_cov_min interface
+1.10.17 fix: fix stock_allotment_cninfo interface
 """
 
 __version__ = "1.10.17"
