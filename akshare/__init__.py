fdd792a7afd58e71dd8bfc83a8ea80bf49dcdacd
Albert King <jindaxiang@163.com>
2023-06-19 15:31:03 +0800

    Date (#3900)
    * update date
    
    * add version
    
    * fix zh_subscribe_exchange_symbol

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2445,9 +2445,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.10.28 fix: fix stock_hot_up_em interface
 1.10.29 fix: fix stock_zh_index_daily_em interface
 1.10.30 fix: fix stock_info_sz_name_code interface
+1.10.31 fix: fix zh_subscribe_exchange_symbol interface
 """
 
-__version__ = "1.10.30"
+__version__ = "1.10.31"
 __author__ = "AKFamily"
 
 import sys
