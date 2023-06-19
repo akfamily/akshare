77789d1e0c0ac27f9da851a4a4890d2074ccdc5f
Albert King <jindaxiang@163.com>
2023-06-19 20:10:31 +0800

    Futures (#3904)
    * add version
    
    * fix futures_display_main_sina
    
    * add version
    
    * add version

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2448,9 +2448,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.10.31 fix: fix zh_subscribe_exchange_symbol interface
 1.10.32 fix: fix get_cffex_daily interface
 1.10.33 fix: fix index_sugar_msweet interface
+1.10.34 fix: fix futures_display_main_sina interface
 """
 
-__version__ = "1.10.33"
+__version__ = "1.10.34"
 __author__ = "AKFamily"
 
 import sys
@@ -4277,7 +4278,7 @@ from akshare.fortune.fortune_it_juzi import (
 """
 新浪主力连续接口
 """
-from akshare.futures_derivative.sina_futures_index import (
+from akshare.futures_derivative.futures_index_sina import (
     futures_main_sina,
     futures_display_main_sina,
 )
