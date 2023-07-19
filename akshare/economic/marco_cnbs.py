5e00622cf1e1b845adfc9a8eb8be772e196193e5
Albert King <jindaxiang@163.com>
2023-07-19 18:31:37 +0800

    Dev (#3990)
    * fix issue #3979 (https://github.com/akfamily/akshare/issues/3979) (#3982)
    
    close #3979
    
    * add version
    
    * fix macro_cnbs
    
    ---------
    
    Co-authored-by: JiangJam <39955422+JiangJamm@users.noreply.github.com>

--- akshare/economic/marco_cnbs.py
+++ akshare/economic/marco_cnbs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2023/3/22 20:22
+Date: 2023/7/19 18:20
 Desc: 国家金融与发展实验室-中国宏观杠杆率数据
 http://114.115.232.154:8080/
 """
@@ -19,9 +19,26 @@ def macro_cnbs() -> pd.DataFrame:
     temp_df = pd.read_excel(
         url, sheet_name="Data", header=0, skiprows=1, engine="openpyxl"
     )
+
     temp_df["Period"] = pd.to_datetime(temp_df["Period"]).dt.strftime("%Y-%m")
     temp_df.dropna(axis=1, inplace=True)
-    temp_df.columns = [
+
+    temp_df.rename(
+        columns={
+            "Period": "年份",
+            "Household": "居民部门",
+            "Non-financial corporations": "非金融企业部门",
+            "Central government ": "中央政府",
+            "Local government": "地方政府",
+            "General government": "政府部门",
+            "Non financial sector": "实体经济部门",
+            "Financial sector(asset side)": "金融部门资产方",
+            "Financial sector(liability side)": "金融部门负债方",
+        },
+        inplace=True,
+    )
+
+    column_order = [
         "年份",
         "居民部门",
         "非金融企业部门",
@@ -32,6 +49,8 @@ def macro_cnbs() -> pd.DataFrame:
         "金融部门资产方",
         "金融部门负债方",
     ]
+    temp_df = temp_df.reindex(columns=column_order)
+
     temp_df["居民部门"] = pd.to_numeric(temp_df["居民部门"], errors="coerce")
     temp_df["非金融企业部门"] = pd.to_numeric(temp_df["非金融企业部门"], errors="coerce")
     temp_df["政府部门"] = pd.to_numeric(temp_df["政府部门"], errors="coerce")
@@ -40,6 +59,7 @@ def macro_cnbs() -> pd.DataFrame:
     temp_df["实体经济部门"] = pd.to_numeric(temp_df["实体经济部门"], errors="coerce")
     temp_df["金融部门资产方"] = pd.to_numeric(temp_df["金融部门资产方"], errors="coerce")
     temp_df["金融部门负债方"] = pd.to_numeric(temp_df["金融部门负债方"], errors="coerce")
+
     return temp_df
 
 
--- docs/changelog.md
+++ docs/changelog.md
@@ -63,6 +63,10 @@
 
 ## 更新说明详情
 
+1.10.61 fix: fix macro_cnbs interface
+
+    1. 修复 macro_cnbs 接口
+
 1.10.60 add: add macro_usa_cpi_yoy interface
 
     1. 新增 macro_usa_cpi_yoy 接口
@@ -2653,6 +2657,8 @@
     3. 修改 air_quality_hist 接口的请求日期格式，从 '2022-01-01' 为 '20220101'
 
 ## 版本更新说明
+
+1.10.61 fix: fix macro_cnbs interface
 
 1.10.60 add: add macro_usa_cpi_yoy interface
 
