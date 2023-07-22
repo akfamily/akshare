739f99cb84ec767ea4a5cf50f009998bc1285118
jindaxiang <jindaxiang@163.com>
2023-07-22 17:25:28 +0800

    add version

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2481,9 +2481,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.10.64 fix: fix stock_zh_b_spot interface
 1.10.65 fix: fix macro_china_lpr interface
 1.10.66 fix: fix stock_financial_report_sina interface
+1.10.67 rem: rem futures_egg_price_yearly interface
 """
 
-__version__ = "1.10.66"
+__version__ = "1.10.67"
 __author__ = "AKFamily"
 
 import sys
@@ -3144,15 +3145,6 @@ from akshare.stock.stock_us_pink import stock_us_pink_spot_em
 REITs
 """
 from akshare.reits.reits_basic import reits_realtime_em
-
-"""
-鸡蛋价格数据
-"""
-from akshare.futures_derivative.futures_egg import (
-    futures_egg_price_yearly,
-    futures_egg_price_area,
-    futures_egg_price,
-)
 
 """
 全部 A 股-等权重市盈率、中位数市盈率
