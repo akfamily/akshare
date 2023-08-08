7747603f7ad328312d01c45a9c83299e7717268c
jindaxiang <jindaxiang@163.com>
2023-08-08 22:48:50 +0800

    add stock_hold_management_detail_em

--- akshare/stock/stock_hold_control_em.py
+++ akshare/stock/stock_hold_control_em.py
@@ -1,202 +1,204 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2023/5/29 23:34
-Desc: 巨潮资讯-数据中心-专题统计-股东股本-实际控制人持股变动
-http://webapi.cninfo.com.cn/#/thematicStatistics
-
-巨潮资讯-数据中心-专题统计-股东股本-高管持股变动明细
-http://webapi.cninfo.com.cn/#/thematicStatistics
+Date: 2023/8/8 21:30
+Desc: 东方财富网-数据中心-特色数据-高管持股
+https://data.eastmoney.com/executive/list.html
 """
-import datetime
-
 import pandas as pd
 import requests
-from py_mini_racer import py_mini_racer
-
-from akshare.datasets import get_ths_js
+from tqdm import tqdm
 
 
-def _get_file_content_cninfo(file: str = "cninfo.js") -> str:
+def stock_hold_management_detail_em() -> pd.DataFrame:
     """
-    获取 JS 文件的内容
-    :param file:  JS 文件名
-    :type file: str
-    :return: 文件内容
-    :rtype: str
-    """
-    setting_file_path = get_ths_js(file)
-    with open(setting_file_path) as f:
-        file_data = f.read()
-    return file_data
-
-
-def stock_hold_control_cninfo(symbol: str = "全部") -> pd.DataFrame:
-    """
-    巨潮资讯-数据中心-专题统计-股东股本-实际控制人持股变动
-    http://webapi.cninfo.com.cn/#/thematicStatistics
-    :param symbol: choice of {"单独控制", "实际控制人", "一致行动人", "家族控制", "全部"}; 从 2010 开始
-    :type symbol: str
-    :return: 实际控制人持股变动
+    东方财富网-数据中心-特色数据-高管持股-董监高及相关人员持股变动明细
+    https://data.eastmoney.com/executive/list.html
+    :return: 董监高及相关人员持股变动明细
     :rtype: pandas.DataFrame
     """
-    symbol_map = {
-        "单独控制": "069001",
-        "实际控制人": "069002",
-        "一致行动人": "069003",
-        "家族控制": "069004",
-        "全部": "",
-    }
-    url = "http://webapi.cninfo.com.cn/api/sysapi/p_sysapi1033"
-    js_code = py_mini_racer.MiniRacer()
-    js_content = _get_file_content_cninfo("cninfo.js")
-    js_code.eval(js_content)
-    mcode = js_code.call("getResCode1")
-    headers = {
-        "Accept": "*/*",
-        "Accept-Enckey": mcode,
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
-        "Cache-Control": "no-cache",
-        "Content-Length": "0",
-        "Host": "webapi.cninfo.com.cn",
-        "Origin": "http://webapi.cninfo.com.cn",
-        "Pragma": "no-cache",
-        "Proxy-Connection": "keep-alive",
-        "Referer": "http://webapi.cninfo.com.cn/",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
-        "X-Requested-With": "XMLHttpRequest",
-    }
+    url = "https://datacenter-web.eastmoney.com/api/data/v1/get"
     params = {
-        "ctype": symbol_map[symbol],
+        "reportName": "RPT_EXECUTIVE_HOLD_DETAILS",
+        "columns": "ALL",
+        "quoteColumns": "",
+        "filter": "",
+        "pageNumber": "1",
+        "pageSize": "5000",
+        "sortTypes": "-1,1,1",
+        "sortColumns": "CHANGE_DATE,SECURITY_CODE,PERSON_NAME",
+        "source": "WEB",
+        "client": "WEB",
+        "p": "1",
+        "pageNo": "1",
+        "pageNum": "1",
+        "_": "1691501763413",
     }
-    r = requests.get(url, headers=headers, params=params)
+    r = requests.get(url, params=params)
     data_json = r.json()
-    temp_df = pd.DataFrame(data_json["records"])
-    temp_df.columns = [
-        "控股比例",
-        "控股数量",
-        "证券简称",
-        "实际控制人名称",
-        "直接控制人名称",
-        "控制类型",
-        "证券代码",
-        "变动日期",
-    ]
-    temp_df = temp_df[
-        [
-            "证券代码",
-            "证券简称",
-            "变动日期",
-            "实际控制人名称",
-            "控股数量",
-            "控股比例",
-            "直接控制人名称",
-            "控制类型",
-        ]
-    ]
-    temp_df["变动日期"] = pd.to_datetime(temp_df["变动日期"]).dt.date
-    temp_df["控股数量"] = pd.to_numeric(temp_df["控股数量"], errors="coerce")
-    temp_df["控股比例"] = pd.to_numeric(temp_df["控股比例"], errors="coerce")
-    return temp_df
+    total_page = data_json["result"]["pages"]
+    big_df = pd.DataFrame()
+    for page in tqdm(range(1, total_page + 1), leave=False):
+        params.update(
+            {
+                "pageNumber": page,
+                "p": page,
+                "pageNo": page,
+                "pageNum": page,
+            }
+        )
+        r = requests.post(url, params=params)
+        data_json = r.json()
+        temp_df = pd.DataFrame(data_json["result"]["data"])
+        big_df = pd.concat([big_df, temp_df], ignore_index=True)
 
+    big_df.rename(
+        columns={
+            "SECURITY_CODE": "代码",
+            "DERIVE_SECURITY_CODE": "-",
+            "SECURITY_NAME": "名称",
+            "CHANGE_DATE": "日期",
+            "PERSON_NAME": "变动人",
+            "CHANGE_SHARES": "变动股数",
+            "AVERAGE_PRICE": "成交均价",
+            "CHANGE_AMOUNT": "变动金额",
+            "CHANGE_REASON": "变动原因",
+            "CHANGE_RATIO": "变动比例",
+            "CHANGE_AFTER_HOLDNUM": "变动后持股数",
+            "HOLD_TYPE": "持股种类",
+            "DSE_PERSON_NAME": "董监高人员姓名",
+            "POSITION_NAME": "职务",
+            "PERSON_DSE_RELATION": "变动人与董监高的关系",
+            "ORG_CODE": "-",
+            "GGEID": "-",
+            "BEGIN_HOLD_NUM": "开始时持有",
+            "END_HOLD_NUM": "结束后持有",
+        },
+        inplace=True,
+    )
 
-def stock_hold_management_detail_cninfo(symbol: str = "增持") -> pd.DataFrame:
-    """
-    巨潮资讯-数据中心-专题统计-股东股本-高管持股变动明细
-    http://webapi.cninfo.com.cn/#/thematicStatistics
-    :param symbol: choice of {"增持", "减持"}
-    :type symbol: str
-    :return: 高管持股变动明细
-    :rtype: pandas.DataFrame
-    """
-    symbol_map = {
-        "增持": "B",
-        "减持": "S",
-    }
-    current_date = datetime.datetime.now().date().isoformat()
-    url = "http://webapi.cninfo.com.cn/api/sysapi/p_sysapi1030"
-    js_code = py_mini_racer.MiniRacer()
-    js_content = _get_file_content_cninfo("cninfo.js")
-    js_code.eval(js_content)
-    mcode = js_code.call("getResCode1")
-    headers = {
-        "Accept": "*/*",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
-        "Cache-Control": "no-cache",
-        "Content-Length": "0",
-        "Host": "webapi.cninfo.com.cn",
-        "Accept-Enckey": mcode,
-        "Origin": "http://webapi.cninfo.com.cn",
-        "Pragma": "no-cache",
-        "Proxy-Connection": "keep-alive",
-        "Referer": "http://webapi.cninfo.com.cn/",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
-        "X-Requested-With": "XMLHttpRequest",
-    }
-    params = {
-        "sdate": str(int(current_date[:4]) - 1) + current_date[4:],
-        "edate": current_date,
-        "varytype": symbol_map[symbol],
-    }
-    r = requests.post(url, headers=headers, params=params)
-    data_json = r.json()
-    temp_df = pd.DataFrame(data_json["records"])
-    temp_df.columns = [
-        "证券简称",
-        "公告日期",
-        "高管姓名",
-        "期末市值",
-        "成交均价",
-        "证券代码",
-        "变动比例",
-        "变动数量",
-        "截止日期",
-        "期末持股数量",
-        "期初持股数量",
-        "变动人与董监高关系",
-        "董监高职务",
-        "董监高姓名",
-        "数据来源",
-        "持股变动原因",
-    ]
-    temp_df = temp_df[
+    big_df = big_df[
         [
-            "证券代码",
-            "证券简称",
-            "截止日期",
-            "公告日期",
-            "高管姓名",
-            "董监高姓名",
-            "董监高职务",
-            "变动人与董监高关系",
-            "期初持股数量",
-            "期末持股数量",
-            "变动数量",
-            "变动比例",
+            "日期",
+            "代码",
+            "名称",
+            "变动人",
+            "变动股数",
             "成交均价",
-            "期末市值",
-            "持股变动原因",
-            "数据来源",
+            "变动金额",
+            "变动原因",
+            "变动比例",
+            "变动后持股数",
+            "持股种类",
+            "董监高人员姓名",
+            "职务",
+            "变动人与董监高的关系",
+            "开始时持有",
+            "结束后持有",
         ]
     ]
-    temp_df["截止日期"] = pd.to_datetime(temp_df["截止日期"]).dt.date
-    temp_df["公告日期"] = pd.to_datetime(temp_df["公告日期"]).dt.date
-    temp_df["期初持股数量"] = pd.to_numeric(temp_df["期初持股数量"], errors="coerce")
-    temp_df["期末持股数量"] = pd.to_numeric(temp_df["期末持股数量"], errors="coerce")
-    temp_df["变动数量"] = pd.to_numeric(temp_df["变动数量"], errors="coerce")
-    temp_df["变动比例"] = pd.to_numeric(temp_df["变动比例"], errors="coerce")
+    big_df["日期"] = pd.to_datetime(big_df["日期"], errors="coerce").dt.date
+    big_df["变动股数"] = pd.to_numeric(big_df["变动股数"], errors="coerce")
+    big_df["成交均价"] = pd.to_numeric(big_df["成交均价"], errors="coerce")
+    big_df["变动金额"] = pd.to_numeric(big_df["变动金额"], errors="coerce")
+    big_df["变动比例"] = pd.to_numeric(big_df["变动比例"], errors="coerce")
+    big_df["变动后持股数"] = pd.to_numeric(big_df["变动后持股数"], errors="coerce")
+    big_df["开始时持有"] = pd.to_numeric(big_df["开始时持有"], errors="coerce")
+    big_df["结束后持有"] = pd.to_numeric(big_df["结束后持有"], errors="coerce")
+    return big_df
+
+
+def stock_hold_management_person_em(
+    symbol: str = "001308", name: str = "吴远"
+) -> pd.DataFrame:
+    """
+    东方财富网-数据中心-特色数据-高管持股-人员增减持股变动明细
+    https://data.eastmoney.com/executive/personinfo.html?name=%E5%90%B4%E8%BF%9C&code=001308
+    :param symbol: 股票代码
+    :type name: str
+    :param name: 高管名称
+    :type symbol: str
+    :return: 人员增减持股变动明细
+    :rtype: pandas.DataFrame
+    """
+    url = "https://datacenter-web.eastmoney.com/api/data/v1/get"
+    params = {
+        "reportName": "RPT_EXECUTIVE_HOLD_DETAILS",
+        "columns": "ALL",
+        "quoteColumns": "",
+        "filter": f'(SECURITY_CODE="{symbol}")(PERSON_NAME="{name}")',
+        "pageNumber": "1",
+        "pageSize": "5000",
+        "sortTypes": "-1,1,1",
+        "sortColumns": "CHANGE_DATE,SECURITY_CODE,PERSON_NAME",
+        "source": "WEB",
+        "client": "WEB",
+        "_": "1691503078611",
+    }
+    r = requests.get(url, params=params)
+    data_json = r.json()
+    temp_df = pd.DataFrame(data_json["result"]["data"])
+    temp_df.rename(
+        columns={
+            "SECURITY_CODE": "代码",
+            "DERIVE_SECURITY_CODE": "-",
+            "SECURITY_NAME": "名称",
+            "CHANGE_DATE": "日期",
+            "PERSON_NAME": "变动人",
+            "CHANGE_SHARES": "变动股数",
+            "AVERAGE_PRICE": "成交均价",
+            "CHANGE_AMOUNT": "变动金额",
+            "CHANGE_REASON": "变动原因",
+            "CHANGE_RATIO": "变动比例",
+            "CHANGE_AFTER_HOLDNUM": "变动后持股数",
+            "HOLD_TYPE": "持股种类",
+            "DSE_PERSON_NAME": "董监高人员姓名",
+            "POSITION_NAME": "职务",
+            "PERSON_DSE_RELATION": "变动人与董监高的关系",
+            "ORG_CODE": "-",
+            "GGEID": "-",
+            "BEGIN_HOLD_NUM": "开始时持有",
+            "END_HOLD_NUM": "结束后持有",
+        },
+        inplace=True,
+    )
+
+    temp_df = temp_df[
+        [
+            "日期",
+            "代码",
+            "名称",
+            "变动人",
+            "变动股数",
+            "成交均价",
+            "变动金额",
+            "变动原因",
+            "变动比例",
+            "变动后持股数",
+            "持股种类",
+            "董监高人员姓名",
+            "职务",
+            "变动人与董监高的关系",
+            "开始时持有",
+            "结束后持有",
+        ]
+    ]
+    temp_df["日期"] = pd.to_datetime(temp_df["日期"], errors="coerce").dt.date
+    temp_df["变动股数"] = pd.to_numeric(temp_df["变动股数"], errors="coerce")
     temp_df["成交均价"] = pd.to_numeric(temp_df["成交均价"], errors="coerce")
-    temp_df["期末市值"] = pd.to_numeric(temp_df["期末市值"], errors="coerce")
+    temp_df["变动金额"] = pd.to_numeric(temp_df["变动金额"], errors="coerce")
+    temp_df["变动比例"] = pd.to_numeric(temp_df["变动比例"], errors="coerce")
+    temp_df["变动后持股数"] = pd.to_numeric(temp_df["变动后持股数"], errors="coerce")
+    temp_df["开始时持有"] = pd.to_numeric(temp_df["开始时持有"], errors="coerce")
+    temp_df["结束后持有"] = pd.to_numeric(temp_df["结束后持有"], errors="coerce")
     return temp_df
 
 
 if __name__ == "__main__":
-    stock_hold_control_cninfo_df = stock_hold_control_cninfo(symbol="全部")
-    print(stock_hold_control_cninfo_df)
+    stock_hold_management_detail_em_df = stock_hold_management_detail_em()
+    print(stock_hold_management_detail_em_df)
 
-    stock_hold_management_detail_cninfo_df = stock_hold_management_detail_cninfo(
-        symbol="增持"
+    stock_hold_management_person_em_df = stock_hold_management_person_em(
+        symbol="001308", name="吴远"
     )
-    print(stock_hold_management_detail_cninfo_df)
+    print(stock_hold_management_person_em_df)
