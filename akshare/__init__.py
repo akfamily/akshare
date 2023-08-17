360ca440659589a83d3c226bc13c41570081d92e
Albert King <jindaxiang@163.com>
2023-08-17 14:38:48 +0800

    Date (#4059)
    * add version
    
    * fix stock_hot_rank_wc
    
    * update date

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2499,9 +2499,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.10.82 fix: fix currency_boc_sina interface
 1.10.83 add: add stock_industry_clf_hist_sw interface
 1.10.84 fix: fix stock_us_fundamental interface
+1.10.85 fix: fix stock_hot_rank_wc interface
 """
 
-__version__ = "1.10.84"
+__version__ = "1.10.85"
 __author__ = "AKFamily"
 
 import sys
