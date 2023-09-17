6ec05455988d1b49da51657ef426548358666fc9
Albert King <jindaxiang@163.com>
2023-09-17 17:02:11 +0800

    Date (#4125)
    * add version
    
    * fix news_economic_baidu
    
    * update date

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2521,9 +2521,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.11.5 fix: fix macro_china_hk_cpi interface
 1.11.6 fix: fix futures_main_sina interface
 1.11.7 fix: fix get_futures_daily interface
+1.11.8 fix: fix news_economic_baidu interface
 """
 
-__version__ = "1.11.7"
+__version__ = "1.11.8"
 __author__ = "AKFamily"
 
 import sys
