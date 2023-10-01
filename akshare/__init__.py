c9ac811f8dca11e67b33e6b74a7e1f1b4d02f355
jindaxiang <jindaxiang@163.com>
2023-10-01 13:19:46 +0800

    add version

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2535,9 +2535,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.11.19 fix: fix drewry_wci_index interface
 1.11.20 fix: fix option_dce_daily interface
 1.11.21 fix: fix get_dce_daily interface
+1.11.22 fix: fix option_dce_daily interface
 """
 
-__version__ = "1.11.21"
+__version__ = "1.11.22"
 __author__ = "AKFamily"
 
 import sys
