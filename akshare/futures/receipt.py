b30943691a549e18070a58b02be2341fcb725f60
jindaxiang <jindaxiang@163.com>
2023-11-13 23:04:28 +0800

    fix get_gfex_receipt

--- akshare/futures/receipt.py
+++ akshare/futures/receipt.py
@@ -457,5 +457,5 @@ def get_receipt(start_day: str = None, end_day: str = None, vars_list: List = co
 
 
 if __name__ == '__main__':
-    get_receipt_df = get_receipt(start_day='20230915', end_day='20230915')
+    get_receipt_df = get_receipt(start_day='20231113', end_day='20231113')
     print(get_receipt_df)
