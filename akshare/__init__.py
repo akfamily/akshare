843668d4d5238f45daf9787d12215e8dee023089
Albert King <jindaxiang@163.com>
2023-11-29 18:02:30 +0800

    Date (#4306)
    * add version 1.11.83
    
    * update date
    
    * fix stock_zt_pool_strong_em

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2596,9 +2596,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.11.80 fix: fix stock_cash_flow_sheet_by_report_em interface
 1.11.81 fix: fix stock_gdfx_free_holding_detail_em interface
 1.11.82 fix: fix bond_zh_us_rate interface
+1.11.83 fix: fix stock_zt_pool_strong_em interface
 """
 
-__version__ = "1.11.82"
+__version__ = "1.11.83"
 __author__ = "AKFamily"
 
 import sys
