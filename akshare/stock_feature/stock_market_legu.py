f564da9335515a5e2a70dbe5a4a387306710e62e
jindaxiang <jindaxiang@163.com>
2023-12-02 18:17:42 +0800

    fix stock_market_activity_legu

--- akshare/stock_feature/stock_market_legu.py
+++ akshare/stock_feature/stock_market_legu.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2022/11/22 11:25
+Date: 2023/12/2 18:00
 Desc: 乐咕乐股网-赚钱效应分析
 https://www.legulegu.com/stockdata/market-activity
 """
+from io import StringIO
+
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
@@ -19,18 +21,18 @@ def stock_market_activity_legu() -> pd.DataFrame:
     """
     url = "https://legulegu.com/stockdata/market-activity"
     r = requests.get(url)
-    temp_df = pd.read_html(r.text)[0]
+    temp_df = pd.read_html(StringIO(r.text))[0]
     temp_df_one = temp_df.iloc[:, :2]
     temp_df_one.columns = ["item", "value"]
     temp_df_two = temp_df.iloc[:, 2:4]
     temp_df_two.columns = ["item", "value"]
     temp_df_three = temp_df.iloc[:, 4:6]
     temp_df_three.columns = ["item", "value"]
-    temp_df = pd.concat([temp_df_one, temp_df_two, temp_df_three])
+    temp_df = pd.concat([temp_df_one, temp_df_two, temp_df_three], ignore_index=True)
     temp_df.dropna(how="all", axis=0, inplace=True)
     soup = BeautifulSoup(r.text, "lxml")
     item_str = soup.find("div", attrs={"class": "current-index"}).text
-    inner_temp_df = pd.DataFrame(item_str.split("：")).T
+    inner_temp_df = pd.DataFrame([item.strip() for item in item_str.split("：")]).T
     inner_temp_df.columns = ["item", "value"]
     temp_df = pd.concat([temp_df, inner_temp_df], ignore_index=True)
     item_str = soup.find("div", attrs={"class": "current-data"}).text.strip()
