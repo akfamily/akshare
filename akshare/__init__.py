bf7dc8b85f723ebed48ee8dfcf03fa183a213975
jindaxiang <jindaxiang@163.com>
2024-01-03 17:13:25 +0800

    add version 1.12.9

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2621,7 +2621,7 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.12.6 fix: fix fund_open_fund_info_em interface
 1.12.7 fix: fix option_gfex_daily interface
 1.12.8 fix: fix fund_open_fund_info_em interface
-1.12.9 add: add fund_individual_basic_info_xq, fund_individual_achievement_xq, fund_individual_analysis_xq, fund_individual_profit_probability_xq, fund_individual_detail_info_xq 
+1.12.9 add: add fund_individual_basic_info_xq interface
 """
 
 __version__ = "1.12.9"
