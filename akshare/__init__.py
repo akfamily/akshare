e29fa2c1f91fe08d56f60dd1882ee42f22d8e97b
Albert King <jindaxiang@163.com>
2024-01-07 00:06:20 +0800

    Date (#4397)
    * add version 1.12.15
    
    * fix stock_hk_profit_forecast_et
    
    * update date

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2627,9 +2627,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.12.12 fix: fix stock_zh_index_daily interface
 1.12.13 fix: fix index_stock_cons_csindex interface
 1.12.14 add: add stock_hk_profit_forecast_et interface
+1.12.15 fix: fix stock_hk_profit_forecast_et interface
 """
 
-__version__ = "1.12.14"
+__version__ = "1.12.15"
 __author__ = "AKFamily"
 
 import sys
