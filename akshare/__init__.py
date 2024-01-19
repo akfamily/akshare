99ef55b27559c11617919cc39b0c675338d0e13a
jindaxiang <jindaxiang@163.com>
2024-01-20 00:15:40 +0800

    fix futures_global_em

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2645,9 +2645,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.12.30 fix: fix futures_settlement_price_sgx interface
 1.12.31 add: add futures_global_em interface
 1.12.32 fix: fix energy_oil_hist interface
+1.12.33 fix: fix futures_global_em interface
 """
 
-__version__ = "1.12.32"
+__version__ = "1.12.33"
 __author__ = "AKFamily"
 
 import sys
