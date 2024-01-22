0085764a18f424ead6b7506171b27dbbc68aa72a
Albert King <jindaxiang@163.com>
2024-01-22 23:36:18 +0800

    Date (#4460)
    * add version 1.12.38
    
    * fix futures_index_ccidx
    
    * fix futures_to_spot_dce

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2650,9 +2650,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.12.35 fix: fix article_epu_index interface
 1.12.36 fix: fix bond_china_close_return interface
 1.12.37 fix: fix futures_delivery_shfe interface
+1.12.38 fix: fix futures_to_spot_dce interface
 """
 
-__version__ = "1.12.37"
+__version__ = "1.12.38"
 __author__ = "AKFamily"
 
 import sys
