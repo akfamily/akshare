b7558ca53adcc1b72b77bfbb6346975e9b7c9b89
jindaxiang <jindaxiang@163.com>
2024-01-22 18:42:52 +0800

    add futures_gfex_warehouse_receipt

--- akshare/futures/futures_warehouse_receipt.py
+++ akshare/futures/futures_warehouse_receipt.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2023/10/11 17:32
+Date: 2024/1/22 18:30
 Desc: 期货-仓单日报
+上海期货交易所-仓单日报
+https://www.shfe.com.cn/statements/dataview.html?paramid=dailystock
 郑州商品交易所-交易数据-仓单日报
 http://www.czce.com.cn/cn/jysj/cdrb/H770310index_1.htm
 大连商品交易所-行情数据-统计数据-日统计-仓单日报
 http://www.dce.com.cn/dalianshangpin/xqsj/tjsj26/rtj/cdrb/index.html
+广州期货交易所-行情数据-仓单日报
+http://www.gfex.com.cn/gfex/cdrb/hqsj_tjsj.shtml
 """
 import re
 from io import BytesIO, StringIO
@@ -15,16 +19,16 @@ import pandas as pd
 import requests
 
 
-def futures_czce_warehouse_receipt(trade_date: str = "20200702") -> dict:
+def futures_czce_warehouse_receipt(date: str = "20200702") -> dict:
     """
     郑州商品交易所-交易数据-仓单日报
     http://www.czce.com.cn/cn/jysj/cdrb/H770310index_1.htm
-    :param trade_date: 交易日, e.g., "20200702"
-    :type trade_date: str
+    :param date: 交易日, e.g., "20200702"
+    :type date: str
     :return: 指定日期的仓单日报数据
     :rtype: dict
     """
-    url = f"http://www.czce.com.cn/cn/DFSStaticFiles/Future/{trade_date[:4]}/{trade_date}/FutureDataWhsheet.xls"
+    url = f"http://www.czce.com.cn/cn/DFSStaticFiles/Future/{date[:4]}/{date}/FutureDataWhsheet.xls"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
     }
@@ -32,13 +36,13 @@ def futures_czce_warehouse_receipt(trade_date: str = "20200702") -> dict:
     temp_df = pd.read_excel(BytesIO(r.content))
     index_list = temp_df[
         temp_df.iloc[:, 0].str.find("品种") == 0.0
-    ].index.to_list()
+        ].index.to_list()
     index_list.append(len(temp_df))
     big_dict = {}
     for inner_index in range(len(index_list) - 1):
         inner_df = temp_df[
-            index_list[inner_index] : index_list[inner_index + 1]
-        ]
+                   index_list[inner_index]: index_list[inner_index + 1]
+                   ]
         inner_key = re.findall(r"[a-zA-Z]+", inner_df.iloc[0, 0])[0]
         inner_df = inner_df.iloc[1:, :]
         inner_df.dropna(axis=0, how="all", inplace=True)
@@ -50,12 +54,12 @@ def futures_czce_warehouse_receipt(trade_date: str = "20200702") -> dict:
     return big_dict
 
 
-def futures_dce_warehouse_receipt(trade_date: str = "20200702") -> dict:
+def futures_dce_warehouse_receipt(date: str = "20200702") -> dict:
     """
     大连商品交易所-行情数据-统计数据-日统计-仓单日报
     http://www.dce.com.cn/dalianshangpin/xqsj/tjsj26/rtj/cdrb/index.html
-    :param trade_date: 交易日, e.g., "20200702"
-    :type trade_date: str
+    :param date: 交易日, e.g., "20200702"
+    :type date: str
     :return: 指定日期的仓单日报数据
     :rtype: dict
     """
@@ -65,15 +69,15 @@ def futures_dce_warehouse_receipt(trade_date: str = "20200702") -> dict:
     }
     params = {
         "wbillWeeklyQuotes.variety": "all",
-        "year": trade_date[:4],
-        "month": str(int(trade_date[4:6]) - 1),
-        "day": trade_date[6:],
+        "year": date[:4],
+        "month": str(int(date[4:6]) - 1),
+        "day": date[6:],
     }
     r = requests.get(url, params=params, headers=headers)
     temp_df = pd.read_html(StringIO(r.text))[0]
     index_list = temp_df[
         temp_df.iloc[:, 0].str.contains("小计") == 1
-    ].index.to_list()
+        ].index.to_list()
     index_list.insert(0, 0)
     big_dict = {}
     for inner_index in range(len(index_list) - 1):
@@ -81,7 +85,7 @@ def futures_dce_warehouse_receipt(trade_date: str = "20200702") -> dict:
             temp_index = 0
         else:
             temp_index = index_list[inner_index] + 1
-        inner_df = temp_df[temp_index : index_list[inner_index + 1] + 1]
+        inner_df = temp_df[temp_index: index_list[inner_index + 1] + 1]
         inner_key = inner_df.iloc[0, 0]
         inner_df.reset_index(inplace=True, drop=True)
         inner_df = inner_df.ffill()
@@ -89,20 +93,20 @@ def futures_dce_warehouse_receipt(trade_date: str = "20200702") -> dict:
     return big_dict
 
 
-def futures_shfe_warehouse_receipt(trade_date: str = "20200702") -> dict:
+def futures_shfe_warehouse_receipt(date: str = "20200702") -> dict:
     """
     上海期货交易所指定交割仓库期货仓单日报
-    http://www.shfe.com.cn/statements/dataview.html?paramid=dailystock&paramdate=20200703
-    :param trade_date: 交易日, e.g., "20200702"
-    :type trade_date: str
+    https://www.shfe.com.cn/statements/dataview.html?paramid=dailystock&paramdate=20200703
+    :param date: 交易日, e.g., "20200702"
+    :type date: str
     :return: 指定日期的仓单日报数据
     :rtype: dict
     """
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
     }
-    url = f"http://www.shfe.com.cn/data/dailydata/{trade_date}dailystock.dat"
-    if trade_date >= "20140519":
+    url = f"http://www.shfe.com.cn/data/dailydata/{date}dailystock.dat"
+    if date >= "20140519":
         r = requests.get(url, headers=headers)
         data_json = r.json()
         temp_df = pd.DataFrame(data_json["o_cursor"])
@@ -119,12 +123,12 @@ def futures_shfe_warehouse_receipt(trade_date: str = "20200702") -> dict:
         for item in set(temp_df["VARNAME"]):
             big_dict[item] = temp_df[temp_df["VARNAME"] == item]
     else:
-        url = f"http://www.shfe.com.cn/data/dailydata/{trade_date}dailystock.html"
+        url = f"http://www.shfe.com.cn/data/dailydata/{date}dailystock.html"
         r = requests.get(url, headers=headers)
         temp_df = pd.read_html(StringIO(r.text))[0]
         index_list = temp_df[
             temp_df.iloc[:, 3].str.contains("单位：") == 1
-        ].index.to_list()
+            ].index.to_list()
         big_dict = {}
         for inner_index in range(len(index_list)):
             temp_index_start = index_list[inner_index]
@@ -145,23 +149,79 @@ def futures_shfe_warehouse_receipt(trade_date: str = "20200702") -> dict:
     return big_dict
 
 
+def futures_gfex_warehouse_receipt(date: str = "20240122") -> dict:
+    """
+    广州期货交易所-行情数据-仓单日报
+    http://www.gfex.com.cn/gfex/cdrb/hqsj_tjsj.shtml
+    :param date: 交易日, e.g., "20240122"
+    :type date: str
+    :return: 指定日期的仓单日报数据
+    :rtype: dict
+    """
+    url = "http://www.gfex.com.cn/u/interfacesWebTdWbillWeeklyQuotes/loadList"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
+    }
+    payload = {
+        "gen_date": date
+    }
+    r = requests.post(url=url, data=payload, headers=headers)
+    data_json = r.json()
+    temp_df = pd.DataFrame(data_json['data'])
+    symbol_list = list(set([item.upper() for item in temp_df['varietyOrder'].tolist() if item != ""]))
+    temp_df.rename(columns={
+        "varietyOrder": "symbol",
+        "variety": "品种",
+        "whAbbr": "仓库/分库",
+        "lastWbillQty": "昨日仓单量",
+        "regWbillQty": "增减",
+    }, inplace=True)
+    temp_df = temp_df[[
+        "symbol",
+        "whType",
+        "品种",
+        "仓库/分库",
+        "昨日仓单量",
+        "增减",
+    ]]
+    temp_df['whType'] = pd.to_numeric(temp_df['whType'], errors="coerce")
+    temp_df.dropna(subset=["whType"], how="any", axis=0, ignore_index=True, inplace=True)
+    big_dict = dict()
+    for symbol in symbol_list:
+        inner_temp_df = temp_df[temp_df['symbol'] == symbol.lower()].copy()
+        inner_temp_df = inner_temp_df[[
+            "品种",
+            "仓库/分库",
+            "昨日仓单量",
+            "增减",
+        ]]
+        inner_temp_df['昨日仓单量'] = pd.to_numeric(inner_temp_df['昨日仓单量'], errors="coerce")
+        inner_temp_df['增减'] = pd.to_numeric(inner_temp_df['增减'], errors="coerce")
+        inner_temp_df.reset_index(inplace=True, drop=True)
+        big_dict[symbol] = inner_temp_df
+    return big_dict
+
+
 if __name__ == "__main__":
     czce_warehouse_receipt_df = futures_czce_warehouse_receipt(
-        trade_date="20151019"
+        date="20151019"
     )
     print(czce_warehouse_receipt_df)
 
     futures_dce_warehouse_receipt_df = futures_dce_warehouse_receipt(
-        trade_date="20200702"
+        date="20200702"
     )
     print(futures_dce_warehouse_receipt_df)
 
     futures_shfe_warehouse_receipt_df = futures_shfe_warehouse_receipt(
-        trade_date="20200702"
+        date="20200702"
     )
     print(futures_shfe_warehouse_receipt_df)
 
     futures_shfe_warehouse_receipt_df = futures_shfe_warehouse_receipt(
-        trade_date="20140516"
+        date="20140516"
     )
     print(futures_shfe_warehouse_receipt_df)
+
+    futures_gfex_warehouse_receipt_df = futures_gfex_warehouse_receipt(date="20240122")
+    print(futures_gfex_warehouse_receipt_df)
