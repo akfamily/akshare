01d7bdeff07fedddc65dd17f8856ce08accda39e
Albert King <jindaxiang@163.com>
2024-01-23 17:31:55 +0800

    Date (#4463)
    * add version 1.12.40
    
    * fix futures_to_spot_dce

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2652,9 +2652,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.12.37 fix: fix futures_delivery_shfe interface
 1.12.38 fix: fix futures_to_spot_dce interface
 1.12.39 fix: fix futures_gfex_warehouse_receipt interface
+1.12.40 fix: fix futures_to_spot_dce interface
 """
 
-__version__ = "1.12.39"
+__version__ = "1.12.40"
 __author__ = "AKFamily"
 
 import sys
