41952ada8e619c74e9043aa4adf430202735a2b1
Albert King <jindaxiang@163.com>
2024-01-25 18:28:11 +0800

    Dev (#4468)
    * simplify macro_china_market_margin_sh (#4465)
    
    Co-authored-by: none2003 <forreg2003-0001@yahoo.com>
    
    * add version 1.12.43
    
    * fix macro_china_market_margin_sh
    
    ---------
    
    Co-authored-by: none2003 <forreg2003-0002@yahoo.com>
    Co-authored-by: none2003 <forreg2003-0001@yahoo.com>

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2655,9 +2655,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.12.40 fix: fix futures_to_spot_dce interface
 1.12.41 fix: fix sw_index_third_cons interface
 1.12.42 fix: fix stock_news_em interface
+1.12.43 fix: fix macro_china_market_margin_sh interface
 """
 
-__version__ = "1.12.42"
+__version__ = "1.12.43"
 __author__ = "AKFamily"
 
 import sys
