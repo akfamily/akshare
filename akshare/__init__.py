5c9d509931913767f877b1643124f45033be69e1
Albert King <jindaxiang@163.com>
2024-01-29 17:32:49 +0800

    Dev (#4476)
    * add version 1.12.46
    
    * fix futures_contract_detail
    
    * update date

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2658,9 +2658,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.12.43 fix: fix macro_china_market_margin_sh interface
 1.12.44 fix: fix stock_zh_ah_daily interface
 1.12.45 fix: fix stock_individual_spot_xq interface
+1.12.46 fix: fix futures_contract_detail interface
 """
 
-__version__ = "1.12.45"
+__version__ = "1.12.46"
 __author__ = "AKFamily"
 
 import sys
