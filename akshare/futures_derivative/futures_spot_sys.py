d1a45406f588e302280921db59640e80e2cdb9a7
jindaxiang <jindaxiang@163.com>
2024-02-24 16:50:46 +0800

    fix futures_spot_sys

--- akshare/futures_derivative/futures_spot_sys.py
+++ akshare/futures_derivative/futures_spot_sys.py
@@ -30,7 +30,8 @@ def __get_sys_spot_futures_dict() -> dict:
 
 def futures_spot_sys(symbol: str = "铜", indicator: str = "市场价格") -> pd.DataFrame:
     """
-    生意社-商品与期货-现期图: 图和表格
+    生意社-商品与期货-现期图
+    https://www.100ppi.com/sf/792.html
     :param symbol: 期货品种
     :type symbol: str
     :param indicator: 市场价格; choice of {"市场价格", "基差率", "主力基差"}
@@ -46,15 +47,24 @@ def futures_spot_sys(symbol: str = "铜", indicator: str = "市场价格") -> pd
         table_df_one['现货价格'] = pd.to_numeric(table_df_one['现货价格'], errors="coerce")
         table_df_one['主力合约'] = pd.to_numeric(table_df_one['主力合约'], errors="coerce")
         table_df_one['最近合约'] = pd.to_numeric(table_df_one['最近合约'], errors="coerce")
+        table_df_one.reset_index(inplace=True)
+        table_df_one.columns.name = None
+        table_df_one.rename(columns={"index": "日期"}, inplace=True)
         return table_df_one
     elif indicator == "基差率":
         table_df_two = pd.read_html(StringIO(r.text), header=0, index_col=0)[2].T
         table_df_two['基差率'] = table_df_two['基差率'].str.replace("%", "")
         table_df_two['基差率'] = pd.to_numeric(table_df_two['基差率'], errors="coerce")
+        table_df_two.reset_index(inplace=True)
+        table_df_two.columns.name = None
+        table_df_two.rename(columns={"index": "日期"}, inplace=True)
         return table_df_two
     else:
         table_df_three = pd.read_html(StringIO(r.text), header=0, index_col=0)[3].T
         table_df_three['主力基差'] = pd.to_numeric(table_df_three['主力基差'], errors="coerce")
+        table_df_three.reset_index(inplace=True)
+        table_df_three.columns.name = None
+        table_df_three.rename(columns={"index": "日期"}, inplace=True)
         return table_df_three
 
 
