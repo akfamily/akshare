613a1e8421ed6f6b2c9ed3dff5fa820adca3e3bf
Albert King <jindaxiang@163.com>
2024-02-27 17:31:23 +0800

    Date (#4537)
    * add version 1.12.72
    
    * add version 1.12.72
    
    * fix stock_hot_rank_wc
    
    * add futures_contract_info_shfe
    
    * update date

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2684,9 +2684,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.12.69 fix: fix futures_hold_pos_sina interface
 1.12.70 fix: fix stock_lhb_detail_daily_sina interface
 1.12.71 fix: fix stock_zh_ah_spot interface
+1.12.72 fix: fix stock_hot_rank_wc interface
 """
 
-__version__ = "1.12.71"
+__version__ = "1.12.72"
 __author__ = "AKFamily"
 
 import sys
@@ -2707,6 +2708,11 @@ if sys.version_info < (3, 9):
     )
 
 del sys
+
+"""
+上海期货交易所-交易所服务-业务数据-交易参数汇总查询
+"""
+from akshare.futures_derivative.futures_contract_info_shfe import futures_contract_info_shfe
 
 """
 上海期货交易所指定交割仓库库存周报
--- docs/tutorial.md
+++ docs/tutorial.md
@@ -1238,6 +1238,8 @@
  "futures_spot_sys"  # 生意社-商品与期货-现期图
  # 上海期货交易所指定交割仓库库存周报
  "futures_stock_shfe_js"  # 上海期货交易所指定交割仓库库存周报
+ # 期货合约信息
+ "futures_contract_info_shfe"  # 上海期货交易所-期货合约信息
 ```
 
 ## 案例演示
