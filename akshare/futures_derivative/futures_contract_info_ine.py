9f498db4e378b344c1dcedff2a4cf15d6ea8a243
jindaxiang <jindaxiang@163.com>
2024-02-28 19:10:35 +0800

    add futures_contract_info_ine

--- akshare/futures_derivative/futures_contract_info_ine.py
+++ akshare/futures_derivative/futures_contract_info_ine.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2024/2/27 17:00
-Desc: 上海期货交易所-交易所服务-业务数据-交易参数汇总查询
-https://www.shfe.com.cn/bourseService/businessdata/summaryinquiry/
+Date: 2024/2/28 19:00
+Desc: 上海国际能源交易中心-业务指南-交易参数汇总(期货)
+https://www.ine.cn/bourseService/summary/?name=currinstrumentprop
 """
 import pandas as pd
 import requests
 
 
-def futures_contract_info_shfe(date: str = "20240227") -> pd.DataFrame:
+def futures_contract_info_ine(date: str = "20240228") -> pd.DataFrame:
     """
-    上海期货交易所-交易所服务-业务数据-交易参数汇总查询
-    https://www.shfe.com.cn/bourseService/businessdata/summaryinquiry/
+    上海国际能源交易中心-业务指南-交易参数汇总(期货)
+    https://www.ine.cn/bourseService/summary/?name=currinstrumentprop
     :param date: 查询日期; 交易日
     :type date: str
     :return: 交易参数汇总查询
     :rtype: pandas.DataFrame
     """
-    url = f"https://www.shfe.com.cn/data/instrument/ContractBaseInfo{date}.dat"
-    r = requests.get(url)
+    url = f"https://www.ine.cn/data/instrument/ContractBaseInfo{date}.dat"
+    params = {
+        "rnd": "0.8312696798757147"
+    }
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+    }
+    r = requests.get(url, params=params, headers=headers)
     data_json = r.json()
     temp_df = pd.DataFrame(data_json['ContractBaseInfo'])
     temp_df.rename(columns={
@@ -30,7 +36,6 @@ def futures_contract_info_shfe(date: str = "20240227") -> pd.DataFrame:
         'OPENDATE': "上市日",
         'STARTDELIVDATE': "开始交割日",
         'TRADINGDAY': "交易日",
-        'UPDATE_DATE': "更新时间",
     }, inplace=True)
     temp_df = temp_df[[
         "合约代码",
@@ -40,7 +45,6 @@ def futures_contract_info_shfe(date: str = "20240227") -> pd.DataFrame:
         "最后交割日",
         "挂牌基准价",
         "交易日",
-        "更新时间",
     ]]
     temp_df['上市日'] = pd.to_datetime(temp_df['上市日'], errors="coerce").dt.date
     temp_df['到期日'] = pd.to_datetime(temp_df['到期日'], errors="coerce").dt.date
@@ -52,5 +56,5 @@ def futures_contract_info_shfe(date: str = "20240227") -> pd.DataFrame:
 
 
 if __name__ == '__main__':
-    futures_contract_info_shfe_df = futures_contract_info_shfe(date="20240227")
-    print(futures_contract_info_shfe_df)
+    futures_contract_info_ine_df = futures_contract_info_ine(date="20240227")
+    print(futures_contract_info_ine_df)
