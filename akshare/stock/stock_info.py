f756a53f710a79a2c932f42177fc0b922b36bc59
Albert King <jindaxiang@163.com>
2024-02-29 19:09:46 +0800

    Dev (#4554)
    * fix: fix stock_info_sh_delist interface (#4552)
    
    * add version 1.12.78
    
    * fix stock_info_sh_delist
    
    ---------
    
    Co-authored-by: Ben Bell <ben_29@live.com>

--- akshare/stock/stock_info.py
+++ akshare/stock/stock_info.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2023/2/19 19:00
+Date: 2024/2/29 18:50
 Desc: 股票基本信息
 """
 import json
 import warnings
 from functools import lru_cache
 from io import BytesIO
+from io import StringIO
 
 import pandas as pd
 import requests
-from tqdm import tqdm
+from akshare.utils.tqdm import get_tqdm
 
 
 @lru_cache()
 def stock_info_sz_name_code(symbol: str = "A股列表") -> pd.DataFrame:
     """
     深圳证券交易所-股票列表
-    http://www.szse.cn/market/product/stock/list/index.html
+    https://www.szse.cn/market/product/stock/list/index.html
     :param symbol: choice of {"A股列表", "B股列表", "CDR列表", "AB股列表"}
     :type symbol: str
     :return: 指定 indicator 的数据
     :rtype: pandas.DataFrame
     """
-    url = "http://www.szse.cn/api/report/ShowReport"
+    url = "https://www.szse.cn/api/report/ShowReport"
     indicator_map = {
         "A股列表": "tab1",
         "B股列表": "tab2",
@@ -120,18 +121,18 @@ def stock_info_sz_name_code(symbol: str = "A股列表") -> pd.DataFrame:
 def stock_info_sh_name_code(symbol: str = "主板A股") -> pd.DataFrame:
     """
     上海证券交易所-股票列表
-    http://www.sse.com.cn/assortment/stock/list/share/
+    https://www.sse.com.cn/assortment/stock/list/share/
     :param symbol: choice of {"主板A股": "1", "主板B股": "2", "科创板": "8"}
     :type symbol: str
     :return: 指定 indicator 的数据
     :rtype: pandas.DataFrame
     """
     indicator_map = {"主板A股": "1", "主板B股": "2", "科创板": "8"}
-    url = "http://query.sse.com.cn/sseQuery/commonQuery.do"
+    url = "https://query.sse.com.cn/sseQuery/commonQuery.do"
     headers = {
         "Host": "query.sse.com.cn",
         "Pragma": "no-cache",
-        "Referer": "http://www.sse.com.cn/assortment/stock/list/share/",
+        "Referer": "https://www.sse.com.cn/assortment/stock/list/share/",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
     }
     params = {
@@ -171,7 +172,7 @@ def stock_info_sh_name_code(symbol: str = "主板A股") -> pd.DataFrame:
             "上市日期",
         ]
     ]
-    temp_df["上市日期"] = pd.to_datetime(temp_df["上市日期"]).dt.date
+    temp_df["上市日期"] = pd.to_datetime(temp_df["上市日期"], errors="coerce").dt.date
     return temp_df
 
 
@@ -197,14 +198,15 @@ def stock_info_bj_name_code() -> pd.DataFrame:
     }
     r = requests.post(url, data=payload, headers=headers)
     data_text = r.text
-    data_json = json.loads(data_text[data_text.find("[") : -1])
+    data_json = json.loads(data_text[data_text.find("["): -1])
     total_page = data_json[0]["totalPages"]
     big_df = pd.DataFrame()
+    tqdm = get_tqdm()
     for page in tqdm(range(total_page), leave=False):
         payload.update({"page": page})
         r = requests.post(url, data=payload, headers=headers)
         data_text = r.text
-        data_json = json.loads(data_text[data_text.find("[") : -1])
+        data_json = json.loads(data_text[data_text.find("["): -1])
         temp_df = data_json[0]["content"]
         temp_df = pd.DataFrame(temp_df)
         big_df = pd.concat([big_df, temp_df], ignore_index=True)
@@ -275,14 +277,21 @@ def stock_info_bj_name_code() -> pd.DataFrame:
     return big_df
 
 
-def stock_info_sh_delist() -> pd.DataFrame:
+def stock_info_sh_delist(symbol: str = "全部") -> pd.DataFrame:
     """
     上海证券交易所-终止上市公司
     http://www.sse.com.cn/assortment/stock/list/delisting/
+    :param symbol: choice of {"全部", "沪市", "科创板"}
+    :type symbol: str
     :return: 终止上市公司
     :rtype: pandas.DataFrame
     """
-    url = "http://query.sse.com.cn/commonQuery.do"
+    symbol_map = {
+        "全部": "1,2,8",
+        "沪市": "1,2",
+        "科创板": "8",
+    }
+    url = "https://query.sse.com.cn/commonQuery.do"
     headers = {
         "Accept": "*/*",
         "Accept-Encoding": "gzip, deflate",
@@ -300,7 +309,7 @@ def stock_info_sh_delist() -> pd.DataFrame:
         "STOCK_CODE": "",
         "CSRC_CODE": "",
         "REG_PROVINCE": "",
-        "STOCK_TYPE": "1,2",
+        "STOCK_TYPE": symbol_map[symbol],
         "COMPANY_STATUS": "3",
         "type": "inParams",
         "pageHelp.cacheSize": "1",
@@ -330,22 +339,22 @@ def stock_info_sh_delist() -> pd.DataFrame:
             "暂停上市日期",
         ]
     ]
-    temp_df["上市日期"] = pd.to_datetime(temp_df["上市日期"]).dt.date
-    temp_df["暂停上市日期"] = pd.to_datetime(temp_df["暂停上市日期"]).dt.date
+    temp_df["上市日期"] = pd.to_datetime(temp_df["上市日期"], errors="coerce").dt.date
+    temp_df["暂停上市日期"] = pd.to_datetime(temp_df["暂停上市日期"], errors="coerce").dt.date
     return temp_df
 
 
 def stock_info_sz_delist(symbol: str = "暂停上市公司") -> pd.DataFrame:
     """
     深证证券交易所-暂停上市公司-终止上市公司
-    http://www.szse.cn/market/stock/suspend/index.html
+    https://www.szse.cn/market/stock/suspend/index.html
     :param symbol: choice of {"暂停上市公司", "终止上市公司"}
     :type symbol: str
     :return: 暂停上市公司 or 终止上市公司 的数据
     :rtype: pandas.DataFrame
     """
     indicator_map = {"暂停上市公司": "tab1", "终止上市公司": "tab2"}
-    url = "http://www.szse.cn/api/report/ShowReport"
+    url = "https://www.szse.cn/api/report/ShowReport"
     params = {
         "SHOWTYPE": "xlsx",
         "CATALOGID": "1793_ssgs",
@@ -367,14 +376,14 @@ def stock_info_sz_delist(symbol: str = "暂停上市公司") -> pd.DataFrame:
 def stock_info_sz_change_name(symbol: str = "全称变更") -> pd.DataFrame:
     """
     深证证券交易所-市场数据-股票数据-名称变更
-    http://www.szse.cn/www/market/stock/changename/index.html
+    https://www.szse.cn/www/market/stock/changename/index.html
     :param symbol: choice of {"全称变更": "tab1", "简称变更": "tab2"}
     :type symbol: str
     :return: 名称变更数据
     :rtype: pandas.DataFrame
     """
     indicator_map = {"全称变更": "tab1", "简称变更": "tab2"}
-    url = "http://www.szse.cn/api/report/ShowReport"
+    url = "https://www.szse.cn/api/report/ShowReport"
     params = {
         "SHOWTYPE": "xlsx",
         "CATALOGID": "SSGSGMXX",
@@ -394,15 +403,15 @@ def stock_info_sz_change_name(symbol: str = "全称变更") -> pd.DataFrame:
 def stock_info_change_name(symbol: str = "000503") -> pd.DataFrame:
     """
     新浪财经-股票曾用名
-    http://vip.stock.finance.sina.com.cn/corp/go.php/vCI_CorpInfo/stockid/300378.phtml
+    https://vip.stock.finance.sina.com.cn/corp/go.php/vCI_CorpInfo/stockid/300378.phtml
     :param symbol: 股票代码
     :type symbol: str
     :return: 股票曾用名
     :rtype: list
     """
-    url = f"http://vip.stock.finance.sina.com.cn/corp/go.php/vCI_CorpInfo/stockid/{symbol}.phtml"
+    url = f"https://vip.stock.finance.sina.com.cn/corp/go.php/vCI_CorpInfo/stockid/{symbol}.phtml"
     r = requests.get(url)
-    temp_df = pd.read_html(r.text)[3].iloc[:, :2]
+    temp_df = pd.read_html(StringIO(r.text))[3].iloc[:, :2]
     temp_df.dropna(inplace=True)
     temp_df.columns = ["item", "value"]
     temp_df["item"] = temp_df["item"].str.split("：", expand=True)[0]
