6df1d1ccef83eda5d13e69803dbf6fd43e9e4b68
jindaxiang <jindaxiang@163.com>
2024-03-11 17:56:42 +0800

    fix fortune_rank

--- akshare/fortune/fortune_500.py
+++ akshare/fortune/fortune_500.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2023/1/7 14:55
+Date: 2024/3/11 17:22
 Desc: 历年世界 500 强榜单数据
 https://www.fortunechina.com/fortune500/index.htm
 特殊情况说明：
 2010年由于网页端没有公布公司所属的国家, 故 2010 年数据没有国家这列
 """
 import json
+from functools import lru_cache
+from io import StringIO
 
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-from functools import lru_cache
 from tqdm import tqdm
 
 
@@ -24,13 +25,15 @@ def _fortune_rank_year_url_map() -> dict:
     :return: 年份和网址映射
     :rtype: dict
     """
-    url = "http://www.fortunechina.com/fortune500/index.htm"
+    url = "https://www.fortunechina.com/fortune500/index.htm"
     r = requests.get(url)
-    soup = BeautifulSoup(r.text, "lxml")
-    node_list = soup.find_all('div', attrs={"class": "swiper-slide"})
+    soup = BeautifulSoup(r.text, features="lxml")
+    url_2023 = soup.find(name='meta', attrs={"property": "og:url"})['content'].strip()
+    node_list = soup.find_all(name='div', attrs={"class": "swiper-slide"})
     url_list = [item.find("a")['href'] for item in node_list]
     year_list = [item.find("a").text for item in node_list]
     year_url_map = dict(zip(year_list, url_list))
+    year_url_map['2023'] = url_2023
     return year_url_map
 
 
@@ -46,36 +49,36 @@ def fortune_rank(year: str = "2015") -> pd.DataFrame:
     r = requests.get(url)
     r.encoding = "utf-8"
     if int(year) < 2007:
-        df = pd.read_html(r.text)[0].iloc[1:-1, ]
-        df.columns = pd.read_html(r.text)[0].iloc[0, :].tolist()
+        df = pd.read_html(StringIO(r.text))[0].iloc[1:-1, ]
+        df.columns = pd.read_html(StringIO(r.text))[0].iloc[0, :].tolist()
         return df
     elif 2006 < int(year) < 2010:
-        df = pd.read_html(r.text)[0].iloc[1:, ]
-        df.columns = pd.read_html(r.text)[0].iloc[0, :].tolist()
+        df = pd.read_html(StringIO(r.text))[0].iloc[1:, ]
+        df.columns = pd.read_html(StringIO(r.text))[0].iloc[0, :].tolist()
         for page in tqdm(range(2, 11), leave=False):
             # page =2
             r = requests.get(url.rsplit(".", maxsplit=1)[0] + "_" + str(page) + ".htm")
             r.encoding = "utf-8"
-            temp_df = pd.read_html(r.text)[0].iloc[1:, ]
-            temp_df.columns = pd.read_html(r.text)[0].iloc[0, :].tolist()
-            df = pd.concat([df, temp_df], ignore_index=True)
+            temp_df = pd.read_html(StringIO(r.text))[0].iloc[1:, ]
+            temp_df.columns = pd.read_html(StringIO(r.text))[0].iloc[0, :].tolist()
+            df = pd.concat(objs=[df, temp_df], ignore_index=True)
         return df
     else:
-        df = pd.read_html(r.text)[0]
+        df = pd.read_html(StringIO(r.text))[0]
         return df
 
 
-def fortune_rank_eng(year: str = "1995") -> pd.DataFrame:
+def fortune_rank_eng(year: str = "2023") -> pd.DataFrame:
     """
     注意你的网速
-    https://fortune.com/global500/
+    https://fortune.com/ranking/global500/
     https://fortune.com/global500/2012/search/
     :param year: "1995"
     :type year: str
     :return: 历年排名
     :rtype: pandas.DataFrame
     """
-    url = f"https://fortune.com/global500/{year}/search/"
+    url = f"https://fortune.com/ranking/global500/{year}/search/"
     res = requests.get(url)
     soup = BeautifulSoup(res.text, "lxml")
     code = json.loads(soup.find("script", attrs={"type": "application/ld+json"}).string)["identifier"]
@@ -90,7 +93,7 @@ def fortune_rank_eng(year: str = "1995") -> pd.DataFrame:
         temp_df = pd.DataFrame([item["fields"][i] for item in res.json()[1]["items"]])
         big_df[temp_df["key"].values[0]] = temp_df["value"]
     big_df["rank"] = big_df["rank"].astype(int)
-    big_df.sort_values("rank", inplace=True)
+    big_df.sort_values(by="rank", inplace=True)
     big_df.reset_index(drop=True, inplace=True)
     return big_df
 
@@ -98,6 +101,9 @@ def fortune_rank_eng(year: str = "1995") -> pd.DataFrame:
 if __name__ == '__main__':
     fortune_rank_eng_df = fortune_rank_eng(year="2022")
     print(fortune_rank_eng_df)
+
+    fortune_rank_df = fortune_rank(year='2023')  # 2010 不一样
+    print(fortune_rank_df)
 
     fortune_rank_df = fortune_rank(year='2022')  # 2010 不一样
     print(fortune_rank_df)
--- docs/data/others/others.md
+++ docs/data/others/others.md
@@ -1091,7 +1091,7 @@ print(air_quality_watch_point_df)
 
 接口: fortune_rank
 
-目标地址: http://www.fortunechina.com/fortune500/node_65.htm
+目标地址: https://www.fortunechina.com/fortune500/node_65.htm
 
 描述: 指定年份财富世界 500 强公司排行榜
 
@@ -1101,7 +1101,7 @@ print(air_quality_watch_point_df)
 
 | 名称   | 类型  | 描述          |
 |------|-----|-------------|
-| year | str | year="2022" |
+| year | str | year="2023" |
 
 输出参数
 
@@ -1114,25 +1114,26 @@ print(air_quality_watch_point_df)
 ```python
 import akshare as ak
 
-fortune_rank_df = ak.fortune_rank(year="2022")
+fortune_rank_df = ak.fortune_rank(year="2023")
 print(fortune_rank_df)
 ```
 
 数据示例
 
 ```
-      排名                                 公司名称(中文)  ...   国家  关键数据
-0      1                             沃尔玛（WALMART)  ...   美国     +
-1      2                          亚马逊（AMAZON.COM)  ...   美国     +
-2      3                     国家电网有限公司（STATE GRID)  ...   中国     +
-3      4  中国石油天然气集团有限公司（CHINA NATIONAL PETROLEUM)  ...   中国     +
-4      5              中国石油化工集团有限公司（SINOPEC GROUP)  ...   中国     +
-..   ...                                      ...  ...  ...   ...
-495  496                               DSV公司（DSV)  ...   丹麦     +
-496  497                             瑞士ABB集团（ABB)  ...   瑞士     +
-497  498             亿滋国际（MONDELEZ INTERNATIONAL)  ...   美国     +
-498  499                               达能（DANONE)  ...   法国     +
-499  500                           优美科公司（UMICORE)  ...  比利时     +
+      排名                                         公司名称(中文)  ...     国家  关键数据
+0      1                                       沃尔玛（WALMART)  ...     美国     +
+1      2                               沙特阿美公司（SAUDI ARAMCO)  ...  沙特阿拉伯     +
+2      3                               国家电网有限公司（STATE GRID)  ...     中国     +
+3      4                                    亚马逊（AMAZON.COM)  ...     美国     +
+4      5            中国石油天然气集团有限公司（CHINA NATIONAL PETROLEUM)  ...     中国     +
+..   ...                                                ...  ...    ...   ...
+495  496                     三星人寿保险（SAMSUNG LIFE INSURANCE)  ...     韩国     +
+496  497                  住友生命保险公司（SUMITOMO LIFE INSURANCE)  ...     日本     +
+497  498                                   CarMax公司（CARMAX)  ...     美国     +
+498  499         日本三菱重工业股份有限公司（MITSUBISHI HEAVY INDUSTRIES)  ...     日本     +
+499  500  新疆广汇实业投资（集团）有限责任公司（XINJIANG GUANGHUI INDUSTRY ...  ...     中国     +
+[500 rows x 6 columns]
 ```
 
 ### 财富排行榜-英文
