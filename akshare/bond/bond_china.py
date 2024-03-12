6c477e567910c226b02ccc1ab07cb8762260c00b
Albert King <jindaxiang@163.com>
2024-03-12 15:05:57 +0800

    Date (#4594)
    * add version 1.12.89
    
    * fix bond_china_yield

--- akshare/bond/bond_china.py
+++ akshare/bond/bond_china.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2023/4/23 17:24
+Date: 2024/3/12 14:00
 Desc: 中国外汇交易中心暨全国银行间同业拆借中心
 中国外汇交易中心暨全国银行间同业拆借中心-市场数据-债券市场行情-现券市场做市报价
 中国外汇交易中心暨全国银行间同业拆借中心-市场数据-债券市场行情-现券市场成交行情
-http://www.chinamoney.com.cn/chinese/mkdatabond/
+https://www.chinamoney.com.cn/chinese/mkdatabond/
 """
+from io import StringIO
+
 import pandas as pd
 import requests
+
+from akshare.bond.bond_china_money import bond_china_close_return_map
 
 
 def bond_spot_quote() -> pd.DataFrame:
     """
     中国外汇交易中心暨全国银行间同业拆借中心-市场数据-债券市场行情-现券市场做市报价
-    http://www.chinamoney.com.cn/chinese/mkdatabond/
+    https://www.chinamoney.com.cn/chinese/mkdatabond/
     :return: 现券市场做市报价
     :rtype: pandas.DataFrame
     """
-    url = "http://www.chinamoney.com.cn/ags/ms/cm-u-md-bond/CbMktMakQuot"
+    bond_china_close_return_map()
+    url = "https://www.chinamoney.com.cn/ags/ms/cm-u-md-bond/CbMktMakQuot"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
     }
@@ -62,21 +67,21 @@ def bond_spot_quote() -> pd.DataFrame:
     temp_df["卖出收益率"] = temp_df["买入/卖出收益率"].str.split("/", expand=True).iloc[:, 1]
     del temp_df["买入/卖出净价"]
     del temp_df["买入/卖出收益率"]
-    temp_df['买入净价'] = pd.to_numeric(temp_df['买入净价'])
-    temp_df['卖出净价'] = pd.to_numeric(temp_df['卖出净价'])
-    temp_df['买入收益率'] = pd.to_numeric(temp_df['买入收益率'])
-    temp_df['卖出收益率'] = pd.to_numeric(temp_df['卖出收益率'])
+    temp_df['买入净价'] = pd.to_numeric(temp_df['买入净价'], errors="coerce")
+    temp_df['卖出净价'] = pd.to_numeric(temp_df['卖出净价'], errors="coerce")
+    temp_df['买入收益率'] = pd.to_numeric(temp_df['买入收益率'], errors="coerce")
+    temp_df['卖出收益率'] = pd.to_numeric(temp_df['卖出收益率'], errors="coerce")
     return temp_df
 
 
 def bond_spot_deal() -> pd.DataFrame:
     """
     中国外汇交易中心暨全国银行间同业拆借中心-市场数据-债券市场行情-现券市场成交行情
-    http://www.chinamoney.com.cn/chinese/mkdatabond/
+    https://www.chinamoney.com.cn/chinese/mkdatabond/
     :return: 现券市场成交行情
     :rtype: pandas.DataFrame
     """
-    url = "http://www.chinamoney.com.cn/ags/ms/cm-u-md-bond/CbtPri"
+    url = "https://www.chinamoney.com.cn/ags/ms/cm-u-md-bond/CbtPri"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
     }
@@ -110,6 +115,7 @@ def bond_spot_deal() -> pd.DataFrame:
         "_",
         "_",
         "_",
+        "_",
     ]
     temp_df = temp_df[
         [
@@ -130,12 +136,12 @@ def bond_spot_deal() -> pd.DataFrame:
 
 
 def bond_china_yield(
-    start_date: str = "20200204", end_date: str = "20210124"
+        start_date: str = "20200204", end_date: str = "20210124"
 ) -> pd.DataFrame:
     """
     中国债券信息网-国债及其他债券收益率曲线
     https://www.chinabond.com.cn/
-    http://yield.chinabond.com.cn/cbweb-pbc-web/pbc/historyQuery?startDate=2019-02-07&endDate=2020-02-04&gjqx=0&qxId=ycqx&locale=cn_ZH
+    https://yield.chinabond.com.cn/cbweb-pbc-web/pbc/historyQuery?startDate=2019-02-07&endDate=2020-02-04&gjqx=0&qxId=ycqx&locale=cn_ZH
     注意: end_date - start_date 应该小于一年
     :param start_date: 需要查询的日期, 返回在该日期之后一年内的数据
     :type start_date: str
@@ -144,7 +150,7 @@ def bond_china_yield(
     :return: 返回在指定日期之间之前一年内的数据
     :rtype: pandas.DataFrame
     """
-    url = "http://yield.chinabond.com.cn/cbweb-pbc-web/pbc/historyQuery"
+    url = "https://yield.chinabond.com.cn/cbweb-pbc-web/pbc/historyQuery"
     params = {
         "startDate": '-'.join([start_date[:4], start_date[4:6], start_date[6:]]),
         "endDate": '-'.join([end_date[:4], end_date[4:6], end_date[6:]]),
@@ -157,8 +163,8 @@ def bond_china_yield(
     }
     res = requests.get(url, params=params, headers=headers)
     data_text = res.text.replace("&nbsp", "")
-    data_df = pd.read_html(data_text, header=0)[1]
-    data_df['日期'] = pd.to_datetime(data_df['日期']).dt.date
+    data_df = pd.read_html(StringIO(data_text), header=0)[1]
+    data_df['日期'] = pd.to_datetime(data_df['日期'], errors="coerce").dt.date
     data_df['3月'] = pd.to_numeric(data_df['3月'], errors="coerce")
     data_df['6月'] = pd.to_numeric(data_df['6月'], errors="coerce")
     data_df['1年'] = pd.to_numeric(data_df['1年'], errors="coerce")
@@ -167,7 +173,7 @@ def bond_china_yield(
     data_df['7年'] = pd.to_numeric(data_df['7年'], errors="coerce")
     data_df['10年'] = pd.to_numeric(data_df['10年'], errors="coerce")
     data_df['30年'] = pd.to_numeric(data_df['30年'], errors="coerce")
-    data_df.sort_values('日期', inplace=True)
+    data_df.sort_values(by='日期', inplace=True)
     data_df.reset_index(inplace=True, drop=True)
     return data_df
 
