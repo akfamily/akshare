694680d76173ff7c3ce43f3cc72dff7fb1c3483d
albertandking <jindaxiang@163.com>
2024-03-25 13:00:54 +0800

    feat: add version 1.13.5

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2716,9 +2716,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.13.2 add: add stock_main_fund_flow interface
 1.13.3 fix: fix stock_main_fund_flow interface
 1.13.4 fix: fix stock_individual_spot_xq interface
+1.13.5 fix: fix stock_main_fund_flow interface
 """
 
-__version__ = "1.13.4"
+__version__ = "1.13.5"
 __author__ = "AKFamily"
 
 import sys
