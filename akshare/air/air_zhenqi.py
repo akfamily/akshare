e7ae4678fe728a901f6dd65d4492af759c99f4ed
jindaxiang <jindaxiang@163.com>
2024-04-02 22:58:36 +0800

    fix: fix air_quality_hist

--- akshare/air/air_zhenqi.py
+++ akshare/air/air_zhenqi.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2022/2/24 14:50
+Date: 2024/4/2 22:40
 Desc: 真气网-空气质量
 https://www.zq12369.com/environment.php
 空气质量在线监测分析平台的空气质量数据
 https://www.aqistudy.cn/
 """
+
 import json
 import os
 import re
@@ -29,9 +30,7 @@ def _get_js_path(name: str = None, module_file: str = None) -> str:
     :return: 路径
     :rtype: str
     """
-    module_folder = os.path.abspath(
-        os.path.dirname(os.path.dirname(module_file))
-    )
+    module_folder = os.path.abspath(os.path.dirname(os.path.dirname(module_file)))
     module_json_path = os.path.join(module_folder, "air", name)
     return module_json_path
 
@@ -71,6 +70,7 @@ def air_city_table() -> pd.DataFrame:
     """
     url = "https://www.zq12369.com/environment.php"
     date = "2020-05-01"
+    temp_df = None
     if len(date.split("-")) == 3:
         params = {
             "date": date,
@@ -83,13 +83,21 @@ def air_city_table() -> pd.DataFrame:
         del temp_df["降序"]
         temp_df.reset_index(inplace=True)
         temp_df["index"] = temp_df.index + 1
-        temp_df.columns = ["序号", "省份", "城市", "AQI", "空气质量", "PM2.5浓度", "首要污染物"]
+        temp_df.columns = [
+            "序号",
+            "省份",
+            "城市",
+            "AQI",
+            "空气质量",
+            "PM2.5浓度",
+            "首要污染物",
+        ]
         temp_df["AQI"] = pd.to_numeric(temp_df["AQI"])
     return temp_df
 
 
 def air_quality_watch_point(
-        city: str = "杭州", start_date: str = "20220408", end_date: str = "20220409"
+    city: str = "杭州", start_date: str = "20220408", end_date: str = "20220409"
 ) -> pd.DataFrame:
     """
     真气网-监测点空气质量-细化到具体城市的每个监测点
@@ -122,12 +130,11 @@ def air_quality_watch_point(
         "city": city_param,
         "startTime": ctx.call("encode_param", start_date),
         "endTime": ctx.call("encode_param", end_date),
-        "secret": ctx.call(
-            "encode_secret", method, city_param, start_date, end_date
-        ),
+        "secret": ctx.call("encode_secret", method, city_param, start_date, end_date),
     }
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/81.0.4044.122 Safari/537.36"
     }
     r = requests.post(url, data=payload, headers=headers)
     data_text = r.text
@@ -137,10 +144,10 @@ def air_quality_watch_point(
 
 
 def air_quality_hist(
-        city: str = "杭州",
-        period: str = "day",
-        start_date: str = "20190327",
-        end_date: str = "20200427",
+    city: str = "杭州",
+    period: str = "day",
+    start_date: str = "20190327",
+    end_date: str = "20200427",
 ) -> pd.DataFrame:
     """
     真气网-空气历史数据
@@ -162,7 +169,7 @@ def air_quality_hist(
     file_data = _get_file_content(file_name="outcrypto.js")
     ctx = py_mini_racer.MiniRacer()
     ctx.eval(file_data)
-    appId = "4f0e3a273d547ce6b7147bfa7ceb4b6e"
+    app_id = "4f0e3a273d547ce6b7147bfa7ceb4b6e"
     method = "CETCITYPERIOD"
     timestamp = ctx.eval("timestamp = new Date().getTime()")
     p_text = json.dumps(
@@ -175,9 +182,7 @@ def air_quality_hist(
         ensure_ascii=False,
         indent=None,
     ).replace(' "', '"')
-    secret = ctx.call(
-        "hex_md5", appId + method + str(timestamp) + "WEB" + p_text
-    )
+    secret = ctx.call("hex_md5", app_id + method + str(timestamp) + "WEB" + p_text)
     payload = {
         "appId": "4f0e3a273d547ce6b7147bfa7ceb4b6e",
         "method": "CETCITYPERIOD",
@@ -200,30 +205,11 @@ def air_quality_hist(
     )
 
     headers = {
-        # 'Accept': '*/*',
-        # 'Accept-Encoding': 'gzip, deflate, br',
-        # 'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-        # 'Cache-Control': 'no-cache',
-        # 'Connection': 'keep-alive',
-        # 'Content-Length': '1174',
-        # 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        # 'Cookie': 'UM_distinctid=1800e5142c5b85-04b8f11aa852f3-1a343370-1fa400-1800e5142c6b7e; CNZZDATA1254317176=1502593570-1649496979-%7C1649507817; city=%E6%9D%AD%E5%B7%9E; SECKEY_ABVK=eSrbUhd28Mjo7jf8Rfh+uY5E9C+tAhQ8mOfYJHSjSfY%3D; BMAP_SECKEY=N5fGcwdWpeJW46eZRpR9GW3qdVnODGQwGm6JE0ELECQHJOTFc9MCuNdyf8OWUspFI6Xq4MMPxgVVr5I13odFOW6AQMgSPOtEvVHciC2NsQwb1pnmFtEaqyKHOUeavelt0ejBy6ETRD_4FXAhZb9FSbVIMPew7qwFX_kdPDxVJH-vHfCVhRx9XDZgb41B_T4D',
-        # 'Host': 'www.zq12369.com',
-        # 'Origin': 'https://www.zq12369.com',
-        # 'Pragma': 'no-cache',
-        # 'Referer': 'https://www.zq12369.com/environment.php?catid=4',
-        # 'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
-        # 'sec-ch-ua-mobile': '?0',
-        # 'sec-ch-ua-platform': '"Windows"',
-        # 'Sec-Fetch-Dest': 'empty',
-        # 'Sec-Fetch-Mode': 'cors',
-        # 'Sec-Fetch-Site': 'same-origin',
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
+        "Chrome/100.0.4896.75 Safari/537.36",
         "X-Requested-With": "XMLHttpRequest",
     }
-    params = {"param": ctx.call("AES.encrypt", need)}
     params = {"param": ctx.call("encode_param", need)}
-
     r = requests.post(url, data=params, headers=headers)
     temp_text = ctx.call("decryptData", r.text)
     data_json = demjson.decode(ctx.call("b.decode", temp_text))
@@ -304,7 +290,7 @@ if __name__ == "__main__":
         city="北京",
         period="day",
         start_date="20220801",
-        end_date="20220816",
+        end_date="20240402",
     )
     print(air_quality_hist_df)
 
