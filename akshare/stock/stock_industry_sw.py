6b98dfbcde389a28e70e7b0aa2cff7be79560dc3
jindaxiang <jindaxiang@163.com>
2024-04-03 16:38:05 +0800

    fix: fix stock_industry_clf_hist_sw

--- akshare/stock/stock_industry_sw.py
+++ akshare/stock/stock_industry_sw.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2023/8/14 16:20
+Date: 2024/4/3 16:20
 Desc: 申万宏源研究-行业分类
 https://www.swhyresearch.com/institute_sw/allIndex/downloadCenter/industryType
 """
+
 import io
-from datetime import datetime
 
 import pandas as pd
 import requests
@@ -23,10 +23,13 @@ def stock_industry_clf_hist_sw() -> pd.DataFrame:
         "https://www.swsresearch.com/swindex/pdf/SwClass2021/StockClassifyUse_stock.xls"
     )
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/102.0.0.0 Safari/537.36"
     }
     r = requests.get(url, headers=headers)
-    temp_df = pd.read_excel(io.BytesIO(r.content), dtype={"股票代码": "str", "行业代码": "str"})
+    temp_df = pd.read_excel(
+        io.BytesIO(r.content), dtype={"股票代码": "str", "行业代码": "str"}
+    )
     temp_df.rename(
         columns={
             "股票代码": "symbol",
