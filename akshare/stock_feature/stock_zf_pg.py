2e04516c16449338a11c1368d25a37fbbc452bd4
jindaxiang <jindaxiang@163.com>
2024-04-04 18:35:55 +0800

    fix: fix stock_pg_em

--- akshare/stock_feature/stock_zf_pg.py
+++ akshare/stock_feature/stock_zf_pg.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2022/1/14 14:23
+Date: 2024/4/4 18:20
 Desc: 增发和配股
 东方财富网-数据中心-新股数据-增发-全部增发
-http://data.eastmoney.com/other/gkzf.html
+https://data.eastmoney.com/other/gkzf.html
 东方财富网-数据中心-新股数据-配股
-http://data.eastmoney.com/xg/pg/
+https://data.eastmoney.com/xg/pg/
 """
+
 import pandas as pd
 import requests
-from tqdm import tqdm
+
+from akshare.utils.tqdm import get_tqdm
 
 
 def stock_qbzf_em() -> pd.DataFrame:
@@ -20,6 +22,9 @@ def stock_qbzf_em() -> pd.DataFrame:
     :return: 全部增发
     :rtype: pandas.DataFrame
     """
+    import warnings
+
+    warnings.filterwarnings(action="ignore", category=FutureWarning)
     url = "https://datacenter-web.eastmoney.com/api/data/v1/get"
     params = {
         "sortColumns": "ISSUE_DATE",
@@ -35,30 +40,36 @@ def stock_qbzf_em() -> pd.DataFrame:
     }
     r = requests.get(url, params=params)
     data_json = r.json()
-    total_page = data_json['result']["pages"]
+    total_page = data_json["result"]["pages"]
     big_df = pd.DataFrame()
+    tqdm = get_tqdm()
     for page in tqdm(range(1, total_page + 1), leave=False):
-        params.update({
-            "pageNumber": page,
-        })
+        params.update(
+            {
+                "pageNumber": page,
+            }
+        )
         r = requests.get(url, params=params)
         data_json = r.json()
-        temp_df = pd.DataFrame(data_json['result']["data"])
-        big_df = pd.concat([big_df, temp_df], ignore_index=True)
+        temp_df = pd.DataFrame(data_json["result"]["data"])
+        big_df = pd.concat(objs=[big_df, temp_df], ignore_index=True)
 
-    big_df.rename(columns={
-        "SECURITY_NAME_ABBR": "股票简称",
-        "SECURITY_CODE": "股票代码",
-        "CORRECODE": "增发代码",
-        "SEO_TYPE": "发行方式",
-        "ISSUE_NUM": "发行总数",
-        "ONLINE_ISSUE_NUM": "网上发行",
-        "ISSUE_PRICE": "发行价格",
-        "NEW_PRICE": "最新价",
-        "ISSUE_DATE": "发行日期",
-        "ISSUE_LISTING_DATE": "增发上市日期",
-        "LOCKIN_PERIOD": "锁定期",
-    }, inplace=True)
+    big_df.rename(
+        columns={
+            "SECURITY_NAME_ABBR": "股票简称",
+            "SECURITY_CODE": "股票代码",
+            "CORRECODE": "增发代码",
+            "SEO_TYPE": "发行方式",
+            "ISSUE_NUM": "发行总数",
+            "ONLINE_ISSUE_NUM": "网上发行",
+            "ISSUE_PRICE": "发行价格",
+            "NEW_PRICE": "最新价",
+            "ISSUE_DATE": "发行日期",
+            "ISSUE_LISTING_DATE": "增发上市日期",
+            "LOCKIN_PERIOD": "锁定期",
+        },
+        inplace=True,
+    )
     big_df = big_df[
         [
             "股票代码",
@@ -77,9 +88,11 @@ def stock_qbzf_em() -> pd.DataFrame:
     big_df["发行总数"] = pd.to_numeric(big_df["发行总数"], errors="coerce")
     big_df["发行价格"] = pd.to_numeric(big_df["发行价格"], errors="coerce")
     big_df["最新价"] = pd.to_numeric(big_df["最新价"], errors="coerce")
-    big_df['发行方式'] = big_df['发行方式'].map({'2': "公开增发", "1": "定向增发"})
-    big_df["发行日期"] = pd.to_datetime(big_df["发行日期"]).dt.date
-    big_df["增发上市日期"] = pd.to_datetime(big_df["增发上市日期"]).dt.date
+    big_df["发行方式"] = big_df["发行方式"].map({"2": "公开增发", "1": "定向增发"})
+    big_df["发行日期"] = pd.to_datetime(big_df["发行日期"], errors="coerce").dt.date
+    big_df["增发上市日期"] = pd.to_datetime(
+        big_df["增发上市日期"], errors="coerce"
+    ).dt.date
     return big_df
 
 
@@ -90,6 +103,9 @@ def stock_pg_em() -> pd.DataFrame:
     :return: 配股
     :rtype: pandas.DataFrame
     """
+    import warnings
+
+    warnings.filterwarnings(action="ignore", category=FutureWarning)
     url = "https://datacenter-web.eastmoney.com/api/data/v1/get"
     params = {
         "sortColumns": "EQUITY_RECORD_DATE",
@@ -105,16 +121,19 @@ def stock_pg_em() -> pd.DataFrame:
     }
     r = requests.get(url, params=params)
     data_json = r.json()
-    total_page = data_json['result']["pages"]
+    total_page = data_json["result"]["pages"]
     big_df = pd.DataFrame()
-    for page in tqdm(range(1, total_page+1), leave=False):
-        params.update({
-            "pageNumber": page,
-        })
+    tqdm = get_tqdm()
+    for page in tqdm(range(1, total_page + 1), leave=False):
+        params.update(
+            {
+                "pageNumber": page,
+            }
+        )
         r = requests.get(url, params=params)
         data_json = r.json()
-        temp_df = pd.DataFrame(data_json['result']["data"])
-        big_df = pd.concat([big_df, temp_df], ignore_index=True)
+        temp_df = pd.DataFrame(data_json["result"]["data"])
+        big_df = pd.concat(objs=[big_df, temp_df], ignore_index=True)
     big_df.columns = [
         "_",
         "_",
@@ -163,16 +182,20 @@ def stock_pg_em() -> pd.DataFrame:
     big_df["最新价"] = pd.to_numeric(big_df["最新价"], errors="coerce")
     big_df["配股前总股本"] = pd.to_numeric(big_df["配股前总股本"], errors="coerce")
     big_df["配股后总股本"] = pd.to_numeric(big_df["配股后总股本"], errors="coerce")
-    big_df["股权登记日"] = pd.to_datetime(big_df["股权登记日"]).dt.date
-    big_df["缴款起始日期"] = pd.to_datetime(big_df["缴款起始日期"]).dt.date
-    big_df["缴款截止日期"] = pd.to_datetime(big_df["缴款截止日期"]).dt.date
-    big_df["上市日"] = pd.to_datetime(big_df["上市日"]).dt.date
+    big_df["股权登记日"] = pd.to_datetime(big_df["股权登记日"], errors="coerce").dt.date
+    big_df["缴款起始日期"] = pd.to_datetime(
+        big_df["缴款起始日期"], errors="coerce"
+    ).dt.date
+    big_df["缴款截止日期"] = pd.to_datetime(
+        big_df["缴款截止日期"], errors="coerce"
+    ).dt.date
+    big_df["上市日"] = pd.to_datetime(big_df["上市日"], errors="coerce").dt.date
     return big_df
 
 
 if __name__ == "__main__":
     stock_qbzf_em_df = stock_qbzf_em()
-    print(stock_qbzf_em_df.info())
+    print(stock_qbzf_em_df)
 
     stock_pg_em_df = stock_pg_em()
     print(stock_pg_em_df)
