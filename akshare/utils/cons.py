00d7531aa06e3886bde9262e333c359379d55dc9
jindaxiang <jindaxiang@163.com>
2024-04-11 17:53:46 +0800

    feat: add cons.py

---
+++ akshare/utils/cons.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python
+"""
+Date: 2024/4/7 15:30
+Desc: 通用变量
+"""
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/114.0.0.0 Safari/537.36"
+}
