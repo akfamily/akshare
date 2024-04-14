f36508b8ab220da798e8c342fdf0aba8017b6aee
jindaxiang <jindaxiang@163.com>
2024-04-14 16:48:15 +0800

    feat: add version 1.13.36

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2747,9 +2747,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.13.33 fix: fix index_realtime_fund_sw interface
 1.13.34 fix: fix bank_fjcf_table_detail interface
 1.13.35 fix: fix stock_margin_szse interface
+1.13.36 fix: fix stock_hsgt_hist_em interface
 """
 
-__version__ = "1.13.35"
+__version__ = "1.13.36"
 __author__ = "AKFamily"
 
 import sys
@@ -4421,12 +4422,6 @@ stock-em-hsgt
 """
 from akshare.stock_feature.stock_hsgt_em import (
     stock_hk_ggt_components_em,
-    stock_hsgt_north_acc_flow_in_em,
-    stock_hsgt_north_cash_em,
-    stock_hsgt_north_net_flow_in_em,
-    stock_hsgt_south_acc_flow_in_em,
-    stock_hsgt_south_cash_em,
-    stock_hsgt_south_net_flow_in_em,
     stock_hsgt_hold_stock_em,
     stock_hsgt_hist_em,
     stock_hsgt_institution_statistics_em,
