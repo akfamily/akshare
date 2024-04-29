4706797b06429bf8e2dd893f757c3717b61f03b8
jindaxiang <jindaxiang@163.com>
2024-04-29 18:34:00 +0800

    fix: fix currency_boc_safe

--- akshare/currency/currency_safe.py
+++ akshare/currency/currency_safe.py
@@ -1,10 +1,11 @@
 # -*- coding:utf-8 -*-
 # !/usr/bin/env python
 """
-Date: 2023/9/17 17:08
+Date: 2024/4/29 17:00
 Desc: 人民币汇率中间价
 https://www.safe.gov.cn/safe/rmbhlzjj/index.html
 """
+
 import re
 from datetime import datetime
 from io import StringIO
@@ -24,11 +25,11 @@ def currency_boc_safe() -> pd.DataFrame:
     url = "https://www.safe.gov.cn/safe/2020/1218/17833.html"
     r = requests.get(url)
     r.encoding = "utf8"
-    soup = BeautifulSoup(r.text, "lxml")
-    content = soup.find("a", string=re.compile("人民币汇率"))["href"]
+    soup = BeautifulSoup(r.text, features="lxml")
+    content = soup.find(name="a", string=re.compile("人民币汇率"))["href"]
     url = f"https://www.safe.gov.cn{content}"
     temp_df = pd.read_excel(url)
-    temp_df.sort_values(["日期"], inplace=True)
+    temp_df.sort_values(by=["日期"], inplace=True)
     temp_df.reset_index(inplace=True, drop=True)
     start_date = (
         (pd.Timestamp(temp_df["日期"].tolist()[-1]) + pd.Timedelta(days=1))
@@ -44,9 +45,9 @@ def currency_boc_safe() -> pd.DataFrame:
     }
     r = requests.post(url, data=payload)
     current_temp_df = pd.read_html(StringIO(r.text))[-1]
-    current_temp_df.sort_values(["日期"], inplace=True)
+    current_temp_df.sort_values(by=["日期"], inplace=True)
     current_temp_df.reset_index(inplace=True, drop=True)
-    big_df = pd.concat([temp_df, current_temp_df], ignore_index=True)
+    big_df = pd.concat(objs=[temp_df, current_temp_df], ignore_index=True)
     column_name_list = big_df.columns[1:]
     for item in column_name_list:
         big_df[item] = pd.to_numeric(big_df[item], errors="coerce")
