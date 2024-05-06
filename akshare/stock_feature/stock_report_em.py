cc20958d1acdd15ef5fcda95ae1a3523cfc48053
jindaxiang <jindaxiang@163.com>
2024-05-06 17:57:22 +0800

    fix: fix stock_zcfz_em

--- akshare/stock_feature/stock_report_em.py
+++ akshare/stock_feature/stock_report_em.py
@@ -195,7 +195,7 @@ def stock_lrb_em(date: str = "20081231") -> pd.DataFrame:
         r = requests.get(url, params=params)
         data_json = r.json()
         temp_df = pd.DataFrame(data_json["result"]["data"])
-        big_df = pd.concat([big_df, temp_df], ignore_index=True)
+        big_df = pd.concat(objs=[big_df, temp_df], ignore_index=True)
 
     big_df.reset_index(inplace=True)
     big_df["index"] = big_df.index + 1
