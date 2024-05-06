942487b1e95d40ddceadd9dde2fa6c88d427bb00
jindaxiang <jindaxiang@163.com>
2024-05-06 17:55:56 +0800

    fix: fix index_detail_hist_cni

--- akshare/stock_feature/stock_board_industry_ths.py
+++ akshare/stock_feature/stock_board_industry_ths.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2024/1/8 17:20
+Date: 2024/5/6 17:30
 Desc: 同花顺-板块-行业板块
 https://q.10jqka.com.cn/thshy/
 """
+
 from datetime import datetime
 from io import StringIO
 
@@ -12,7 +13,7 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from py_mini_racer import py_mini_racer
-from tqdm import tqdm
+from akshare.utils.tqdm import get_tqdm
 
 from akshare.datasets import get_ths_js
 from akshare.utils import demjson
@@ -377,30 +378,35 @@ def stock_board_industry_cons_ths(symbol: str = "半导体及元件") -> pd.Data
     js_code.eval(js_content)
     v_code = js_code.call("v")
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/89.0.4389.90 Safari/537.36",
         "Cookie": f"v={v_code}",
     }
     url = f"http://q.10jqka.com.cn/thshy/detail/field/199112/order/desc/page/1/ajax/1/code/{symbol}"
     r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, "lxml")
+    soup = BeautifulSoup(r.text, features="lxml")
     try:
-        page_num = int(soup.find_all("a", attrs={"class": "changePage"})[-1]["page"])
-    except IndexError as e:
+        page_num = int(
+            soup.find_all(name="a", attrs={"class": "changePage"})[-1]["page"]
+        )
+    except IndexError:
         page_num = 1
     big_df = pd.DataFrame()
+    tqdm = get_tqdm()
     for page in tqdm(range(1, page_num + 1), leave=False):
         v_code = js_code.call("v")
         headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/89.0.4389.90 Safari/537.36",
             "Cookie": f"v={v_code}",
         }
         url = f"http://q.10jqka.com.cn/thshy/detail/field/199112/order/desc/page/{page}/ajax/1/code/{symbol}"
         r = requests.get(url, headers=headers)
         temp_df = pd.read_html(StringIO(r.text))[0]
-        big_df = pd.concat([big_df, temp_df], ignore_index=True)
+        big_df = pd.concat(objs=[big_df, temp_df], ignore_index=True)
 
     big_df.rename(
-        {
+        mapper={
             "涨跌幅(%)": "涨跌幅",
             "涨速(%)": "涨速",
             "换手(%)": "换手",
@@ -429,17 +435,18 @@ def stock_board_industry_info_ths(symbol: str = "半导体及元件") -> pd.Data
     ].values[0]
     url = f"http://q.10jqka.com.cn/thshy/detail/code/{symbol_code}/"
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/89.0.4389.90 Safari/537.36",
     }
     r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, "lxml")
+    soup = BeautifulSoup(r.text, features="lxml")
     name_list = [
         item.text.strip()
-        for item in soup.find("div", attrs={"class": "board-infos"}).find_all("dt")
+        for item in soup.find(name="div", attrs={"class": "board-infos"}).find_all("dt")
     ]
     value_list = [
         item.text.strip().replace("\n", "/")
-        for item in soup.find("div", attrs={"class": "board-infos"}).find_all("dd")
+        for item in soup.find(name="div", attrs={"class": "board-infos"}).find_all("dd")
     ]
     temp_df = pd.DataFrame([name_list, value_list]).T
     temp_df.columns = ["项目", "值"]
@@ -469,10 +476,12 @@ def stock_board_industry_index_ths(
     big_df = pd.DataFrame()
     current_year = datetime.now().year
     begin_year = int(start_date[:4])
+    tqdm = get_tqdm()
     for year in tqdm(range(begin_year, current_year + 1), leave=False):
-        url = f"http://d.10jqka.com.cn/v4/line/bk_{symbol_code}/01/{year}.js"
+        url = f"https://d.10jqka.com.cn/v4/line/bk_{symbol_code}/01/{year}.js"
         headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/89.0.4389.90 Safari/537.36",
             "Referer": "http://q.10jqka.com.cn",
             "Host": "d.10jqka.com.cn",
         }
@@ -480,7 +489,7 @@ def stock_board_industry_index_ths(
         data_text = r.text
         try:
             demjson.decode(data_text[data_text.find("{") : -1])
-        except:
+        except:  # noqa: E722
             continue
         temp_df = demjson.decode(data_text[data_text.find("{") : -1])
         temp_df = pd.DataFrame(temp_df["data"].split(";"))
@@ -552,26 +561,29 @@ def stock_ipo_benefit_ths() -> pd.DataFrame:
     js_code.eval(js_content)
     v_code = js_code.call("v")
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/89.0.4389.90 Safari/537.36",
         "Cookie": f"v={v_code}",
         "hexin-v": v_code,
     }
-    url = f"http://data.10jqka.com.cn/ipo/syg/field/invest/order/desc/page/1/ajax/1/free/1/"
+    url = "https://data.10jqka.com.cn/ipo/syg/field/invest/order/desc/page/1/ajax/1/free/1/"
     r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, "lxml")
-    page_num = soup.find("span", attrs={"class": "page_info"}).text.split("/")[1]
+    soup = BeautifulSoup(r.text, features="lxml")
+    page_num = soup.find(name="span", attrs={"class": "page_info"}).text.split("/")[1]
     big_df = pd.DataFrame()
+    tqdm = get_tqdm()
     for page in tqdm(range(1, int(page_num) + 1), leave=False):
-        url = f"http://data.10jqka.com.cn/ipo/syg/field/invest/order/desc/page/{page}/ajax/1/free/1/"
+        url = f"https://data.10jqka.com.cn/ipo/syg/field/invest/order/desc/page/{page}/ajax/1/free/1/"
         v_code = js_code.call("v")
         headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/89.0.4389.90 Safari/537.36",
             "Cookie": f"v={v_code}",
             "hexin-v": v_code,
         }
         r = requests.get(url, headers=headers)
         temp_df = pd.read_html(StringIO(r.text))[0]
-        big_df = pd.concat([big_df, temp_df], ignore_index=True)
+        big_df = pd.concat(objs=[big_df, temp_df], ignore_index=True)
 
     big_df.columns = [
         "序号",
@@ -606,14 +618,16 @@ def stock_board_industry_summary_ths() -> pd.DataFrame:
     js_code.eval(js_content)
     v_code = js_code.call("v")
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/89.0.4389.90 Safari/537.36",
         "Cookie": f"v={v_code}",
     }
-    url = f"http://q.10jqka.com.cn/thshy/index/field/199112/order/desc/page/1/ajax/1/"
+    url = "http://q.10jqka.com.cn/thshy/index/field/199112/order/desc/page/1/ajax/1/"
     r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, "lxml")
-    page_num = soup.find("span", attrs={"class": "page_info"}).text.split("/")[1]
+    soup = BeautifulSoup(r.text, features="lxml")
+    page_num = soup.find(name="span", attrs={"class": "page_info"}).text.split("/")[1]
     big_df = pd.DataFrame()
+    tqdm = get_tqdm()
     for page in tqdm(range(1, int(page_num) + 1), leave=False):
         url = f"http://q.10jqka.com.cn/thshy/index/field/199112/order/desc/page/{page}/ajax/1/"
         r = requests.get(url, headers=headers)
