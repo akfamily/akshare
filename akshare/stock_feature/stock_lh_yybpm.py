cc109ac197f2e52dc4f660f9b701a31a30ce3728
jindaxiang <jindaxiang@163.com>
2024-05-07 16:25:16 +0800

    fix: fix stock_lh_yyb_most

--- akshare/stock_feature/stock_lh_yybpm.py
+++ akshare/stock_feature/stock_lh_yybpm.py
@@ -1,37 +1,40 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2023/5/3 20:30
+Date: 2024/5/6 20:30
 Desc: 同花顺-数据中心-营业部排名
-http://data.10jqka.com.cn/market/longhu/
+https://data.10jqka.com.cn/market/longhu/
 """
-import requests
+
+from io import StringIO
+
 import pandas as pd
-from tqdm import tqdm
+import requests
 from bs4 import BeautifulSoup
+
+from akshare.utils.tqdm import get_tqdm
+from akshare.utils.cons import headers
 
 
 def stock_lh_yyb_most() -> pd.DataFrame:
     """
     同花顺-数据中心-营业部排名-上榜次数最多
-    http://data.10jqka.com.cn/market/longhu/
+    https://data.10jqka.com.cn/market/longhu/
     :return: 上榜次数最多
     :rtype: pandas.DataFrame
     """
-    url = "http://data.10jqka.com.cn/ifmarket/lhbyyb/type/1/tab/sbcs/field/sbcs/sort/desc/page/1/"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
-    }
+    url = "https://data.10jqka.com.cn/ifmarket/lhbyyb/type/1/tab/sbcs/field/sbcs/sort/desc/page/1/"
     r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, "lxml")
-    page_str = soup.find("span", attrs={"class": "page_info"}).text
+    soup = BeautifulSoup(r.text, features="lxml")
+    page_str = soup.find(name="span", attrs={"class": "page_info"}).text
     total_page = int(page_str.split("/")[1]) + 1
     big_df = pd.DataFrame()
-    for page in tqdm(range(1, total_page)):
-        url = f"http://data.10jqka.com.cn/ifmarket/lhbyyb/type/1/tab/sbcs/field/sbcs/sort/desc/page/{page}/"
+    tqdm = get_tqdm()
+    for page in tqdm(range(1, total_page), leave=False):
+        url = f"https://data.10jqka.com.cn/ifmarket/lhbyyb/type/1/tab/sbcs/field/sbcs/sort/desc/page/{page}/"
         r = requests.get(url, headers=headers)
-        temp_df = pd.read_html(r.text)[0]
-        big_df = pd.concat([big_df, temp_df], ignore_index=True)
+        temp_df = pd.read_html(StringIO(r.text))[0]
+        big_df = pd.concat(objs=[big_df, temp_df], ignore_index=True)
     big_df.reset_index(inplace=True, drop=True)
     return big_df
 
@@ -39,24 +42,22 @@ def stock_lh_yyb_most() -> pd.DataFrame:
 def stock_lh_yyb_capital() -> pd.DataFrame:
     """
     同花顺-数据中心-营业部排名-资金实力最强
-    http://data.10jqka.com.cn/market/longhu/
+    https://data.10jqka.com.cn/market/longhu/
     :return: 资金实力最强
     :rtype: pandas.DataFrame
     """
-    url = "http://data.10jqka.com.cn/ifmarket/lhbyyb/type/1/tab/zjsl/field/zgczje/sort/desc/page/1/"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
-    }
+    url = "https://data.10jqka.com.cn/ifmarket/lhbyyb/type/1/tab/zjsl/field/zgczje/sort/desc/page/1/"
     r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, "lxml")
-    page_str = soup.find("span", attrs={"class": "page_info"}).text
+    soup = BeautifulSoup(r.text, features="lxml")
+    page_str = soup.find(name="span", attrs={"class": "page_info"}).text
     total_page = int(page_str.split("/")[1]) + 1
     big_df = pd.DataFrame()
-    for page in tqdm(range(1, total_page)):
-        url = f"http://data.10jqka.com.cn/ifmarket/lhbyyb/type/1/tab/zjsl/field/zgczje/sort/desc/page/{page}/"
+    tqdm = get_tqdm()
+    for page in tqdm(range(1, total_page), leave=False):
+        url = f"https://data.10jqka.com.cn/ifmarket/lhbyyb/type/1/tab/zjsl/field/zgczje/sort/desc/page/{page}/"
         r = requests.get(url, headers=headers)
-        temp_df = pd.read_html(r.text)[0]
-        big_df = pd.concat([big_df, temp_df], ignore_index=True)
+        temp_df = pd.read_html(StringIO(r.text))[0]
+        big_df = pd.concat(objs=[big_df, temp_df], ignore_index=True)
     big_df.reset_index(inplace=True, drop=True)
     return big_df
 
@@ -64,24 +65,22 @@ def stock_lh_yyb_capital() -> pd.DataFrame:
 def stock_lh_yyb_control() -> pd.DataFrame:
     """
     同花顺-数据中心-营业部排名-抱团操作实力
-    http://data.10jqka.com.cn/market/longhu/
+    https://data.10jqka.com.cn/market/longhu/
     :return: 抱团操作实力
     :rtype: pandas.DataFrame
     """
-    url = "http://data.10jqka.com.cn/ifmarket/lhbyyb/type/1/tab/btcz/field/xsjs/sort/desc/page/1/"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
-    }
+    url = "https://data.10jqka.com.cn/ifmarket/lhbyyb/type/1/tab/btcz/field/xsjs/sort/desc/page/1/"
     r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, "lxml")
-    page_str = soup.find("span", attrs={"class": "page_info"}).text
+    soup = BeautifulSoup(r.text, features="lxml")
+    page_str = soup.find(name="span", attrs={"class": "page_info"}).text
     total_page = int(page_str.split("/")[1]) + 1
     big_df = pd.DataFrame()
-    for page in tqdm(range(1, total_page)):
-        url = f"http://data.10jqka.com.cn/ifmarket/lhbyyb/type/1/tab/btcz/field/xsjs/sort/desc/page/{page}/"
+    tqdm = get_tqdm()
+    for page in tqdm(range(1, total_page), leave=False):
+        url = f"https://data.10jqka.com.cn/ifmarket/lhbyyb/type/1/tab/btcz/field/xsjs/sort/desc/page/{page}/"
         r = requests.get(url, headers=headers)
-        temp_df = pd.read_html(r.text)[0]
-        big_df = pd.concat([big_df, temp_df], ignore_index=True)
+        temp_df = pd.read_html(StringIO(r.text))[0]
+        big_df = pd.concat(objs=[big_df, temp_df], ignore_index=True)
     big_df.reset_index(inplace=True, drop=True)
     return big_df
 
