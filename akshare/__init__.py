88da5a6ba10ee61ccfef0ce6a2f8b62fee5c8e5d
jindaxiang <jindaxiang@163.com>
2024-05-20 18:20:25 +0800

    feat: add version 1.13.80

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2791,9 +2791,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.13.77 fix: fix stock_news_em interface
 1.13.78 fix: fix futures_gfex_position_rank interface
 1.13.79 fix: fix stock_industry_category_cninfo interface
+1.13.80 fix: fix stock_irm_cninfo interface
 """
 
-__version__ = "1.13.79"
+__version__ = "1.13.80"
 __author__ = "AKFamily"
 
 import sys
