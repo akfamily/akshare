a19d267279570338463da870883de6e5f2523de4
jindaxiang <jindaxiang@163.com>
2024-05-24 16:19:53 +0800

    fix: fix stock_a_high_low_statistics

--- akshare/stock_feature/stock_a_high_low.py
+++ akshare/stock_feature/stock_a_high_low.py
@@ -5,8 +5,11 @@ Date: 2023/9/10 15:32
 Desc: 乐咕乐股-创新高、新低的股票数量
 https://www.legulegu.com/stockdata/high-low-statistics
 """
+
 import pandas as pd
 import requests
+
+from akshare.utils.cons import headers
 
 
 def stock_a_high_low_statistics(symbol: str = "all") -> pd.DataFrame:
@@ -19,7 +22,7 @@ def stock_a_high_low_statistics(symbol: str = "all") -> pd.DataFrame:
     :rtype: pandas.DataFrame
     """
     url = f"https://www.legulegu.com/stockdata/member-ship/get-high-low-statistics/{symbol}"
-    r = requests.get(url)
+    r = requests.get(url, headers=headers)
     data_json = r.json()
     temp_df = pd.DataFrame(data_json)
     del temp_df["indexCode"]
@@ -31,7 +34,7 @@ def stock_a_high_low_statistics(symbol: str = "all") -> pd.DataFrame:
     temp_df["low60"] = pd.to_numeric(temp_df["low60"], errors="coerce")
     temp_df["high120"] = pd.to_numeric(temp_df["high120"], errors="coerce")
     temp_df["low120"] = pd.to_numeric(temp_df["low120"], errors="coerce")
-    temp_df.sort_values(["date"], inplace=True, ignore_index=True)
+    temp_df.sort_values(by=["date"], inplace=True, ignore_index=True)
     return temp_df
 
 
