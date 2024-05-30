24c315cc65936c22ef3252c6b416840ae1bc360b
jindaxiang <jindaxiang@163.com>
2024-05-30 17:23:13 +0800

    feat: add version 1.13.90

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2804,7 +2804,7 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.13.90 fix: fix stock_zh_valuation_baidu interface
 """
 
-__version__ = "1.13.89"
+__version__ = "1.13.90"
 __author__ = "AKFamily"
 
 import sys
