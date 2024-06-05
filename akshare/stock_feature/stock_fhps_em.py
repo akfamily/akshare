9406513124c7095d69f6d851c7ba2f9c0cc5748a
jindaxiang <jindaxiang@163.com>
2024-06-05 15:57:35 +0800

    fix: fix stock_fhps_detail_em

--- akshare/stock_feature/stock_fhps_em.py
+++ akshare/stock_feature/stock_fhps_em.py
@@ -5,12 +5,13 @@ Date: 2023/4/7 15:22
 Desc: 东方财富网-数据中心-年报季报-分红送配
 https://data.eastmoney.com/yjfp/
 """
+
 import pandas as pd
 import requests
-from tqdm import tqdm
+from akshare.utils.tqdm import get_tqdm
 
 
-def stock_fhps_em(date: str = "20210630") -> pd.DataFrame:
+def stock_fhps_em(date: str = "20231231") -> pd.DataFrame:
     """
     东方财富网-数据中心-年报季报-分红送配
     https://data.eastmoney.com/yjfp/
@@ -19,6 +20,10 @@ def stock_fhps_em(date: str = "20210630") -> pd.DataFrame:
     :return: 分红送配
     :rtype: pandas.DataFrame
     """
+    import warnings
+
+    warnings.simplefilter(action="ignore", category=FutureWarning)
+
     url = "https://datacenter-web.eastmoney.com/api/data/v1/get"
     params = {
         "sortColumns": "PLAN_NOTICE_DATE",
@@ -38,12 +43,13 @@ def stock_fhps_em(date: str = "20210630") -> pd.DataFrame:
     data_json = r.json()
     total_pages = int(data_json["result"]["pages"])
     big_df = pd.DataFrame()
+    tqdm = get_tqdm()
     for page in tqdm(range(1, total_pages + 1), leave=False):
         params.update({"pageNumber": page})
         r = requests.get(url, params=params)
         data_json = r.json()
         temp_df = pd.DataFrame(data_json["result"]["data"])
-        big_df = pd.concat([big_df, temp_df], ignore_index=True)
+        big_df = pd.concat(objs=[big_df, temp_df], ignore_index=True)
 
     big_df.columns = [
         "_",
@@ -99,22 +105,35 @@ def stock_fhps_em(date: str = "20210630") -> pd.DataFrame:
             "最新公告日期",
         ]
     ]
-    big_df["送转股份-送转总比例"] = pd.to_numeric(big_df["送转股份-送转总比例"])
-    big_df["送转股份-送转比例"] = pd.to_numeric(big_df["送转股份-送转比例"])
-    big_df["送转股份-转股比例"] = pd.to_numeric(big_df["送转股份-转股比例"])
-    big_df["现金分红-现金分红比例"] = pd.to_numeric(big_df["现金分红-现金分红比例"])
-    big_df["现金分红-股息率"] = pd.to_numeric(big_df["现金分红-股息率"])
-    big_df["每股收益"] = pd.to_numeric(big_df["每股收益"])
-    big_df["每股净资产"] = pd.to_numeric(big_df["每股净资产"])
-    big_df["每股公积金"] = pd.to_numeric(big_df["每股公积金"])
-    big_df["每股未分配利润"] = pd.to_numeric(big_df["每股未分配利润"])
-    big_df["净利润同比增长"] = pd.to_numeric(big_df["净利润同比增长"])
-    big_df["总股本"] = pd.to_numeric(big_df["总股本"])
+    big_df["送转股份-送转总比例"] = pd.to_numeric(
+        big_df["送转股份-送转总比例"], errors="coerce"
+    )
+    big_df["送转股份-送转比例"] = pd.to_numeric(
+        big_df["送转股份-送转比例"], errors="coerce"
+    )
+    big_df["送转股份-转股比例"] = pd.to_numeric(
+        big_df["送转股份-转股比例"], errors="coerce"
+    )
+    big_df["现金分红-现金分红比例"] = pd.to_numeric(
+        big_df["现金分红-现金分红比例"], errors="coerce"
+    )
+    big_df["现金分红-股息率"] = pd.to_numeric(
+        big_df["现金分红-股息率"], errors="coerce"
+    )
+    big_df["每股收益"] = pd.to_numeric(big_df["每股收益"], errors="coerce")
+    big_df["每股净资产"] = pd.to_numeric(big_df["每股净资产"], errors="coerce")
+    big_df["每股公积金"] = pd.to_numeric(big_df["每股公积金"], errors="coerce")
+    big_df["每股未分配利润"] = pd.to_numeric(big_df["每股未分配利润"], errors="coerce")
+    big_df["净利润同比增长"] = pd.to_numeric(big_df["净利润同比增长"], errors="coerce")
+    big_df["总股本"] = pd.to_numeric(big_df["总股本"], errors="coerce")
 
     big_df["预案公告日"] = pd.to_datetime(big_df["预案公告日"], errors="coerce").dt.date
     big_df["股权登记日"] = pd.to_datetime(big_df["股权登记日"], errors="coerce").dt.date
     big_df["除权除息日"] = pd.to_datetime(big_df["除权除息日"], errors="coerce").dt.date
-    big_df["最新公告日期"] = pd.to_datetime(big_df["最新公告日期"], errors="coerce").dt.date
+    big_df["最新公告日期"] = pd.to_datetime(
+        big_df["最新公告日期"], errors="coerce"
+    ).dt.date
+    big_df.sort_values(["最新公告日期"], inplace=True, ignore_index=True)
     return big_df
 
 
@@ -146,12 +165,13 @@ def stock_fhps_detail_em(symbol: str = "300073") -> pd.DataFrame:
     data_json = r.json()
     total_pages = int(data_json["result"]["pages"])
     big_df = pd.DataFrame()
+    tqdm = get_tqdm()
     for page in tqdm(range(1, total_pages + 1), leave=False):
         params.update({"pageNumber": page})
         r = requests.get(url, params=params)
         data_json = r.json()
         temp_df = pd.DataFrame(data_json["result"]["data"])
-        big_df = pd.concat([big_df, temp_df], ignore_index=True)
+        big_df = pd.concat(objs=[big_df, temp_df], ignore_index=True)
 
     big_df.columns = [
         "_",
@@ -209,29 +229,44 @@ def stock_fhps_detail_em(symbol: str = "300073") -> pd.DataFrame:
         ]
     ]
     big_df["报告期"] = pd.to_datetime(big_df["报告期"], errors="coerce").dt.date
-    big_df["业绩披露日期"] = pd.to_datetime(big_df["业绩披露日期"], errors="coerce").dt.date
+    big_df["业绩披露日期"] = pd.to_datetime(
+        big_df["业绩披露日期"], errors="coerce"
+    ).dt.date
     big_df["预案公告日"] = pd.to_datetime(big_df["预案公告日"], errors="coerce").dt.date
     big_df["股权登记日"] = pd.to_datetime(big_df["股权登记日"], errors="coerce").dt.date
     big_df["除权除息日"] = pd.to_datetime(big_df["除权除息日"], errors="coerce").dt.date
-    big_df["最新公告日期"] = pd.to_datetime(big_df["最新公告日期"], errors="coerce").dt.date
+    big_df["最新公告日期"] = pd.to_datetime(
+        big_df["最新公告日期"], errors="coerce"
+    ).dt.date
 
-    big_df["送转股份-送转总比例"] = pd.to_numeric(big_df["送转股份-送转总比例"], errors="coerce")
-    big_df["送转股份-送股比例"] = pd.to_numeric(big_df["送转股份-送股比例"], errors="coerce")
-    big_df["送转股份-转股比例"] = pd.to_numeric(big_df["送转股份-转股比例"], errors="coerce")
-    big_df["现金分红-现金分红比例"] = pd.to_numeric(big_df["现金分红-现金分红比例"], errors="coerce")
-    big_df["现金分红-股息率"] = pd.to_numeric(big_df["现金分红-股息率"], errors="coerce")
+    big_df["送转股份-送转总比例"] = pd.to_numeric(
+        big_df["送转股份-送转总比例"], errors="coerce"
+    )
+    big_df["送转股份-送股比例"] = pd.to_numeric(
+        big_df["送转股份-送股比例"], errors="coerce"
+    )
+    big_df["送转股份-转股比例"] = pd.to_numeric(
+        big_df["送转股份-转股比例"], errors="coerce"
+    )
+    big_df["现金分红-现金分红比例"] = pd.to_numeric(
+        big_df["现金分红-现金分红比例"], errors="coerce"
+    )
+    big_df["现金分红-股息率"] = pd.to_numeric(
+        big_df["现金分红-股息率"], errors="coerce"
+    )
     big_df["每股收益"] = pd.to_numeric(big_df["每股收益"], errors="coerce")
     big_df["每股净资产"] = pd.to_numeric(big_df["每股净资产"], errors="coerce")
     big_df["每股公积金"] = pd.to_numeric(big_df["每股公积金"], errors="coerce")
     big_df["每股未分配利润"] = pd.to_numeric(big_df["每股未分配利润"], errors="coerce")
     big_df["净利润同比增长"] = pd.to_numeric(big_df["净利润同比增长"], errors="coerce")
     big_df["总股本"] = pd.to_numeric(big_df["总股本"], errors="coerce")
+    big_df.sort_values(["报告期"], inplace=True, ignore_index=True)
     return big_df
 
 
 if __name__ == "__main__":
-    stock_fhps_em_df = stock_fhps_em(date="20221231")
+    stock_fhps_em_df = stock_fhps_em(date="20231231")
     print(stock_fhps_em_df)
 
-    stock_fhps_detail_em_df = stock_fhps_detail_em(symbol="300073")
+    stock_fhps_detail_em_df = stock_fhps_detail_em(symbol="000005")
     print(stock_fhps_detail_em_df)
