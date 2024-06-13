3f211decc2f0f94423f3c95987710c5706851017
jindaxiang <jindaxiang@163.com>
2024-06-13 13:29:15 +0800

    fix: fix stock_notice_report

--- akshare/stock_fundamental/stock_notice.py
+++ akshare/stock_fundamental/stock_notice.py
@@ -1,19 +1,23 @@
 # -*- coding:utf-8 -*-
 # !/usr/bin/env python
 """
-Date: 2022/5/13 19:25
+Date: 2024/6/13 13:00
 Desc: 东方财富网-数据中心-公告大全-沪深 A 股公告
-http://data.eastmoney.com/notices/hsa/5.html
+https://data.eastmoney.com/notices/hsa/5.html
 """
+
+import math
+
 import pandas as pd
 import requests
-from tqdm import tqdm
+
+from akshare.utils.tqdm import get_tqdm
 
 
 def stock_notice_report(symbol: str = "全部", date: str = "20220511") -> pd.DataFrame:
     """
     东方财富网-数据中心-公告大全-沪深京 A 股公告
-    http://data.eastmoney.com/notices/hsa/5.html
+    https://data.eastmoney.com/notices/hsa/5.html
     :param symbol: 报告类型; choice of {"全部", "重大事项", "财务报告", "融资公告", "风险提示", "资产重组", "信息变更", "持股变动"}
     :type symbol: str
     :param date: 制定日期
@@ -21,7 +25,7 @@ def stock_notice_report(symbol: str = "全部", date: str = "20220511") -> pd.Da
     :return: 沪深京 A 股公告
     :rtype: pandas.DataFrame
     """
-    url = "http://np-anotice-stock.eastmoney.com/api/security/ann"
+    url = "https://np-anotice-stock.eastmoney.com/api/security/ann"
     report_map = {
         "全部": "0",
         "财务报告": "1",
@@ -45,11 +49,9 @@ def stock_notice_report(symbol: str = "全部", date: str = "20220511") -> pd.Da
     }
     r = requests.get(url, params=params)
     data_json = r.json()
-    import math
-
     total_page = math.ceil(data_json["data"]["total_hits"] / 100)
-
     big_df = pd.DataFrame()
+    tqdm = get_tqdm()
     for page in tqdm(range(1, int(total_page) + 1), leave=False):
         params.update(
             {
@@ -66,16 +68,16 @@ def stock_notice_report(symbol: str = "全部", date: str = "20220511") -> pd.Da
             temp_columns_df = pd.DataFrame(
                 [item["columns"][0] for item in data_json["data"]["list"]]
             )
-        except:
+        except:  # noqa: E722
             continue
         del temp_df["codes"]
         del temp_df["columns"]
-        temp_df = pd.concat([temp_df, temp_columns_df, temp_codes_df], axis=1)
-        big_df = pd.concat([big_df, temp_df], ignore_index=True)
+        temp_df = pd.concat(objs=[temp_df, temp_columns_df, temp_codes_df], axis=1)
+        big_df = pd.concat(objs=[big_df, temp_df], ignore_index=True)
 
     big_df.rename(
         columns={
-            "art_code": "_",
+            "art_code": "编码",
             "display_time": "-",
             "eiTime": "-",
             "notice_date": "公告日期",
@@ -90,6 +92,8 @@ def stock_notice_report(symbol: str = "全部", date: str = "20220511") -> pd.Da
         },
         inplace=True,
     )
+    url = "https://data.eastmoney.com/notices/detail/"
+    big_df["网址"] = url + big_df["代码"] + "/" + big_df["编码"] + ".html"
     big_df = big_df[
         [
             "代码",
@@ -97,17 +101,27 @@ def stock_notice_report(symbol: str = "全部", date: str = "20220511") -> pd.Da
             "公告标题",
             "公告类型",
             "公告日期",
+            "网址",
         ]
     ]
-    big_df["公告日期"] = pd.to_datetime(big_df["公告日期"]).dt.date
+    big_df["公告日期"] = pd.to_datetime(big_df["公告日期"], errors="coerce").dt.date
     return big_df
 
 
 if __name__ == "__main__":
-    stock_notice_report_df = stock_notice_report(symbol='财务报告', date="20220511")
+    stock_notice_report_df = stock_notice_report(symbol="财务报告", date="20240612")
     print(stock_notice_report_df)
 
-    item_list = ["全部", "财务报告", "融资公告", "风险提示", "信息变更", "重大事项", "资产重组", "持股变动"]
+    item_list = [
+        "全部",
+        "财务报告",
+        "融资公告",
+        "风险提示",
+        "信息变更",
+        "重大事项",
+        "资产重组",
+        "持股变动",
+    ]
     for temp_item in item_list:
         stock_notice_report_df = stock_notice_report(symbol=temp_item, date="20220511")
         print(stock_notice_report_df)
