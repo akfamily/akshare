15531c802e0b060091b211cbbd86d4921440903e
jindaxiang <jindaxiang@163.com>
2024-06-21 17:08:40 +0800

    chore: fix typos

--- akshare/utils/tqdm.py
+++ akshare/utils/tqdm.py
@@ -1,12 +1,22 @@
-def get_tqdm():
+def get_tqdm(enable: bool = True):
     """
     返回适用于当前环境的 tqdm 对象。
+
+    Args:
+        enable (bool): 是否启用进度条。默认为 True。
+
+    Returns:
+        tqdm 对象。
     """
+    if not enable:
+        # 如果进度条被禁用，返回一个不显示进度条的 tqdm 对象
+        return lambda iterable, *args, **kwargs: iterable
+
     try:
-        # 尝试检查是否在 Jupyter notebook 环境中，有利于退出进度条
+        # 尝试检查是否在 jupyter notebook 环境中，有利于退出进度条
         # noinspection PyUnresolvedReferences
         shell = get_ipython().__class__.__name__
-        if shell == 'ZMQInteractiveShell':
+        if shell == "ZMQInteractiveShell":
             from tqdm.notebook import tqdm
         else:
             from tqdm import tqdm
