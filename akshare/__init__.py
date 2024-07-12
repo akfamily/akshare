b848817558466eb1bf4dfe659edcae7048b78614
jindaxiang <jindaxiang@163.com>
2024-07-12 18:27:58 +0800

    feat: add version 1.14.29

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2839,9 +2839,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.14.26 fix: fix futures_fees_info interface
 1.14.27 fix: fix nlp_ownthink interface
 1.14.28 fix: fix news_trade_notify_dividend_baidu interface
+1.14.29 fix: fix stock_individual_spot_xq interface
 """
 
-__version__ = "1.14.28"
+__version__ = "1.14.29"
 __author__ = "AKFamily"
 
 import sys
