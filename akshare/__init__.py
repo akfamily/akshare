5ada2b316095e5fca6105a4a319a0fcca3d3b241
jindaxiang <jindaxiang@163.com>
2024-07-16 17:10:19 +0800

    feat: add version 1.14.32

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2842,9 +2842,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.14.29 fix: fix stock_individual_spot_xq interface
 1.14.30 fix: fix stock_us_hist_min_em interface
 1.14.31 fix: fix option_shfe_daily interface
+1.14.32 add: add stock_concept_cons_futu interface
 """
 
-__version__ = "1.14.31"
+__version__ = "1.14.32"
 __author__ = "AKFamily"
 
 import sys
@@ -2865,6 +2866,11 @@ if sys.version_info < (3, 9):
     )
 
 del sys
+
+"""
+富途牛牛-主题投资-概念板块-成分股
+"""
+from akshare.stock_feature.stock_concept_futu import stock_concept_cons_futu_df
 
 """
 商品期权手续费
