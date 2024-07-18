c61ad2f4492be54838219301289a1d7608f267fd
jindaxiang <jindaxiang@163.com>
2024-07-18 17:06:27 +0800

    feat: add macro_rmb_deposit

--- akshare/economic/macro_finance_ths.py
+++ akshare/economic/macro_finance_ths.py
@@ -77,9 +77,57 @@ def macro_rmb_loan() -> pd.DataFrame:
     return temp_df
 
 
+def macro_rmb_deposit() -> pd.DataFrame:
+    """
+    同花顺-数据中心-宏观数据-人民币存款余额
+    https://data.10jqka.com.cn/macro/rmb/
+    :return: 人民币存款余额
+    :rtype: pandas.DataFrame
+    """
+    url = "https://data.10jqka.com.cn/macro/rmb/"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+        "(KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+    }
+    r = requests.get(url, headers=headers)
+    temp_df = pd.read_html(StringIO(r.text), skiprows=0)[0]
+    temp_df.columns = [
+        "月份",
+        "新增存款-数量",
+        "新增存款-同比",
+        "新增存款-环比",
+        "新增企业存款-数量",
+        "新增企业存款-同比",
+        "新增企业存款-环比",
+        "新增储蓄存款-数量",
+        "新增储蓄存款-同比",
+        "新增储蓄存款-环比",
+        "新增其他存款-数量",
+        "新增其他存款-同比",
+        "新增其他存款-环比",
+    ]
+    temp_df["新增存款-数量"] = pd.to_numeric(temp_df["新增存款-数量"], errors="coerce")
+    temp_df["新增企业存款-数量"] = pd.to_numeric(
+        temp_df["新增企业存款-数量"], errors="coerce"
+    )
+    temp_df["新增企业存款-数量"] = pd.to_numeric(
+        temp_df["新增企业存款-数量"], errors="coerce"
+    )
+    temp_df["新增储蓄存款-数量"] = pd.to_numeric(
+        temp_df["新增储蓄存款-数量"], errors="coerce"
+    )
+    temp_df["新增其他存款-数量"] = pd.to_numeric(
+        temp_df["新增其他存款-数量"], errors="coerce"
+    )
+    return temp_df
+
+
 if __name__ == "__main__":
     macro_stock_finance_df = macro_stock_finance()
     print(macro_stock_finance_df)
 
     macro_rmb_loan_df = macro_rmb_loan()
     print(macro_rmb_loan_df)
+
+    macro_rmb_deposit_df = macro_rmb_deposit()
+    print(macro_rmb_deposit_df)
