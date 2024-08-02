36522659fe1e7b811adb1ed6c3f695db7ba40d91
jindaxiang <jindaxiang@163.com>
2024-08-02 14:55:15 +0800

    feat: add spot_hog_year_trend_soozhu

--- akshare/spot/spot_hog_soozhu.py
+++ akshare/spot/spot_hog_soozhu.py
@@ -38,6 +38,33 @@ def spot_hog_soozhu() -> pd.DataFrame:
     return big_df
 
 
+def spot_hog_year_trend_soozhu() -> pd.DataFrame:
+    """
+    搜猪-生猪大数据-今年以来全国出栏均价走势
+    https://www.soozhu.com/price/data/center/
+    :return: 今年以来全国出栏均价走势
+    :rtype: pd.DataFrame
+    """
+    session = requests.session()
+    url = "https://www.soozhu.com/price/data/center/"
+    r = session.get(url)
+    soup = BeautifulSoup(r.text, features="lxml")
+    token = soup.find(name="input", attrs={"name": "csrfmiddlewaretoken"})["value"]
+    url = "https://www.soozhu.com/price/data/center/"
+    payload = {"act": "yeartrend", "csrfmiddlewaretoken": token}
+    r = session.post(url, data=payload)
+    data_json = r.json()
+    temp_df = pd.DataFrame(data_json["nationlist"])
+    temp_df.columns = ["日期", "价格"]
+    temp_df["日期"] = pd.to_datetime(temp_df["日期"], errors="coerce").dt.date
+    temp_df["价格"] = pd.to_numeric(temp_df["价格"], errors="coerce")
+    temp_df.sort_values(by=["日期"], ignore_index=True, inplace=True)
+    return temp_df
+
+
 if __name__ == "__main__":
     spot_hog_soozhu_df = spot_hog_soozhu()
     print(spot_hog_soozhu_df)
+
+    spot_hog_year_trend_soozhu_df = spot_hog_year_trend_soozhu()
+    print(spot_hog_year_trend_soozhu_df)
