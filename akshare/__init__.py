ea0674d6f1355550c07b0b42321f02f84a2917e6
jindaxiang <jindaxiang@163.com>
2024-08-02 14:55:51 +0800

    feat: add version 1.14.48

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2858,9 +2858,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.14.45 fix: fix option_lhb_em interface
 1.14.46 add: add spot_hog_soozhu interface
 1.14.47 fix: fix spot_hog_soozhu interface
+1.14.48 add: add spot_hog_year_trend_soozhu interface
 """
 
-__version__ = "1.14.47"
+__version__ = "1.14.48"
 __author__ = "AKFamily"
 
 import sys
@@ -2885,7 +2886,7 @@ del sys
 """
 搜猪-生猪大数据-各省均价实时排行榜
 """
-from akshare.spot.spot_hog_soozhu import spot_hog_soozhu
+from akshare.spot.spot_hog_soozhu import spot_hog_soozhu, spot_hog_year_trend_soozhu
 
 """
 知名港股
@@ -3446,11 +3447,6 @@ from akshare.stock.stock_hk_hot_rank_em import (
     stock_hk_hot_rank_detail_realtime_em,
     stock_hk_hot_rank_em,
 )
-
-"""
-冬奥会历届奖牌榜
-"""
-from akshare.sport.sport_olympic_winter import sport_olympic_winter_hist
 
 """
 财新指数
