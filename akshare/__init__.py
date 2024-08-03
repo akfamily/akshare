16058b85466d00567506a1d3a1e945b45580058f
jindaxiang <jindaxiang@163.com>
2024-08-03 20:28:30 +0800

    feat: add version 1.14.49

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2859,9 +2859,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.14.46 add: add spot_hog_soozhu interface
 1.14.47 fix: fix spot_hog_soozhu interface
 1.14.48 add: add spot_hog_year_trend_soozhu interface
+1.14.49 fix: fix fortune_rank interface
 """
 
-__version__ = "1.14.48"
+__version__ = "1.14.49"
 __author__ = "AKFamily"
 
 import sys
