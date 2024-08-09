97d383def88d4071f42378006dd673b7918760cd
jindaxiang <jindaxiang@163.com>
2024-08-09 17:16:39 +0800

    feat: add version 1.14.55

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2865,9 +2865,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.14.52 fix: fix stock_esg_hz_sina interface
 1.14.53 add: add spot_hog_three_way_soozhu interface
 1.14.54 fix: fix stock_individual_info_em interface
+1.14.55 add: add spot_hog_crossbred_soozhu interface
 """
 
-__version__ = "1.14.54"
+__version__ = "1.14.55"
 __author__ = "AKFamily"
 
 import sys
@@ -2902,6 +2903,7 @@ from akshare.spot.spot_hog_soozhu import (
     spot_hog_year_trend_soozhu,
     spot_hog_lean_price_soozhu,
     spot_hog_three_way_soozhu,
+    spot_hog_crossbred_soozhu,
 )
 
 """
