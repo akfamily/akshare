bf267d7932e54df58e805a570834b807fe8f5c08
jindaxiang <jindaxiang@163.com>
2024-08-09 17:15:37 +0800

    feat: add spot_hog_crossbred_soozhu

--- akshare/spot/spot_hog_soozhu.py
+++ akshare/spot/spot_hog_soozhu.py
@@ -110,6 +110,30 @@ def spot_hog_three_way_soozhu() -> pd.DataFrame:
     return temp_df
 
 
+def spot_hog_crossbred_soozhu() -> pd.DataFrame:
+    """
+    搜猪-生猪大数据-全国后备二元母猪
+    https://www.soozhu.com/price/data/center/
+    :return: 全国后备二元母猪
+    :rtype: pd.DataFrame
+    """
+    session = requests.session()
+    url = "https://www.soozhu.com/price/data/center/"
+    r = session.get(url)
+    soup = BeautifulSoup(r.text, features="lxml")
+    token = soup.find(name="input", attrs={"name": "csrfmiddlewaretoken"})["value"]
+    url = "https://www.soozhu.com/price/data/center/"
+    payload = {"act": "pricetrend", "indid": "6", "csrfmiddlewaretoken": token}
+    r = session.post(url, data=payload)
+    data_json = r.json()
+    temp_df = pd.DataFrame(data_json["datalist"])
+    temp_df.columns = ["日期", "价格"]
+    temp_df["日期"] = pd.to_datetime(temp_df["日期"], errors="coerce").dt.date
+    temp_df["价格"] = pd.to_numeric(temp_df["价格"], errors="coerce")
+    temp_df.sort_values(by=["日期"], ignore_index=True, inplace=True)
+    return temp_df
+
+
 if __name__ == "__main__":
     spot_hog_soozhu_df = spot_hog_soozhu()
     print(spot_hog_soozhu_df)
@@ -122,3 +146,6 @@ if __name__ == "__main__":
 
     spot_hog_three_way_soozhu_df = spot_hog_three_way_soozhu()
     print(spot_hog_three_way_soozhu_df)
+
+    spot_hog_crossbred_soozhu_df = spot_hog_crossbred_soozhu()
+    print(spot_hog_crossbred_soozhu_df)
