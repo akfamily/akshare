73320ad9321df2611a0381d91b569eee54ef6ed3
jindaxiang <jindaxiang@163.com>
2024-08-10 15:31:01 +0800

    feat: add spot_corn_price_soozhu

--- akshare/spot/spot_hog_soozhu.py
+++ akshare/spot/spot_hog_soozhu.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 # !/usr/bin/env python
 """
-Date: 2024/7/31 16:30
+Date: 2024/8/10 15:30
 Desc: 搜猪-生猪大数据-各省均价实时排行榜
 https://www.soozhu.com/price/data/center/
 """
@@ -16,7 +16,7 @@ def spot_hog_soozhu() -> pd.DataFrame:
     搜猪-生猪大数据-各省均价实时排行榜
     https://www.soozhu.com/price/data/center/
     :return: 各省均价实时排行榜
-    :rtype: pd.DataFrame
+    :rtype: pandas.DataFrame
     """
     session = requests.session()
     url = "https://www.soozhu.com/price/data/center/"
@@ -43,7 +43,7 @@ def spot_hog_year_trend_soozhu() -> pd.DataFrame:
     搜猪-生猪大数据-今年以来全国出栏均价走势
     https://www.soozhu.com/price/data/center/
     :return: 今年以来全国出栏均价走势
-    :rtype: pd.DataFrame
+    :rtype: pandas.DataFrame
     """
     session = requests.session()
     url = "https://www.soozhu.com/price/data/center/"
@@ -67,7 +67,7 @@ def spot_hog_lean_price_soozhu() -> pd.DataFrame:
     搜猪-生猪大数据-全国瘦肉型肉猪
     https://www.soozhu.com/price/data/center/
     :return: 全国瘦肉型肉猪
-    :rtype: pd.DataFrame
+    :rtype: pandas.DataFrame
     """
     session = requests.session()
     url = "https://www.soozhu.com/price/data/center/"
@@ -91,7 +91,7 @@ def spot_hog_three_way_soozhu() -> pd.DataFrame:
     搜猪-生猪大数据-全国三元仔猪
     https://www.soozhu.com/price/data/center/
     :return: 全国三元仔猪
-    :rtype: pd.DataFrame
+    :rtype: pandas.DataFrame
     """
     session = requests.session()
     url = "https://www.soozhu.com/price/data/center/"
@@ -115,7 +115,7 @@ def spot_hog_crossbred_soozhu() -> pd.DataFrame:
     搜猪-生猪大数据-全国后备二元母猪
     https://www.soozhu.com/price/data/center/
     :return: 全国后备二元母猪
-    :rtype: pd.DataFrame
+    :rtype: pandas.DataFrame
     """
     session = requests.session()
     url = "https://www.soozhu.com/price/data/center/"
@@ -124,6 +124,30 @@ def spot_hog_crossbred_soozhu() -> pd.DataFrame:
     token = soup.find(name="input", attrs={"name": "csrfmiddlewaretoken"})["value"]
     url = "https://www.soozhu.com/price/data/center/"
     payload = {"act": "pricetrend", "indid": "6", "csrfmiddlewaretoken": token}
+    r = session.post(url, data=payload)
+    data_json = r.json()
+    temp_df = pd.DataFrame(data_json["datalist"])
+    temp_df.columns = ["日期", "价格"]
+    temp_df["日期"] = pd.to_datetime(temp_df["日期"], errors="coerce").dt.date
+    temp_df["价格"] = pd.to_numeric(temp_df["价格"], errors="coerce")
+    temp_df.sort_values(by=["日期"], ignore_index=True, inplace=True)
+    return temp_df
+
+
+def spot_corn_price_soozhu() -> pd.DataFrame:
+    """
+    搜猪-生猪大数据-全国玉米价格走势
+    https://www.soozhu.com/price/data/center/
+    :return: 全国玉米价格走势
+    :rtype: pandas.DataFrame
+    """
+    session = requests.session()
+    url = "https://www.soozhu.com/price/data/center/"
+    r = session.get(url)
+    soup = BeautifulSoup(r.text, features="lxml")
+    token = soup.find(name="input", attrs={"name": "csrfmiddlewaretoken"})["value"]
+    url = "https://www.soozhu.com/price/data/center/"
+    payload = {"act": "pricetrend", "indid": "8", "csrfmiddlewaretoken": token}
     r = session.post(url, data=payload)
     data_json = r.json()
     temp_df = pd.DataFrame(data_json["datalist"])
@@ -149,3 +173,6 @@ if __name__ == "__main__":
 
     spot_hog_crossbred_soozhu_df = spot_hog_crossbred_soozhu()
     print(spot_hog_crossbred_soozhu_df)
+
+    spot_corn_price_soozhu_df = spot_corn_price_soozhu()
+    print(spot_corn_price_soozhu_df)
