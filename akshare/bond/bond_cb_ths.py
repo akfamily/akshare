b1eb97e9a916316bd98bd55ebbcb0c9907dc7eeb
jindaxiang <jindaxiang@163.com>
2024-08-14 11:34:09 +0800

    fix: fix bond_zh_cov_info_ths

--- akshare/bond/bond_cb_ths.py
+++ akshare/bond/bond_cb_ths.py
@@ -1,24 +1,26 @@
 # -*- coding:utf-8 -*-
 # !/usr/bin/env python
 """
-Date: 2023/5/4 22:46
+Date: 2024/8/14 11:30
 Desc: 同花顺-数据中心-可转债
-http://data.10jqka.com.cn/ipo/bond/
+https://data.10jqka.com.cn/ipo/bond/
 """
-import requests
+
 import pandas as pd
+import requests
 
 
 def bond_zh_cov_info_ths() -> pd.DataFrame:
     """
     同花顺-数据中心-可转债
-    http://data.10jqka.com.cn/ipo/bond/
+    https://data.10jqka.com.cn/ipo/bond/
     :return: 可转债行情
     :rtype: pandas.DataFrame
     """
-    url = "http://data.10jqka.com.cn/ipo/kzz/"
+    url = "https://data.10jqka.com.cn/ipo/kzz/"
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/89.0.4389.90 Safari/537.36",
     }
     r = requests.get(url, headers=headers)
     data_json = r.json()
@@ -67,14 +69,18 @@ def bond_zh_cov_info_ths() -> pd.DataFrame:
             "中签率",
         ]
     ]
-    temp_df["申购日期"] = pd.to_datetime(temp_df["申购日期"], format="%Y-%m-%d").dt.date
+    temp_df["申购日期"] = pd.to_datetime(
+        temp_df["申购日期"], format="%Y-%m-%d", errors="coerce"
+    ).dt.date
     temp_df["中签公布日"] = pd.to_datetime(
         temp_df["中签公布日"], format="%Y-%m-%d", errors="coerce"
     ).dt.date
     temp_df["上市日期"] = pd.to_datetime(
         temp_df["上市日期"], format="%Y-%m-%d", errors="coerce"
     ).dt.date
-    temp_df["到期时间"] = pd.to_datetime(temp_df["到期时间"], format="%Y-%m-%d").dt.date
+    temp_df["到期时间"] = pd.to_datetime(
+        temp_df["到期时间"], format="%Y-%m-%d", errors="coerce"
+    ).dt.date
     temp_df["每股获配额"] = pd.to_numeric(temp_df["每股获配额"], errors="coerce")
     temp_df["计划发行量"] = pd.to_numeric(temp_df["计划发行量"], errors="coerce")
     temp_df["实际发行量"] = pd.to_numeric(temp_df["实际发行量"], errors="coerce")
