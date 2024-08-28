d64aafdbdfd17642af19ec5bc5ee33e77ce02c5e
jindaxiang <jindaxiang@163.com>
2024-08-28 15:23:10 +0800

    fix: fix cost_living

--- akshare/cost/cost_living.py
+++ akshare/cost/cost_living.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2024/1/20 23:30
+Date: 2024/8/28 15:20
 Desc: 世界各大城市生活成本数据
 https://expatistan.com/cost-of-living/index
 """
+
 from io import StringIO
 
 import pandas as pd
@@ -20,14 +21,14 @@ def _get_region() -> dict:
     """
     url = "https://www.expatistan.com/cost-of-living/index"
     r = requests.get(url)
-    soup = BeautifulSoup(r.text, "lxml")
+    soup = BeautifulSoup(r.text, features="lxml")
     half_url_list = [
         item["href"]
-        for item in soup.find("ul", attrs={"class": "regions"}).find_all("a")
+        for item in soup.find(name="ul", attrs={"class": "regions"}).find_all("a")
     ]
     name_list = [
         item["href"].split("/")[-1]
-        for item in soup.find("ul", attrs={"class": "regions"}).find_all("a")
+        for item in soup.find(name="ul", attrs={"class": "regions"}).find_all("a")
     ]
     name_url_dict = dict(zip(name_list, half_url_list))
     name_url_dict["world"] = "/cost-of-living/index"
@@ -38,7 +39,8 @@ def cost_living(symbol: str = "world") -> pd.DataFrame:
     """
     国家或地区生活成本数据
     https://expatistan.com/cost-of-living/index
-    :param symbol: choice of {"europe", "north-america", "latin-america", "asia", "middle-east", "africa", "oceania", "world"}
+    :param symbol: choice of {"europe", "north-america", "latin-america",
+    "asia", "middle-east", "africa", "oceania", "world"}
     :type symbol: str
     :return: 国家或地区生活成本数据
     :rtype: pandas.DataFrame
