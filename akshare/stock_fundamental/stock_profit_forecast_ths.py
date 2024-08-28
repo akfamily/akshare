ec75305a1358f45f7198db603bee028b2be17ea7
jindaxiang <jindaxiang@163.com>
2024-08-28 15:15:19 +0800

    fix: fix stock_profit_forecast_ths

--- akshare/stock_fundamental/stock_profit_forecast_ths.py
+++ akshare/stock_fundamental/stock_profit_forecast_ths.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2023/4/9 21:44
+Date: 2024/8/28 15:00
 Desc: 同花顺-盈利预测
 https://basic.10jqka.com.cn/new/600519/worth.html
 """
+
+from io import StringIO
+
 import pandas as pd
 import requests
+
+from akshare.utils.cons import headers
 
 
 def stock_profit_forecast_ths(
@@ -23,21 +28,18 @@ def stock_profit_forecast_ths(
     :rtype: pandas.DataFrame
     """
     url = f"https://basic.10jqka.com.cn/new/{symbol}/worth.html"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
-    }
     r = requests.get(url, headers=headers)
     r.encoding = "gbk"
     if indicator == "预测年报每股收益":
-        temp_df = pd.read_html(r.text)[0]
+        temp_df = pd.read_html(StringIO(r.text))[0]
         temp_df["年度"] = temp_df["年度"].astype(str)
         return temp_df
     if indicator == "预测年报净利润":
-        temp_df = pd.read_html(r.text)[1]
+        temp_df = pd.read_html(StringIO(r.text))[1]
         temp_df["年度"] = temp_df["年度"].astype(str)
         return temp_df
     if indicator == "业绩预测详表-机构":
-        temp_df = pd.read_html(r.text)[2]
+        temp_df = pd.read_html(StringIO(r.text))[2]
         columns_list = []
         for item in temp_df.columns:
             columns_list.append(item[1])
@@ -51,7 +53,7 @@ def stock_profit_forecast_ths(
         temp_df["报告日期"] = pd.to_datetime(temp_df["报告日期"]).dt.date
         return temp_df
     if indicator == "业绩预测详表-详细指标预测":
-        temp_df = pd.read_html(r.text)[3]
+        temp_df = pd.read_html(StringIO(r.text))[3]
         temp_df.columns = [
             item.replace("（", "-").replace("）", "") for item in temp_df.columns
         ]
@@ -59,6 +61,13 @@ def stock_profit_forecast_ths(
 
 
 if __name__ == "__main__":
-    for item in ["预测年报每股收益", "预测年报净利润", "业绩预测详表-机构", "业绩预测详表-详细指标预测"]:
-        stock_profit_forecast_ths_df = stock_profit_forecast_ths(symbol="600519", indicator=item)
+    for _item in [
+        "预测年报每股收益",
+        "预测年报净利润",
+        "业绩预测详表-机构",
+        "业绩预测详表-详细指标预测",
+    ]:
+        stock_profit_forecast_ths_df = stock_profit_forecast_ths(
+            symbol="600519", indicator=_item
+        )
         print(stock_profit_forecast_ths_df)
