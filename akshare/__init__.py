027f9b919e03ebbe7077253a522ee07753d152e4
jindaxiang <jindaxiang@163.com>
2024-09-06 20:54:12 +0800

    feat: add version 1.14.73

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2883,9 +2883,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.14.70 fix: fix setup.py
 1.14.71 fix: fix stock_mda_ym interface
 1.14.72 fix: fix fund_etf_hist_min_em interface
+1.14.73 fix: fix stock_gpzy_profile_em interface
 """
 
-__version__ = "1.14.72"
+__version__ = "1.14.73"
 __author__ = "AKFamily"
 
 import sys
