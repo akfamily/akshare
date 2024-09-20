02171da16af78c7007642ba23fc72bbe9b746c29
jindaxiang <jindaxiang@163.com>
2024-09-20 16:27:49 +0800

    feat: add stock_zcfz_bj_em

--- akshare/stock_feature/stock_report_em.py
+++ akshare/stock_feature/stock_report_em.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2024/4/2 21:30
+Date: 2024/9/20 15:30
 Desc: 东方财富-数据中心-年报季报-业绩快报-三大报表
 资产负债表
 https://data.eastmoney.com/bbsj/202003/zcfz.html
@@ -17,7 +17,7 @@ import requests
 from akshare.utils.tqdm import get_tqdm
 
 
-def stock_zcfz_em(date: str = "20220331") -> pd.DataFrame:
+def stock_zcfz_em(date: str = "20240331") -> pd.DataFrame:
     """
     东方财富-数据中心-年报季报-业绩快报-资产负债表
     https://data.eastmoney.com/bbsj/202003/zcfz.html
@@ -38,6 +38,147 @@ def stock_zcfz_em(date: str = "20220331") -> pd.DataFrame:
         "reportName": "RPT_DMSK_FN_BALANCE",
         "columns": "ALL",
         "filter": f"""(SECURITY_TYPE_CODE in ("058001001","058001008"))(TRADE_MARKET_CODE!="069001017")
+        (REPORT_DATE='{'-'.join([date[:4], date[4:6], date[6:]])}')""",
+    }
+    r = requests.get(url, params=params)
+    data_json = r.json()
+    page_num = data_json["result"]["pages"]
+    big_df = pd.DataFrame()
+    tqdm = get_tqdm()
+    for page in tqdm(range(1, page_num + 1), leave=False):
+        params.update(
+            {
+                "pageNumber": page,
+            }
+        )
+        r = requests.get(url, params=params)
+        data_json = r.json()
+        temp_df = pd.DataFrame(data_json["result"]["data"])
+        big_df = pd.concat(objs=[big_df, temp_df], join="outer", ignore_index=True)
+
+    big_df.reset_index(inplace=True)
+    big_df["index"] = big_df.index + 1
+    big_df.columns = [
+        "序号",
+        "_",
+        "股票代码",
+        "_",
+        "_",
+        "股票简称",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "公告日期",
+        "_",
+        "资产-总资产",
+        "_",
+        "资产-货币资金",
+        "_",
+        "资产-应收账款",
+        "_",
+        "资产-存货",
+        "_",
+        "负债-总负债",
+        "负债-应付账款",
+        "_",
+        "负债-预收账款",
+        "_",
+        "股东权益合计",
+        "_",
+        "资产-总资产同比",
+        "负债-总负债同比",
+        "_",
+        "资产负债率",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+    ]
+    big_df = big_df[
+        [
+            "序号",
+            "股票代码",
+            "股票简称",
+            "资产-货币资金",
+            "资产-应收账款",
+            "资产-存货",
+            "资产-总资产",
+            "资产-总资产同比",
+            "负债-应付账款",
+            "负债-预收账款",
+            "负债-总负债",
+            "负债-总负债同比",
+            "资产负债率",
+            "股东权益合计",
+            "公告日期",
+        ]
+    ]
+
+    big_df["资产-货币资金"] = pd.to_numeric(big_df["资产-货币资金"], errors="coerce")
+    big_df["资产-应收账款"] = pd.to_numeric(big_df["资产-应收账款"], errors="coerce")
+    big_df["资产-存货"] = pd.to_numeric(big_df["资产-存货"], errors="coerce")
+    big_df["资产-总资产"] = pd.to_numeric(big_df["资产-总资产"], errors="coerce")
+    big_df["资产-总资产同比"] = pd.to_numeric(
+        big_df["资产-总资产同比"], errors="coerce"
+    )
+    big_df["负债-应付账款"] = pd.to_numeric(big_df["负债-应付账款"], errors="coerce")
+    big_df["负债-预收账款"] = pd.to_numeric(big_df["负债-预收账款"], errors="coerce")
+    big_df["负债-总负债"] = pd.to_numeric(big_df["负债-总负债"], errors="coerce")
+    big_df["负债-总负债同比"] = pd.to_numeric(
+        big_df["负债-总负债同比"], errors="coerce"
+    )
+    big_df["资产负债率"] = pd.to_numeric(big_df["资产负债率"], errors="coerce")
+    big_df["股东权益合计"] = pd.to_numeric(big_df["股东权益合计"], errors="coerce")
+    big_df["股东权益合计"] = pd.to_numeric(big_df["股东权益合计"], errors="coerce")
+    big_df["公告日期"] = pd.to_datetime(big_df["公告日期"], errors="coerce").dt.date
+    return big_df
+
+
+def stock_zcfz_bj_em(date: str = "20240331") -> pd.DataFrame:
+    """
+    东方财富-数据中心-年报季报-业绩快报-资产负债表
+    https://data.eastmoney.com/bbsj/202003/zcfz.html
+    :param date: choice of {"20200331", "20200630", "20200930", "20201231", "..."}; 从 20100331 开始
+    :type date: str
+    :return: 资产负债表
+    :rtype: pandas.DataFrame
+    """
+    import warnings
+
+    warnings.filterwarnings(action="ignore", category=FutureWarning)
+    url = "https://datacenter-web.eastmoney.com/api/data/v1/get"
+    params = {
+        "sortColumns": "NOTICE_DATE,SECURITY_CODE",
+        "sortTypes": "-1,-1",
+        "pageSize": "500",
+        "pageNumber": "1",
+        "reportName": "RPT_DMSK_FN_BALANCE",
+        "columns": "ALL",
+        "filter": f"""(TRADE_MARKET_CODE="069001017")
         (REPORT_DATE='{'-'.join([date[:4], date[4:6], date[6:]])}')""",
     }
     r = requests.get(url, params=params)
@@ -432,8 +573,11 @@ def stock_xjll_em(date: str = "20220331") -> pd.DataFrame:
 
 
 if __name__ == "__main__":
-    stock_zcfz_em_df = stock_zcfz_em(date="20220331")
+    stock_zcfz_em_df = stock_zcfz_em(date="20240331")
     print(stock_zcfz_em_df)
+
+    stock_zcfz_bj_em_df = stock_zcfz_bj_em(date="20240331")
+    print(stock_zcfz_bj_em_df)
 
     stock_lrb_em_df = stock_lrb_em(date="20220331")
     print(stock_lrb_em_df)
