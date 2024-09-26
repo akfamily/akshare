cb231ef075bc5689e051f0358e1cd0c6cadabe46
jindaxiang <jindaxiang@163.com>
2024-09-26 18:30:47 +0800

    fix: fix drewry_wci_index

--- akshare/index/index_drewry.py
+++ akshare/index/index_drewry.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2023/9/30 15:30
+Date: 2024/9/26 18:00
 Desc: Drewry 集装箱指数
 https://www.drewry.co.uk/supply-chain-advisors/supply-chain-expertise/world-container-index-assessed-by-drewry
 https://infogram.com/world-container-index-1h17493095xl4zj
 """
+
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
@@ -17,7 +18,8 @@ def drewry_wci_index(symbol: str = "composite") -> pd.DataFrame:
     """
     Drewry 集装箱指数
     https://infogram.com/world-container-index-1h17493095xl4zj
-    :param symbol: choice of {"composite", "shanghai-rotterdam", "rotterdam-shanghai", "shanghai-los angeles", "los angeles-shanghai", "shanghai-genoa", "new york-rotterdam", "rotterdam-new york"}
+    :param symbol: choice of {"composite", "shanghai-rotterdam", "rotterdam-shanghai", "shanghai-los angeles",
+    "los angeles-shanghai", "shanghai-genoa", "new york-rotterdam", "rotterdam-new york"}
     :type symbol: str
     :return: Drewry 集装箱指数
     :rtype: pandas.DataFrame
@@ -34,24 +36,23 @@ def drewry_wci_index(symbol: str = "composite") -> pd.DataFrame:
     }
     url = "https://infogram.com/world-container-index-1h17493095xl4zj"
     r = requests.get(url)
-    soup = BeautifulSoup(r.text, "lxml")
+    soup = BeautifulSoup(r.text, features="lxml")
     data_text = soup.find_all("script")[-4].string.strip("window.infographicData=")[:-1]
     data_json = demjson.decode(data_text)
-    temp_df = pd.DataFrame(data_json["elements"][2]["data"][symbol_map[symbol]])
-    temp_df = temp_df.iloc[1:, :]
+    data_json_need = data_json["elements"]["content"]["content"]["entities"][
+        "7a55585f-3fb3-44e6-9b54-beea1cd20b4d"
+    ]["data"][symbol_map[symbol]]
+    date_list = [item[0]["value"] for item in data_json_need[1:]]
+    try:
+        value_list = [item[1]["value"] for item in data_json_need[1:]]
+    except TypeError:
+        value_list = [item[1]["value"] for item in data_json_need[1:-1]]
+    temp_df = pd.DataFrame([date_list, value_list]).T
     temp_df.columns = ["date", "wci"]
-    temp_df["date"] = [item["value"] for item in temp_df["date"]]
-    temp_df["wci"] = [item["value"] for item in temp_df["wci"]]
-    day = temp_df["date"].str.split("-", expand=True).iloc[:, 0].str.strip()
-    month = temp_df["date"].str.split("-", expand=True).iloc[:, 1].str.strip()
-    month = month.str.replace("July", "Jul")
-    year = temp_df["date"].str.split("-", expand=True).iloc[:, 2].str.strip()
-    temp_df["date"] = day + "-" + month + "-" + year
-    # 修正数据源中日期格式的错误
-    temp_df["date"] = temp_df["date"].str.replace("Sept", "Sep")
-    temp_df["date"] = pd.to_datetime(temp_df["date"], format="%d-%b-%y").dt.date
+    temp_df["date"] = pd.to_datetime(
+        temp_df["date"], format="%d-%b-%y", errors="coerce"
+    ).dt.date
     temp_df["wci"] = pd.to_numeric(temp_df["wci"], errors="coerce")
-    temp_df.reset_index(inplace=True, drop=True)
     return temp_df
 
 
