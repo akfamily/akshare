140ebfd901ea1d08198258e09276e580fc7af23c
jindaxiang <jindaxiang@163.com>
2024-10-14 22:59:00 +0800

    fix: fix stock_individual_spot_xq

--- akshare/stock/stock_xq.py
+++ akshare/stock/stock_xq.py
@@ -1,7 +1,7 @@
 # !/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2024/9/4 19:30
+Date: 2024/10/14 21:20
 Desc: 雪球-行情中心-个股
 https://xueqiu.com/S/SH513520
 """
@@ -13,22 +13,18 @@ import pandas as pd
 import requests
 
 
-def _convert_timestamp(timestamp_ms):
+def _convert_timestamp(timestamp_ms: int) -> str:
     """
-    将以毫秒为单位的时间戳转换为日期和时间，并保留到秒。
-
-    参数:
-    timestamp_ms (int): 以毫秒为单位的时间戳
-
-    返回:
-    datetime: 对应的日期和时间，保留到秒
+    时间戳转换为字符串时间
+    :param timestamp_ms: 时间戳
+    :type timestamp_ms: int
+    :return: 字符串
+    :rtype: str
     """
     # 将毫秒转换为秒
     timestamp_s = timestamp_ms / 1000
-
     # 使用 fromtimestamp 方法将时间戳转换为 datetime 对象
     datetime_obj = datetime.fromtimestamp(timestamp_s)
-
     return datetime_obj.strftime("%Y-%m-%d %H:%M:%S")
 
 
@@ -122,7 +118,6 @@ def stock_individual_spot_xq(
     temp_df.loc[temp_df["item"] == "时间", "value"] = temp_df.loc[
         temp_df["item"] == "时间", "value"
     ].apply(lambda x: _convert_timestamp(int(x)))
-
     return temp_df
 
 
