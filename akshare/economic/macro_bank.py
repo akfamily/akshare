7f3cc36e665a71932c74bf8609491df032f56d31
jindaxiang <jindaxiang@163.com>
2024-11-06 17:44:32 +0800

    fix: fix macro_bank_usa_interest_rate

--- akshare/economic/macro_bank.py
+++ akshare/economic/macro_bank.py
@@ -135,7 +135,7 @@ def macro_bank_newzealand_interest_rate() -> pd.DataFrame:
 # 金十数据中心-经济指标-央行利率-主要央行利率-中国央行决议报告
 def macro_bank_china_interest_rate() -> pd.DataFrame:
     """
-    新西兰联储决议报告, 数据区间从 19990401-至今
+    中国央行决议报告, 数据区间从 19990105-至今
     https://datacenter.jin10.com/reportType/dc_newzealand_interest_rate_decision
     https://cdn.jin10.com/dc/reports/dc_newzealand_interest_rate_decision_all.js?v=1578582075
     :return: 新西兰联储决议报告-今值(%)
