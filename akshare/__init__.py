1f5b548dfec7966fcd66648e2cda099f7f3bbec3
jindaxiang <jindaxiang@163.com>
2024-11-17 23:16:31 +0800

    feat: add version 1.15.22

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2931,9 +2931,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.15.19 fix: fix stock_share_hold_change_szse indicator
 1.15.20 fix: fix rv_from_stock_zh_a_hist_min_em indicator
 1.15.21 fix: fix get_futures_daily indicator
+1.15.22 fix: fix air_quality_hebei indicator
 """
 
-__version__ = "1.15.21"
+__version__ = "1.15.22"
 __author__ = "AKFamily"
 
 import sys
