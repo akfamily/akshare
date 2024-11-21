365c793f4edb29215b88aa017ad0ce16d6af175c
jindaxiang <jindaxiang@163.com>
2024-11-21 17:58:53 +0800

    fix: fix sw_index_second_info

--- akshare/stock_feature/stock_hk_valuation_baidu.py
+++ akshare/stock_feature/stock_hk_valuation_baidu.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2023/3/9 20:26
+Date: 2024/11/21 18:26
 Desc: 百度股市通-港股-财务报表-估值数据
 https://gushitong.baidu.com/stock/hk-06969
 """
+
 import http.client
 import json
 import urllib
@@ -22,25 +23,36 @@ def stock_hk_valuation_baidu(
     :type symbol: str
     :param indicator: choice of {"总市值", "市盈率(TTM)", "市盈率(静)", "市净率", "市现率"}
     :type indicator: str
-    :param period: choice of {"近一年", "近三年", "近五年", "近十年", "全部"}
+    :param period: choice of {"近一年", "近三年", "全部"}
     :type period: str
     :return: 估值数据
     :rtype: pandas.DataFrame
     """
     params = {
-        "srcid": "51171",
+        "openapi": "1",
+        "dspName": "iphone",
+        "tn": "tangram",
+        "client": "app",
+        "query": indicator,
         "code": symbol,
+        "word": "",
+        "resource_id": "51171",
         "market": "hk",
         "tag": indicator,
         "chart_select": period,
-        "skip_industry": "0",
+        "industry_select": "",
+        "skip_industry": "1",
         "finClientType": "pc",
     }
-    conn = http.client.HTTPSConnection("finance.pae.baidu.com")
-    conn.request("GET", f"/selfselect/openapi?{urllib.parse.urlencode(params)}")
+    conn = http.client.HTTPSConnection("gushitong.baidu.com")
+    conn.request(method="GET", url=f"/opendata?{urllib.parse.urlencode(params)}")
     r = conn.getresponse()
     data_json = json.loads(r.read())
-    temp_df = pd.DataFrame(data_json["Result"]["chartInfo"][0]["body"])
+    temp_df = pd.DataFrame(
+        data_json["Result"][0]["DisplayData"]["resultData"]["tplData"]["result"][
+            "chartInfo"
+        ][0]["body"]
+    )
     temp_df.columns = ["date", "value"]
     temp_df["date"] = pd.to_datetime(temp_df["date"]).dt.date
     temp_df["value"] = pd.to_numeric(temp_df["value"])
@@ -49,6 +61,6 @@ def stock_hk_valuation_baidu(
 
 if __name__ == "__main__":
     stock_hk_valuation_baidu_df = stock_hk_valuation_baidu(
-        symbol="00700", indicator="总市值", period="近五年"
+        symbol="06969", indicator="总市值", period="近一年"
     )
     print(stock_hk_valuation_baidu_df)
