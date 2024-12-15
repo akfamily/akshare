c0b4bf541d934ea4ffb61c405c7b3160095565d8
jindaxiang <jindaxiang@163.com>
2024-12-15 22:01:48 +0800

    fix: fix get_futures_daily

--- akshare/futures/futures_daily_bar.py
+++ akshare/futures/futures_daily_bar.py
@@ -682,7 +682,7 @@ def get_futures_daily(
 
 if __name__ == "__main__":
     get_futures_daily_df = get_futures_daily(
-        start_date="20241118", end_date="20241118", market="DCE"
+        start_date="20180326", end_date="20180331", market="INE"
     )
     print(get_futures_daily_df)
 
