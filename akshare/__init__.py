3cf3a5478298537c857dbfc89949c9f53eb4a768
Albert King <jindaxiang@163.com>
2024-12-16 19:37:22 +0800

    Dev (#5431)
    * fix: fix stock_zygc_em
    
    * docs: update date
    
    * fix: fix stock_mda_ym
    
    * feat: add version 1.15.48

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2957,9 +2957,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.15.45 fix: fix stock_jgdy_detail_em interface
 1.15.46 fix: fix fund_etf_fund_info_em interface
 1.15.47 fix: fix stock_individual_spot_xq interface
+1.15.48 fix: fix stock_mda_ym interface
 """
 
-__version__ = "1.15.47"
+__version__ = "1.15.48"
 __author__ = "AKFamily"
 
 import sys
