8b04050dda3bb98f0fb43ba08188b1ea1f3554be
jindaxiang <jindaxiang@163.com>
2024-12-26 19:05:16 +0800

    fix: fix spot_goods

--- akshare/index/index_spot.py
+++ akshare/index/index_spot.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2023/6/11 21:30
+Date: 2024/12/26 18:30
 Desc: 商品现货价格指数
-http://finance.sina.com.cn/futuremarket/spotprice.shtml#titlePos_0
+https://finance.sina.com.cn/futuremarket/spotprice.shtml#titlePos_0
 """
+
 import pandas as pd
 import requests
 
@@ -12,13 +13,13 @@ import requests
 def spot_goods(symbol: str = "波罗的海干散货指数") -> pd.DataFrame:
     """
     新浪财经-商品现货价格指数
-    http://finance.sina.com.cn/futuremarket/spotprice.shtml#titlePos_0
+    https://finance.sina.com.cn/futuremarket/spotprice.shtml#titlePos_0
     :param symbol: choice of {"波罗的海干散货指数", "钢坯价格指数", "澳大利亚粉矿价格"}
     :type symbol: str
     :return: 商品现货价格指数
     :rtype: pandas.DataFrame
     """
-    url = "http://stock.finance.sina.com.cn/futures/api/openapi.php/GoodsIndexService.get_goods_index"
+    url = "https://stock.finance.sina.com.cn/futures/api/openapi.php/GoodsIndexService.get_goods_index"
     symbol_url_dict = {
         "波罗的海干散货指数": "BDI",
         "钢坯价格指数": "GP",
@@ -31,10 +32,13 @@ def spot_goods(symbol: str = "波罗的海干散货指数") -> pd.DataFrame:
     temp_df = pd.DataFrame(data_json["result"]["data"]["data"])
     temp_df = temp_df[["opendate", "price", "zde", "zdf"]]
     temp_df.columns = ["日期", "指数", "涨跌额", "涨跌幅"]
-    temp_df["日期"] = pd.to_datetime(temp_df["日期"], format="%Y-%m-%d", errors="coerce").dt.date
+    temp_df["日期"] = pd.to_datetime(
+        temp_df["日期"], format="%Y-%m-%d", errors="coerce"
+    ).dt.date
     temp_df["指数"] = pd.to_numeric(temp_df["指数"], errors="coerce")
     temp_df["涨跌额"] = pd.to_numeric(temp_df["涨跌额"], errors="coerce")
     temp_df["涨跌幅"] = pd.to_numeric(temp_df["涨跌幅"], errors="coerce")
+    temp_df.dropna(inplace=True, ignore_index=True)
     return temp_df
 
 
