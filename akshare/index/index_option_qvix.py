3d17d7978f52f02e4e92a8c0decf10ae143f9876
jindaxiang <jindaxiang@163.com>
2025-01-17 15:49:54 +0800

    fix: fix index_option_50etf_qvix

--- akshare/index/index_option_qvix.py
+++ akshare/index/index_option_qvix.py
@@ -7,14 +7,23 @@ Desc: 50 ETF 期权波动率指数 QVIX
 http://1.optbbs.com/s/vix.shtml?50ETF
 http://1.optbbs.com/s/vix.shtml?300ETF
 """
+
 import pandas as pd
 from functools import lru_cache
 
+
 @lru_cache
-def _get_optbbs_daily():
+def __get_optbbs_daily() -> pd.DataFrame:
+    """
+    读取原始数据
+    http://1.optbbs.com/d/csv/d/k.csv
+    :return: 原始数据
+    :rtype: pandas.DataFrame
+    """
     url = "http://1.optbbs.com/d/csv/d/k.csv"
     temp_df = pd.read_csv(url, encoding="gbk")
     return temp_df
+
 
 def index_option_50etf_qvix() -> pd.DataFrame:
     """
@@ -23,7 +32,7 @@ def index_option_50etf_qvix() -> pd.DataFrame:
     :return: 50ETF 期权波动率指数 QVIX
     :rtype: pandas.DataFrame
     """
-    temp_df = _get_optbbs_daily().iloc[:, :5]
+    temp_df = __get_optbbs_daily().iloc[:, :5]
     temp_df.columns = [
         "date",
         "open",
@@ -63,7 +72,7 @@ def index_option_300etf_qvix() -> pd.DataFrame:
     :return: 300 ETF 期权波动率指数 QVIX
     :rtype: pandas.DataFrame
     """
-    temp_df = _get_optbbs_daily().iloc[:, [0, 9, 10, 11, 12]]
+    temp_df = __get_optbbs_daily().iloc[:, [0, 9, 10, 11, 12]]
     temp_df.columns = [
         "date",
         "open",
@@ -73,7 +82,7 @@ def index_option_300etf_qvix() -> pd.DataFrame:
     ]
     temp_df.loc[:, "date"] = pd.to_datetime(temp_df["date"], errors="coerce").dt.date
     temp_df.loc[:, "open"] = pd.to_numeric(temp_df["open"], errors="coerce")
-    temp_df.loc[:, 'high'] = pd.to_numeric(temp_df['high'], errors="coerce")
+    temp_df.loc[:, "high"] = pd.to_numeric(temp_df["high"], errors="coerce")
     temp_df.loc[:, "low"] = pd.to_numeric(temp_df["low"], errors="coerce")
     temp_df.loc[:, "close"] = pd.to_numeric(temp_df["close"], errors="coerce")
     return temp_df
@@ -103,7 +112,7 @@ def index_option_500etf_qvix() -> pd.DataFrame:
     :return: 500 ETF 期权波动率指数 QVIX
     :rtype: pandas.DataFrame
     """
-    temp_df = _get_optbbs_daily().iloc[:, [0, 67, 68, 69, 70]]
+    temp_df = __get_optbbs_daily().iloc[:, [0, 67, 68, 69, 70]]
     temp_df.columns = [
         "date",
         "open",
@@ -143,7 +152,7 @@ def index_option_cyb_qvix() -> pd.DataFrame:
     :return: 创业板 期权波动率指数 QVIX
     :rtype: pandas.DataFrame
     """
-    temp_df = _get_optbbs_daily().iloc[:, [0, 71, 72, 73, 74]]
+    temp_df = __get_optbbs_daily().iloc[:, [0, 71, 72, 73, 74]]
     temp_df.columns = [
         "date",
         "open",
@@ -183,7 +192,7 @@ def index_option_kcb_qvix() -> pd.DataFrame:
     :return: 科创板 期权波动率指数 QVIX
     :rtype: pandas.DataFrame
     """
-    temp_df = _get_optbbs_daily().iloc[:, [0, 83, 84, 85, 86]]
+    temp_df = __get_optbbs_daily().iloc[:, [0, 83, 84, 85, 86]]
     temp_df.columns = [
         "date",
         "open",
@@ -223,7 +232,7 @@ def index_option_100etf_qvix() -> pd.DataFrame:
     :return: 深证100ETF 期权波动率指数 QVIX
     :rtype: pandas.DataFrame
     """
-    temp_df = _get_optbbs_daily().iloc[:, [0, 75, 76, 77, 78]]
+    temp_df = __get_optbbs_daily().iloc[:, [0, 75, 76, 77, 78]]
     temp_df.columns = [
         "date",
         "open",
@@ -263,7 +272,7 @@ def index_option_300index_qvix() -> pd.DataFrame:
     :return: 中证300股指 期权波动率指数 QVIX
     :rtype: pandas.DataFrame
     """
-    temp_df = _get_optbbs_daily().iloc[:, [0, 17, 18, 19, 20]]
+    temp_df = __get_optbbs_daily().iloc[:, [0, 17, 18, 19, 20]]
     temp_df.columns = [
         "date",
         "open",
@@ -303,7 +312,7 @@ def index_option_1000index_qvix() -> pd.DataFrame:
     :return: 中证1000股指 期权波动率指数 QVIX
     :rtype: pandas.DataFrame
     """
-    temp_df = _get_optbbs_daily().iloc[:, [0, 25, 26, 27, 28]]
+    temp_df = __get_optbbs_daily().iloc[:, [0, 25, 26, 27, 28]]
     temp_df.columns = [
         "date",
         "open",
@@ -343,7 +352,7 @@ def index_option_50index_qvix() -> pd.DataFrame:
     :return: 上证50股指 期权波动率指数 QVIX
     :rtype: pandas.DataFrame
     """
-    temp_df = _get_optbbs_daily().iloc[:, [0, 79, 80, 81, 82]]
+    temp_df = __get_optbbs_daily().iloc[:, [0, 79, 80, 81, 82]]
     temp_df.columns = [
         "date",
         "open",
