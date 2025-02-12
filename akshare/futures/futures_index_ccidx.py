6adc5a63c48d811821a3927e0d65a14d9fc0b6c3
jindaxiang <jindaxiang@163.com>
2025-02-12 16:12:24 +0800

    fix: fix futures_index_ccidx

--- akshare/futures/futures_index_ccidx.py
+++ akshare/futures/futures_index_ccidx.py
@@ -1,12 +1,12 @@
 # -*- coding:utf-8 -*-
 # !/usr/bin/env python
 """
-Date: 2023/11/9 14:00
+Date: 2025/2/12 16:00
 Desc: 中证商品指数
 http://www.ccidx.com/
 """
 
-from io import BytesIO
+import json
 
 import pandas as pd
 import requests
@@ -25,31 +25,28 @@ def futures_index_ccidx(symbol: str = "中证商品期货指数") -> pd.DataFram
         "中证商品期货指数": "100001.CCI",
         "中证商品期货价格指数": "000001.CCI",
     }
-    url = "http://www.ccidx.com/front/ajax_downZSHQ.do"
-    params = {"indexCode": futures_index_map[symbol]}
+    url = "http://www.ccidx.com/CCI-ZZZS/index/getDateLine"
+    params = {"indexId": futures_index_map[symbol]}
     r = requests.get(url, params=params)
-    temp_df = pd.read_excel(BytesIO(r.content), header=1, engine="openpyxl")
-    temp_df.columns = [
-        "日期",
-        "指数代码",
-        "指数中文全称",
-        "指数中文简称",
-        "指数英文全称",
-        "指数英文简称",
-        "开盘",
-        "最高",
-        "最低",
-        "收盘",
-        "结算",
-        "涨跌",
-        "涨跌幅",
-    ]
+    data_json = r.json()
+    temp_df = pd.DataFrame(
+        [json.loads(item) for item in data_json["data"]["dateLineJson"]]
+    )
+    temp_df.rename(
+        columns={
+            "tradeDate": "日期",
+            "indexId": "指数代码",
+            "closingPrice": "收盘点位",
+            "settlePrice": "结算点位",
+            "dailyIncreaseAndDecrease": "涨跌",
+            "dailyIncreaseAndDecreasePercentage": "涨跌幅",
+        },
+        inplace=True,
+    )
+
     temp_df["日期"] = pd.to_datetime(temp_df["日期"], errors="coerce").dt.date
-    temp_df["开盘"] = pd.to_numeric(temp_df["开盘"], errors="coerce")
-    temp_df["最高"] = pd.to_numeric(temp_df["最高"], errors="coerce")
-    temp_df["最低"] = pd.to_numeric(temp_df["最低"], errors="coerce")
-    temp_df["收盘"] = pd.to_numeric(temp_df["收盘"], errors="coerce")
-    temp_df["结算"] = pd.to_numeric(temp_df["结算"], errors="coerce")
+    temp_df["收盘点位"] = pd.to_numeric(temp_df["收盘点位"], errors="coerce")
+    temp_df["结算点位"] = pd.to_numeric(temp_df["结算点位"], errors="coerce")
     temp_df["涨跌"] = pd.to_numeric(temp_df["涨跌"], errors="coerce")
     temp_df["涨跌幅"] = pd.to_numeric(temp_df["涨跌幅"], errors="coerce")
     temp_df.sort_values(by=["日期"], inplace=True)
