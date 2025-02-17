5c132dec5afc2ac0a1170db358a19d4273ca05a1
jindaxiang <jindaxiang@163.com>
2025-02-17 14:12:26 +0800

    fix: fix stock_bid_ask_em

--- akshare/stock/stock_ask_bid_em.py
+++ akshare/stock/stock_ask_bid_em.py
@@ -6,83 +6,10 @@ Desc: 东方财富-行情报价
 https://quote.eastmoney.com/sz000001.html
 """
 
-from functools import lru_cache
-
 import pandas as pd
 import requests
 
-
-@lru_cache()
-def __code_id_map_em() -> dict:
-    """
-    东方财富-股票和市场代码
-    https://quote.eastmoney.com/center/gridlist.html#hs_a_board
-    :return: 股票和市场代码
-    :rtype: dict
-    """
-    url = "https://80.push2.eastmoney.com/api/qt/clist/get"
-    params = {
-        "pn": "1",
-        "pz": "50000",
-        "po": "1",
-        "np": "1",
-        "ut": "bd1d9ddb04089700cf9c27f6f7426281",
-        "fltt": "2",
-        "invt": "2",
-        "fid": "f3",
-        "fs": "m:1 t:2,m:1 t:23",
-        "fields": "f12",
-        "_": "1623833739532",
-    }
-    r = requests.get(url, params=params)
-    data_json = r.json()
-    if not data_json["data"]["diff"]:
-        return dict()
-    temp_df = pd.DataFrame(data_json["data"]["diff"])
-    temp_df["market_id"] = 1
-    temp_df.columns = ["sh_code", "sh_id"]
-    code_id_dict = dict(zip(temp_df["sh_code"], temp_df["sh_id"]))
-    params = {
-        "pn": "1",
-        "pz": "50000",
-        "po": "1",
-        "np": "1",
-        "ut": "bd1d9ddb04089700cf9c27f6f7426281",
-        "fltt": "2",
-        "invt": "2",
-        "fid": "f3",
-        "fs": "m:0 t:6,m:0 t:80",
-        "fields": "f12",
-        "_": "1623833739532",
-    }
-    r = requests.get(url, params=params)
-    data_json = r.json()
-    if not data_json["data"]["diff"]:
-        return dict()
-    temp_df_sz = pd.DataFrame(data_json["data"]["diff"])
-    temp_df_sz["sz_id"] = 0
-    code_id_dict.update(dict(zip(temp_df_sz["f12"], temp_df_sz["sz_id"])))
-    params = {
-        "pn": "1",
-        "pz": "50000",
-        "po": "1",
-        "np": "1",
-        "ut": "bd1d9ddb04089700cf9c27f6f7426281",
-        "fltt": "2",
-        "invt": "2",
-        "fid": "f3",
-        "fs": "m:0 t:81 s:2048",
-        "fields": "f12",
-        "_": "1623833739532",
-    }
-    r = requests.get(url, params=params)
-    data_json = r.json()
-    if not data_json["data"]["diff"]:
-        return dict()
-    temp_df_sz = pd.DataFrame(data_json["data"]["diff"])
-    temp_df_sz["bj_id"] = 0
-    code_id_dict.update(dict(zip(temp_df_sz["f12"], temp_df_sz["bj_id"])))
-    return code_id_dict
+from akshare.stock_feature.stock_hist_em import code_id_map_em
 
 
 def stock_bid_ask_em(symbol: str = "000001") -> pd.DataFrame:
@@ -95,7 +22,7 @@ def stock_bid_ask_em(symbol: str = "000001") -> pd.DataFrame:
     :rtype: pandas.DataFrame
     """
     url = "https://push2.eastmoney.com/api/qt/stock/get"
-    code_id_map_em_dict = __code_id_map_em()
+    code_id_map_em_dict = code_id_map_em()
     params = {
         "fltt": "2",
         "invt": "2",
