b4b4393d5af09d182d244b80d707fcd2272dbf1d
jindaxiang <jindaxiang@163.com>
2025-02-28 16:54:58 +0800

    fix: fix stock_board_concept_spot_em

--- akshare/stock/stock_board_concept_em.py
+++ akshare/stock/stock_board_concept_em.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2025/2/26 17:00
+Date: 2025/2/28 16:30
 Desc: 东方财富-沪深板块-概念板块
 https://quote.eastmoney.com/center/boardlist.html#concept_board
 """
 
+import re
 from functools import lru_cache
 
 import pandas as pd
@@ -13,6 +14,90 @@ import requests
 
 
 @lru_cache()
+def __stock_board_concept_name_em() -> pd.DataFrame:
+    """
+    东方财富网-行情中心-沪深京板块-概念板块-名称
+    https://quote.eastmoney.com/center/boardlist.html#concept_board
+    :return: 概念板块-名称
+    :rtype: pandas.DataFrame
+    """
+    url = "https://79.push2.eastmoney.com/api/qt/clist/get"
+    params = {
+        "pn": "1",
+        "pz": "50000",
+        "po": "1",
+        "np": "2",
+        "ut": "bd1d9ddb04089700cf9c27f6f7426281",
+        "fltt": "2",
+        "invt": "2",
+        "fid": "f3",
+        "fs": "m:90 t:3 f:!50",
+        "fields": "f2,f3,f4,f8,f12,f14,f15,f16,f17,f18,f20,f21,f24,f25,f22,f33,f11,f62,f128,f124,f107,f104,f105,f136",
+        "_": "1626075887768",
+    }
+    r = requests.get(url, params=params)
+    data_json = r.json()
+    temp_df = pd.DataFrame(data_json["data"]["diff"]).T
+    temp_df.reset_index(inplace=True)
+    temp_df["index"] = range(1, len(temp_df) + 1)
+    temp_df.columns = [
+        "排名",
+        "最新价",
+        "涨跌幅",
+        "涨跌额",
+        "换手率",
+        "_",
+        "板块代码",
+        "板块名称",
+        "_",
+        "_",
+        "_",
+        "_",
+        "总市值",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "上涨家数",
+        "下跌家数",
+        "_",
+        "_",
+        "领涨股票",
+        "_",
+        "_",
+        "领涨股票-涨跌幅",
+    ]
+    temp_df = temp_df[
+        [
+            "排名",
+            "板块名称",
+            "板块代码",
+            "最新价",
+            "涨跌额",
+            "涨跌幅",
+            "总市值",
+            "换手率",
+            "上涨家数",
+            "下跌家数",
+            "领涨股票",
+            "领涨股票-涨跌幅",
+        ]
+    ]
+    temp_df["最新价"] = pd.to_numeric(temp_df["最新价"], errors="coerce")
+    temp_df["涨跌额"] = pd.to_numeric(temp_df["涨跌额"], errors="coerce")
+    temp_df["涨跌幅"] = pd.to_numeric(temp_df["涨跌幅"], errors="coerce")
+    temp_df["总市值"] = pd.to_numeric(temp_df["总市值"], errors="coerce")
+    temp_df["换手率"] = pd.to_numeric(temp_df["换手率"], errors="coerce")
+    temp_df["上涨家数"] = pd.to_numeric(temp_df["上涨家数"], errors="coerce")
+    temp_df["下跌家数"] = pd.to_numeric(temp_df["下跌家数"], errors="coerce")
+    temp_df["领涨股票-涨跌幅"] = pd.to_numeric(
+        temp_df["领涨股票-涨跌幅"], errors="coerce"
+    )
+    return temp_df
+
+
 def stock_board_concept_name_em() -> pd.DataFrame:
     """
     东方财富网-行情中心-沪深京板块-概念板块-名称
@@ -97,88 +182,53 @@ def stock_board_concept_name_em() -> pd.DataFrame:
     return temp_df
 
 
-def stock_board_concept_spot_em() -> pd.DataFrame:
+def stock_board_concept_spot_em(symbol: str = "可燃冰") -> pd.DataFrame:
     """
     东方财富网-行情中心-沪深京板块-概念板块-实时行情
-    https://quote.eastmoney.com/center/boardlist.html#concept_board
+    https://quote.eastmoney.com/bk/90.BK0818.html
     :return: 概念板块-实时行情
     :rtype: pandas.DataFrame
     """
-    url = "https://79.push2.eastmoney.com/api/qt/clist/get"
-    params = {
-        "pn": "1",
-        "pz": "50000",
-        "po": "1",
-        "np": "2",
-        "ut": "bd1d9ddb04089700cf9c27f6f7426281",
-        "fltt": "2",
-        "invt": "2",
-        "fid": "f3",
-        "fs": "m:90 t:3 f:!50",
-        "fields": "f2,f3,f4,f8,f12,f14,f15,f16,f17,f18,f20,f21,f24,f25,f22,f33,f11,f62,f128,f124,f107,f104,f105,f136",
-        "_": "1626075887768",
+    url = "https://91.push2.eastmoney.com/api/qt/stock/get"
+    field_map = {
+        "f43": "最新",
+        "f44": "最高",
+        "f45": "最低",
+        "f46": "开盘",
+        "f47": "成交量",
+        "f48": "成交额",
+        "f170": "涨跌幅",
+        "f171": "振幅",
+        "f168": "换手率",
+        "f169": "涨跌额",
     }
-    r = requests.get(url, params=params)
-    data_json = r.json()
-    temp_df = pd.DataFrame(data_json["data"]["diff"]).T
-    temp_df.reset_index(inplace=True)
-    temp_df["index"] = range(1, len(temp_df) + 1)
-    temp_df.columns = [
-        "排名",
-        "最新价",
-        "涨跌幅",
-        "涨跌额",
-        "换手率",
-        "_",
-        "板块代码",
-        "板块名称",
-        "_",
-        "_",
-        "_",
-        "_",
-        "总市值",
-        "_",
-        "_",
-        "_",
-        "_",
-        "_",
-        "_",
-        "上涨家数",
-        "下跌家数",
-        "_",
-        "_",
-        "领涨股票",
-        "_",
-        "_",
-        "领涨股票-涨跌幅",
-    ]
-    temp_df = temp_df[
-        [
-            "排名",
-            "板块名称",
-            "板块代码",
-            "最新价",
-            "涨跌额",
-            "涨跌幅",
-            "总市值",
-            "换手率",
-            "上涨家数",
-            "下跌家数",
-            "领涨股票",
-            "领涨股票-涨跌幅",
-        ]
-    ]
-    temp_df["最新价"] = pd.to_numeric(temp_df["最新价"], errors="coerce")
-    temp_df["涨跌额"] = pd.to_numeric(temp_df["涨跌额"], errors="coerce")
-    temp_df["涨跌幅"] = pd.to_numeric(temp_df["涨跌幅"], errors="coerce")
-    temp_df["总市值"] = pd.to_numeric(temp_df["总市值"], errors="coerce")
-    temp_df["换手率"] = pd.to_numeric(temp_df["换手率"], errors="coerce")
-    temp_df["上涨家数"] = pd.to_numeric(temp_df["上涨家数"], errors="coerce")
-    temp_df["下跌家数"] = pd.to_numeric(temp_df["下跌家数"], errors="coerce")
-    temp_df["领涨股票-涨跌幅"] = pd.to_numeric(
-        temp_df["领涨股票-涨跌幅"], errors="coerce"
+
+    if re.match(pattern=r"^BK\d+", string=symbol):
+        em_code = symbol
+    else:
+        industry_listing = __stock_board_concept_name_em()
+        em_code = industry_listing.query("板块名称 == @symbol")["板块代码"].values[0]
+    params = dict(
+        fields=",".join(field_map.keys()),
+        mpi="1000",
+        invt="2",
+        fltt="1",
+        secid=f"90.{em_code}",
+        ut="fa5fd1943c7b386f172d6893dbfba10b",
     )
-    return temp_df
+    r = requests.get(url, params=params)
+    data_dict = r.json()
+    result = pd.DataFrame.from_dict(data_dict["data"], orient="index")
+    result.rename(field_map, inplace=True)
+    result.reset_index(inplace=True)
+    result.columns = ["item", "value"]
+    result["value"] = pd.to_numeric(result["value"], errors="coerce")
+
+    # 各项转换成正常单位. 除了成交量与成交额, 原始数据中已是正常单位(元)
+    result["value"] = result["value"] * 1e-2
+    result.iloc[4, 1] = result.iloc[4, 1] * 1e2
+    result.iloc[5, 1] = result.iloc[5, 1] * 1e2
+    return result
 
 
 def stock_board_concept_hist_em(
@@ -209,7 +259,7 @@ def stock_board_concept_hist_em(
         "weekly": "102",
         "monthly": "103",
     }
-    stock_board_concept_em_map = stock_board_concept_name_em()
+    stock_board_concept_em_map = __stock_board_concept_name_em()
     stock_board_code = stock_board_concept_em_map[
         stock_board_concept_em_map["板块名称"] == symbol
     ]["板块代码"].values[0]
@@ -285,7 +335,7 @@ def stock_board_concept_hist_min_em(
     :return: 分时历史行情
     :rtype: pandas.DataFrame
     """
-    stock_board_concept_em_map = stock_board_concept_name_em()
+    stock_board_concept_em_map = __stock_board_concept_name_em()
     stock_board_code = stock_board_concept_em_map[
         stock_board_concept_em_map["板块名称"] == symbol
     ]["板块代码"].values[0]
@@ -391,7 +441,7 @@ def stock_board_concept_cons_em(symbol: str = "融资融券") -> pd.DataFrame:
     :return: 板块成份
     :rtype: pandas.DataFrame
     """
-    stock_board_concept_em_map = stock_board_concept_name_em()
+    stock_board_concept_em_map = __stock_board_concept_name_em()
     stock_board_code = stock_board_concept_em_map[
         stock_board_concept_em_map["板块名称"] == symbol
     ]["板块代码"].values[0]
@@ -490,7 +540,7 @@ if __name__ == "__main__":
     stock_board_concept_em_df = stock_board_concept_name_em()
     print(stock_board_concept_em_df)
 
-    stock_board_concept_spot_em_df = stock_board_concept_spot_em()
+    stock_board_concept_spot_em_df = stock_board_concept_spot_em(symbol="可燃冰")
     print(stock_board_concept_spot_em_df)
 
     stock_board_concept_hist_em_df = stock_board_concept_hist_em(
