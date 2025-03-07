b8a9e24aaa4356701dcc4872e8426877478ee085
jindaxiang <jindaxiang@163.com>
2025-03-07 18:35:25 +0800

    feat: add index_global_hist_sina

--- akshare/index/index_global_sina.py
+++ akshare/index/index_global_sina.py
@@ -1,45 +1,82 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2024/3/17 18:20
-Desc: 新浪财经-美股指数行情
-https://stock.finance.sina.com.cn/usstock/quotes/.IXIC.html
+Date: 2025/3/7 17:00
+Desc: 新浪财经-行情中心-环球市场
+https://finance.sina.com.cn/stock/globalindex/quotes/UKX
 """
 
 import pandas as pd
 import requests
-import py_mini_racer
 
-from akshare.stock.cons import (
-    zh_js_decode,
-)
+from akshare.index.cons import index_global_sina_symbol_map
 
 
-def index_us_stock_sina(symbol: str = ".INX") -> pd.DataFrame:
+def index_global_name_table() -> pd.DataFrame:
     """
-    新浪财经-美股指数行情
-    https://stock.finance.sina.com.cn/usstock/quotes/.IXIC.html
-    :param symbol: choice of {".IXIC", ".DJI", ".INX", ".NDX"}
-    :type symbol: str
-    :return: 美股指数行情
+    新浪财经-行情中心-环球市场-名称代码映射表
+    https://finance.sina.com.cn/stock/globalindex/quotes/UKX
+    :return: 名称代码映射表
     :rtype: pandas.DataFrame
     """
-    url = f"https://finance.sina.com.cn/staticdata/us/{symbol}"
-    r = requests.get(url)
-    js_code = py_mini_racer.MiniRacer()
-    js_code.eval(zh_js_decode)
-    dict_list = js_code.call("d", r.text.split("=")[1].split(";")[0].replace('"', ""))
-    temp_df = pd.DataFrame(dict_list)
+    temp_df = pd.DataFrame.from_dict(
+        index_global_sina_symbol_map, orient="index", columns=["代码"]
+    )
+    temp_df.index.name = "指数名称"
+    temp_df.reset_index(inplace=True)
+    return temp_df
+
+
+def index_global_hist_sina(symbol: str = "OMX") -> pd.DataFrame:
+    """
+    新浪财经-行情中心-环球市场-历史行情
+    https://finance.sina.com.cn/stock/globalindex/quotes/UKX
+    :param symbol: 指数名称；可以通过 ak.index_global_name_table() 获取
+    :type symbol: str
+    :return: 环球市场历史行情
+    :rtype: pandas.DataFrame
+    """
+    url = "https://gi.finance.sina.com.cn/hq/daily"
+    params = {
+        "symbol": index_global_sina_symbol_map[symbol],
+        "num": "10000",
+    }
+    r = requests.get(url=url, params=params)
+    data_json = r.json()
+    temp_df = pd.DataFrame(data_json["result"]["data"])
+    temp_df.rename(
+        columns={
+            "d": "date",
+            "o": "open",
+            "h": "high",
+            "l": "low",
+            "c": "close",
+            "v": "volume",
+        },
+        inplace=True,
+    )
+    temp_df = temp_df[
+        [
+            "date",
+            "open",
+            "high",
+            "low",
+            "close",
+            "volume",
+        ]
+    ]
     temp_df["date"] = pd.to_datetime(temp_df["date"], errors="coerce").dt.date
     temp_df["open"] = pd.to_numeric(temp_df["open"], errors="coerce")
     temp_df["high"] = pd.to_numeric(temp_df["high"], errors="coerce")
     temp_df["low"] = pd.to_numeric(temp_df["low"], errors="coerce")
     temp_df["close"] = pd.to_numeric(temp_df["close"], errors="coerce")
     temp_df["volume"] = pd.to_numeric(temp_df["volume"], errors="coerce")
-    temp_df["amount"] = pd.to_numeric(temp_df["amount"], errors="coerce")
     return temp_df
 
 
 if __name__ == "__main__":
-    index_us_stock_sina_df = index_us_stock_sina(symbol=".INX")
-    print(index_us_stock_sina_df)
+    index_global_name_table_df = index_global_name_table()
+    print(index_global_name_table_df)
+
+    index_global_hist_sina_df = index_global_hist_sina(symbol="瑞士股票指数")
+    print(index_global_hist_sina_df)
