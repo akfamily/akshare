9bdc59ccf9352a0fa581d8be0fccc8c1e37bd71f
jindaxiang <jindaxiang@163.com>
2025-03-11 17:37:27 +0800

    fix: fix option_value_analysis_em

--- akshare/option/option_value_analysis_em.py
+++ akshare/option/option_value_analysis_em.py
@@ -1,13 +1,14 @@
 # -*- coding:utf-8 -*-
 # !/usr/bin/env python
 """
-Date: 2025/2/22 21:00
+Date: 2025/3/11 17:00
 Desc: 东方财富网-数据中心-特色数据-期权价值分析
 https://data.eastmoney.com/other/valueAnal.html
 """
 
-import requests
 import pandas as pd
+
+from akshare.utils.func import fetch_paginated_data
 
 
 def option_value_analysis_em() -> pd.DataFrame:
@@ -21,19 +22,18 @@ def option_value_analysis_em() -> pd.DataFrame:
     params = {
         "fid": "f301",
         "po": "1",
-        "pz": "50000",
+        "pz": "100",
         "pn": "1",
-        "np": "2",
+        "np": "1",
         "fltt": "2",
         "invt": "2",
         "ut": "b2884a393a59ad64002292a3e90d46a5",
         "fields": "f1,f2,f3,f12,f13,f14,f298,f299,f249,f300,f330,f331,f332,f333,f334,f335,f336,f301,f152",
         "fs": "m:10",
     }
-    r = requests.get(url, params=params)
-    data_json = r.json()
-    temp_df = pd.DataFrame(data_json["data"]["diff"]).T
+    temp_df = fetch_paginated_data(url, params)
     temp_df.columns = [
+        "-",
         "-",
         "最新价",
         "-",
