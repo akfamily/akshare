1afd79bd0a0ad20bc3a7ace8d3d9c18a26384421
jindaxiang <jindaxiang@163.com>
2025-03-11 17:36:59 +0800

    fix: fix option_premium_analysis_em

--- akshare/option/option_premium_analysis_em.py
+++ akshare/option/option_premium_analysis_em.py
@@ -1,13 +1,14 @@
 # -*- coding:utf-8 -*-
 # !/usr/bin/env python
 """
-Date: 2025/2/22 21:00
+Date: 2025/3/11 17:00
 Desc: 东方财富网-数据中心-特色数据-期权折溢价
 https://data.eastmoney.com/other/premium.html
 """
 
-import requests
 import pandas as pd
+
+from akshare.utils.func import fetch_paginated_data
 
 
 def option_premium_analysis_em() -> pd.DataFrame:
@@ -21,19 +22,18 @@ def option_premium_analysis_em() -> pd.DataFrame:
     params = {
         "fid": "f250",
         "po": "1",
-        "pz": "50000",
+        "pz": "100",
         "pn": "1",
-        "np": "2",
+        "np": "1",
         "fltt": "2",
         "invt": "2",
         "ut": "b2884a393a59ad64002292a3e90d46a5",
         "fields": "f1,f2,f3,f12,f13,f14,f161,f250,f330,f331,f332,f333,f334,f335,f337,f301,f152",
         "fs": "m:10",
     }
-    r = requests.get(url, params=params)
-    data_json = r.json()
-    temp_df = pd.DataFrame(data_json["data"]["diff"]).T
+    temp_df = fetch_paginated_data(url, params)
     temp_df.columns = [
+        "-",
         "-",
         "最新价",
         "涨跌幅",
@@ -74,7 +74,9 @@ def option_premium_analysis_em() -> pd.DataFrame:
     temp_df["标的最新价"] = pd.to_numeric(temp_df["标的最新价"], errors="coerce")
     temp_df["标的涨跌幅"] = pd.to_numeric(temp_df["标的涨跌幅"], errors="coerce")
     temp_df["盈亏平衡价"] = pd.to_numeric(temp_df["盈亏平衡价"], errors="coerce")
-    temp_df["到期日"] = pd.to_datetime(temp_df["到期日"].astype(str)).dt.date
+    temp_df["到期日"] = pd.to_datetime(
+        temp_df["到期日"].astype(str), errors="coerce"
+    ).dt.date
     return temp_df
 
 
