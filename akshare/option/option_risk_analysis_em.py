7a977554e09b576482be389faebe40414e5da5c9
jindaxiang <jindaxiang@163.com>
2025-03-13 21:54:56 +0800

    fix: fix option_risk_analysis_em

--- akshare/option/option_risk_analysis_em.py
+++ akshare/option/option_risk_analysis_em.py
@@ -1,13 +1,14 @@
 # -*- coding:utf-8 -*-
 # !/usr/bin/env python
 """
-Date: 2025/2/22 21:00
+Date: 2025/3/13 21:50
 Desc: 东方财富网-数据中心-特色数据-期权风险分析
 https://data.eastmoney.com/other/riskanal.html
 """
 
-import requests
 import pandas as pd
+
+from akshare.utils.func import fetch_paginated_data
 
 
 def option_risk_analysis_em() -> pd.DataFrame:
@@ -19,21 +20,20 @@ def option_risk_analysis_em() -> pd.DataFrame:
     """
     url = "https://push2.eastmoney.com/api/qt/clist/get"
     params = {
-        "fid": "f3",
+        "fid": "f12",
         "po": "1",
-        "pz": "50000",
+        "pz": "100",
         "pn": "1",
-        "np": "2",
+        "np": "1",
         "fltt": "2",
         "invt": "2",
         "ut": "b2884a393a59ad64002292a3e90d46a5",
         "fields": "f1,f2,f3,f12,f13,f14,f302,f303,f325,f326,f327,f329,f328,f301,f152,f154",
         "fs": "m:10",
     }
-    r = requests.get(url, params=params)
-    data_json = r.json()
-    temp_df = pd.DataFrame(data_json["data"]["diff"]).T
+    temp_df = fetch_paginated_data(url, params)
     temp_df.columns = [
+        "-",
         "-",
         "最新价",
         "涨跌幅",
@@ -76,7 +76,9 @@ def option_risk_analysis_em() -> pd.DataFrame:
     temp_df["Vega"] = pd.to_numeric(temp_df["Vega"], errors="coerce")
     temp_df["Rho"] = pd.to_numeric(temp_df["Rho"], errors="coerce")
     temp_df["Theta"] = pd.to_numeric(temp_df["Theta"], errors="coerce")
-    temp_df["到期日"] = pd.to_datetime(temp_df["到期日"], format="%Y%m%d").dt.date
+    temp_df["到期日"] = pd.to_datetime(
+        temp_df["到期日"], format="%Y%m%d", errors="coerce"
+    ).dt.date
     return temp_df
 
 
