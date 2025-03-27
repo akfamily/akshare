4bd6d9746c3a91a8e8447f204ff4a826ac1b494c
jindaxiang <jindaxiang@163.com>
2025-03-27 14:45:26 +0800

    fix: fix stock_financial_us_report_em

--- akshare/stock_fundamental/stock_finance_us_em.py
+++ akshare/stock_fundamental/stock_finance_us_em.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2025/3/4 17:00
+Date: 2025/3/27 14:00
 Desc: 美股-基本面数据
 https://emweb.eastmoney.com/PC_USF10/pages/index.html?code=TSLA&type=web&color=w#/cwfx
 """
@@ -12,12 +12,43 @@ import requests
 from akshare.utils.cons import headers
 
 
+def __stock_financial_us_report_query_market_em(symbol: str = "TSLA") -> str:
+    """
+    东方财富-美股-财务分析-三大报表-查询市场
+    https://emweb.eastmoney.com/PC_USF10/pages/index.html?code=TSLA&type=web&color=w#/cwfx
+    :param symbol: choice of {"资产负债表", "综合损益表", "现金流量表"}
+    :type symbol: str
+    :return: 查询市场
+    :rtype: str
+    """
+    url = "https://datacenter.eastmoney.com/securities/api/data/v1/get"
+    params = {
+        "reportName": "RPT_USF10_INFO_ORGPROFILE",
+        "columns": "SECUCODE,SECURITY_CODE,ORG_CODE,SECURITY_INNER_CODE,ORG_NAME,ORG_EN_ABBR,BELONG_INDUSTRY,"
+        "FOUND_DATE,CHAIRMAN,REG_PLACE,ADDRESS,EMP_NUM,ORG_TEL,ORG_FAX,ORG_EMAIL,ORG_WEB,ORG_PROFILE",
+        "quoteColumns": "",
+        "filter": f'(SECURITY_CODE="{symbol}")',
+        "pageNumber": "1",
+        "pageSize": "200",
+        "sortTypes": "",
+        "sortColumns": "",
+        "source": "SECURITIES",
+        "client": "PC",
+        "v": "04406064331266868",
+    }
+
+    r = requests.get(url, params=params)
+    data_json = r.json()
+    stock_code = data_json["result"]["data"][0]["SECUCODE"]
+    return stock_code
+
+
 def __stock_financial_us_report_em(
     stock: str = "TSLA", symbol: str = "综合损益表", indicator: str = "年报"
 ) -> str:
     """
     东方财富-美股-财务分析-三大报表
-    https://emweb.securities.eastmoney.com/PC_HKF10/FinancialAnalysis/index?type=web&code=00700
+    https://emweb.eastmoney.com/PC_USF10/pages/index.html?code=TSLA&type=web&color=w#/cwfx
     :param stock: 股票代码
     :type stock: str
     :param symbol: choice of {"资产负债表", "综合损益表", "现金流量表"}
@@ -28,6 +59,7 @@ def __stock_financial_us_report_em(
     :rtype: str
     """
     url = "https://datacenter.eastmoney.com/securities/api/data/v1/get"
+    stock = __stock_financial_us_report_query_market_em(stock)
     if symbol == "资产负债表":
         report_name = "RPT_USF10_FN_BALANCE"
     elif symbol == "综合损益表":
@@ -41,7 +73,7 @@ def __stock_financial_us_report_em(
         "columns": "SECUCODE,SECURITY_CODE,SECURITY_NAME_ABBR,REPORT,REPORT_DATE,FISCAL_YEAR,CURRENCY,"
         "ACCOUNT_STANDARD,REPORT_TYPE,DATE_TYPE_CODE",
         "quoteColumns": "",
-        "filter": f'(SECUCODE="{stock}.O")',
+        "filter": f'(SECUCODE="{stock}")',
         "pageNumber": "",
         "pageSize": "",
         "sortTypes": "-1",
@@ -77,7 +109,7 @@ def stock_financial_us_report_em(
 ) -> pd.DataFrame:
     """
     东方财富-美股-财务分析-三大报表
-    https://emweb.securities.eastmoney.com/PC_HKF10/FinancialAnalysis/index?type=web&code=00700
+    https://emweb.eastmoney.com/PC_USF10/pages/index.html?code=TSLA&type=web&color=w#/cwfx
     :param stock: 股票代码
     :type stock: str
     :param symbol: choice of {"资产负债表", "综合损益表", "现金流量表"}
@@ -91,6 +123,7 @@ def stock_financial_us_report_em(
     date_str = __stock_financial_us_report_em(
         stock=stock, symbol=symbol, indicator=indicator
     )
+    stock = __stock_financial_us_report_query_market_em(stock)
     if symbol == "资产负债表":
         report_name = "RPT_USF10_FN_BALANCE"
     elif symbol == "综合损益表":
@@ -104,7 +137,7 @@ def stock_financial_us_report_em(
         "columns": "SECUCODE,SECURITY_CODE,SECURITY_NAME_ABBR,REPORT_DATE,REPORT_TYPE,REPORT,"
         "STD_ITEM_CODE,AMOUNT,ITEM_NAME",
         "quoteColumns": "",
-        "filter": f'(SECUCODE="{stock}.O")(REPORT in ' + date_str + ")",
+        "filter": f'(SECUCODE="{stock}")(REPORT in ' + date_str + ")",
         "pageNumber": "",
         "pageSize": "",
         "sortTypes": "1,-1",
@@ -124,7 +157,7 @@ def stock_financial_us_analysis_indicator_em(
 ) -> pd.DataFrame:
     """
     东方财富-美股-财务分析-主要指标
-    https://emweb.eastmoney.com/PC_USF10/pages/index.html?code=TSLA&type=web&color=w#/cwfx/zyzb
+    https://emweb.eastmoney.com/PC_USF10/pages/index.html?code=TSLA&type=web&color=w#/cwfx
     :param symbol: 股票代码
     :type symbol: str
     :param indicator: choice of {"年报", "单季报", "累计季报"}
@@ -133,6 +166,7 @@ def stock_financial_us_analysis_indicator_em(
     :rtype: pandas.DataFrame
     """
     url = "https://datacenter.eastmoney.com/securities/api/data/v1/get"
+    symbol = __stock_financial_us_report_query_market_em(symbol)
     params = {
         "reportName": "RPT_USF10_FN_GMAININDICATOR",
         "columns": "USF10_FN_GMAININDICATOR",
@@ -146,16 +180,16 @@ def stock_financial_us_analysis_indicator_em(
         "v": "01975982096513973",
     }
     if indicator == "年报":
-        params.update({"filter": f"""(SECUCODE="{symbol}.O")(DATE_TYPE_CODE="001")"""})
+        params.update({"filter": f"""(SECUCODE="{symbol}")(DATE_TYPE_CODE="001")"""})
     elif indicator == "单季报":
         params.update(
             {
-                "filter": f"""(SECUCODE="{symbol}.O")(DATE_TYPE_CODE in ("003","006","007","008"))"""
+                "filter": f"""(SECUCODE="{symbol}")(DATE_TYPE_CODE in ("003","006","007","008"))"""
             }
         )
     elif indicator == "累计季报":
         params.update(
-            {"filter": f"""(SECUCODE="{symbol}.O")(DATE_TYPE_CODE in ("002","004"))"""}
+            {"filter": f"""(SECUCODE="{symbol}")(DATE_TYPE_CODE in ("002","004"))"""}
         )
     else:
         raise ValueError("请输入正确的 indicator 参数")
@@ -182,7 +216,7 @@ if __name__ == "__main__":
     print(stock_financial_us_analysis_indicator_em_df)
 
     stock_financial_us_report_em_df = stock_financial_us_report_em(
-        stock="TSLA", symbol="资产负债表", indicator="年报"
+        stock="BABA", symbol="资产负债表", indicator="年报"
     )
     print(stock_financial_us_report_em_df)
 
