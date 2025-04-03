960fe48ed8bec5197551bf5b4b4cde9d6b8a1309
Albert <18234520+albertandking@users.noreply.github.com>
2025-04-03 18:00:57 +0800

    feat: add version 1.16.70

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -3078,9 +3078,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.16.67 fix: fix stock_individual_basic_info_xq interface
 1.16.68 fix: fix option_dce_daily interface
 1.16.69 fix: fix stock_individual_spot_xq interface
+1.16.70 fix: fix stock_zh_ah_spot_em interface
 """
 
-__version__ = "1.16.69"
+__version__ = "1.16.70"
 __author__ = "AKFamily"
 
 import sys
