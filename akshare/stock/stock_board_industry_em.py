6846a0f702a1affdc0d154bf5f9f49768f1631a9
Albert <18234520+albertandking@users.noreply.github.com>
2025-04-05 18:25:33 +0800

    fix: fix stock_board_industry_hist_em

--- akshare/stock/stock_board_industry_em.py
+++ akshare/stock/stock_board_industry_em.py
@@ -400,9 +400,9 @@ def stock_board_industry_hist_min_em(
         temp_df["最新价"] = pd.to_numeric(temp_df["最新价"], errors="coerce")
         return temp_df
     else:
-        url = "http://7.push2his.eastmoney.com/api/qt/stock/kline/get"
+        url = "https://7.push2his.eastmoney.com/api/qt/stock/kline/get"
         params = {
-            "secid": f"90.{stock_board_code}",
+            "secid": f"90.{em_code}",
             "fields1": "f1,f2,f3,f4,f5,f6",
             "fields2": "f51,f52,f53,f54,f55,f56,f57,f58,f59,f60,f61",
             "klt": period,
