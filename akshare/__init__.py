2855a3d6bafc334b8901507f9f4d87f04d0fb034
smartmakeswatch <206515907+smartmakeswatch@users.noreply.github.com>
2025-06-16 19:15:58 +0800

    feat: add version 1.17.2

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -3108,9 +3108,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.16.98 fix: fix fund_open_fund_info_em interface
 1.16.99 fix: fix stock_info_sz_delist interface
 1.17.1 fix: fix stock_gdfx_holding_detail_em interface
+1.17.2 fix: fix stock_ggcg_em interface
 """
 
-__version__ = "1.17.1"
+__version__ = "1.17.2"
 __author__ = "AKFamily"
 
 import sys
