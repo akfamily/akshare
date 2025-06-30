a24676e3913c6444d0540396c41655ac2b4e899e
smartmakeswatch <206515907+smartmakeswatch@users.noreply.github.com>
2025-06-30 23:20:37 +0800

    feat: add version 1.17.10

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -3116,9 +3116,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.17.7 fix: fix futures_spot_price_daily interface
 1.17.8 fix: fix forex_spot_em interface
 1.17.9 fix: fix stock_zh_ab_comparison_em interface
+1.17.10 fix: fix get_shfe_daily interface
 """
 
-__version__ = "1.17.9"
+__version__ = "1.17.10"
 __author__ = "AKFamily"
 
 import sys
