b09cd0c1d65b0f1d324b605b7fabd6e3e0cedf3a
smartmakeswatch <206515907+smartmakeswatch@users.noreply.github.com>
2025-07-02 17:18:52 +0800

    feat: add version 1.17.14

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -3120,9 +3120,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.17.11 fix: fix stock_individual_spot_xq interface
 1.17.12 fix: fix get_receipt interface
 1.17.13 fix: fix futures_shfe_warehouse_receipt interface
+1.17.14 fix: fix forex_spot_em interface
 """
 
-__version__ = "1.17.13"
+__version__ = "1.17.14"
 __author__ = "AKFamily"
 
 import sys
