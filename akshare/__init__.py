dae39d8ac9202e3bf7869b7e76b29af94dd7cb1a
smartmakeswatch <206515907+smartmakeswatch@users.noreply.github.com>
2025-08-19 16:26:00 +0800

    feat: add version 1.17.38

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -3144,9 +3144,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.17.35 fix: fix option_dce_daily interface
 1.17.36 fix: fix futures_hist_em interface
 1.17.37 fix: fix stock_zh_index_daily_em interface
+1.17.38 fix: fix futures_inventory_99 interface
 """
 
-__version__ = "1.17.37"
+__version__ = "1.17.38"
 __author__ = "AKFamily"
 
 import sys
