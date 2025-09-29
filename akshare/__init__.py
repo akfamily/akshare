0a08b5383e05d8b22d1084d82ab55ab414657232
smartmakeswatch <206515907+smartmakeswatch@users.noreply.github.com>
2025-09-29 17:57:37 +0800

    feat: add version 1.17.61

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -3167,9 +3167,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.17.58 fix: fix fund_etf_fund_daily_em interface
 1.17.59 fix: fix fx_c_swap_cm interface
 1.17.60 fix: fix stock_intraday_sina interface
+1.17.61 fix: fix stock_individual_spot_xq interface
 """
 
-__version__ = "1.17.60"
+__version__ = "1.17.61"
 __author__ = "AKFamily"
 
 import sys
