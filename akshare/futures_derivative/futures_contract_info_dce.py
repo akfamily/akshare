6c3e2cef129d0dba95aea3f009b4f82cd6f05738
smartmakeswatch <206515907+smartmakeswatch@users.noreply.github.com>
2025-10-30 17:18:57 +0800

    fix: fix futures_contract_info_dce

--- akshare/futures_derivative/futures_contract_info_dce.py
+++ akshare/futures_derivative/futures_contract_info_dce.py
@@ -6,28 +6,47 @@ Desc: 大连商品交易所-业务/服务-业务参数-交易参数-合约信息
 http://www.dce.com.cn/dalianshangpin/ywfw/ywcs/jycs/hyxxcx/index.html
 """
 
-from io import StringIO
-
 import pandas as pd
 import requests
 
 
 def futures_contract_info_dce() -> pd.DataFrame:
     """
-    大连商品交易所-业务/服务-业务参数-交易参数-合约信息查询
-    http://www.dce.com.cn/dalianshangpin/ywfw/ywcs/jycs/hyxxcx/index.html
+    大连商品交易所-数据中心-业务数据-交易参数-合约信息
+    http://www.dce.com.cn/dce/channel/list/180.html
     :return: 交易参数汇总查询
     :rtype: pandas.DataFrame
     """
-    url = "http://portal.dce.com.cn/publicweb/businessguidelines/queryContractInfo.html"
-    params = {
-        "contractInformation.variety": "all",
-        "contractInformation.trade_type": "0",
+    url = "http://www.dce.com.cn/dcereport/publicweb/tradepara/contractInfo"
+    payload = {
+        "lang": "zh",
+        "tradeType": "1",
+        "varietyId": "all",
     }
-    r = requests.post(url, params=params)
-    temp_df = pd.read_html(StringIO(r.text))[0]
+    r = requests.post(url, json=payload)
+    data_json = r.json()
+    temp_df = pd.DataFrame(data_json['data'])
+    temp_df.rename(columns={
+        "contractId": "合约",
+        "variety": "品种名称",
+        "varietyOrder": "品种代码",
+        "unit": "交易单位",
+        "tick": "最小变动价位",
+        "startTradeDate": "开始交易日",
+        "endTradeDate": "最后交易日",
+        "endDeliveryDate": "最后交割日",
+        "tradeType": "",
+    }, inplace=True)
+    temp_df = temp_df[[
+        "品种名称",
+        "合约",
+        "交易单位",
+        "最小变动价位",
+        "开始交易日",
+        "最后交易日",
+        "最后交割日",
+    ]]
     temp_df["交易单位"] = pd.to_numeric(temp_df["交易单位"], errors="coerce")
-    temp_df["最小变动价位"] = pd.to_numeric(temp_df["最小变动价位"], errors="coerce")
     temp_df["最小变动价位"] = pd.to_numeric(temp_df["最小变动价位"], errors="coerce")
     temp_df["开始交易日"] = pd.to_datetime(
         temp_df["开始交易日"], format="%Y%m%d", errors="coerce"
