556217e5ae74a8c2de21c609230b8367a3e0b26d
smartmakeswatch <206515907+smartmakeswatch@users.noreply.github.com>
2025-10-30 19:47:41 +0800

    feat: add version 1.17.80

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -3186,9 +3186,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.17.77 fix: fix get_dce_daily interface
 1.17.78 fix: fix update python 3.14 support
 1.17.79 fix: fix futures_contract_info_dce interface
+1.17.80 fix: fix futures_contract_detail_em interface
 """
 
-__version__ = "1.17.79"
+__version__ = "1.17.80"
 __author__ = "AKFamily"
 
 import sys
@@ -3209,6 +3210,11 @@ if sys.version_info < (3, 9):
     )
 
 del sys
+
+"""
+openctp-合约信息接口
+"""
+from akshare.option.option_contract_info_ctp import option_contract_info_ctp
 
 """
 中国外汇交易中心暨全国银行间同业拆借中心-基准-外汇市场-外汇掉期曲线-外汇掉漆 C-Swap 定盘曲线
@@ -4684,7 +4690,7 @@ from akshare.stock.stock_zh_kcb_report import stock_zh_kcb_report_em
 """
 期货合约详情
 """
-from akshare.futures.futures_contract_detail import futures_contract_detail
+from akshare.futures.futures_contract_detail import futures_contract_detail, futures_contract_detail_em
 
 """
 胡润排行榜
