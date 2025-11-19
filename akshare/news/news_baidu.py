a8daf86395d6f0bb41f52ffc8dbe1ce3fb1e47ec
wids1356 <liucfreedom@sina.com>
2025-11-19 10:00:14 +0800

    fix: fix news_trade_notify_dividend_baidu

--- akshare/news/news_baidu.py
+++ akshare/news/news_baidu.py
@@ -30,7 +30,13 @@ def news_economic_baidu(date: str = "20241107") -> pd.DataFrame:
         "cate": "economic_data",
         "finClientType": "pc",
     }
-    r = requests.get(url=url, params=params)
+    headers = {
+        'Host': 'finance.pae.baidu.com',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36',
+        'Connection': 'keep-alive',
+        'Accept': '*/*',
+    }
+    r = requests.get(url=url, params=params, headers=headers)
     data_json = r.json()
     big_df = pd.DataFrame()
     for item in data_json["Result"]:
@@ -92,7 +98,13 @@ def news_trade_notify_suspend_baidu(date: str = "20241107") -> pd.DataFrame:
         "cate": "notify_suspend",
         "finClientType": "pc",
     }
-    r = requests.get(url=url, params=params)
+    headers = {
+        'Host': 'finance.pae.baidu.com',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36',
+        'Connection': 'keep-alive',
+        'Accept': '*/*',
+    }
+    r = requests.get(url=url, params=params, headers=headers)
     data_json = r.json()
     big_df = pd.DataFrame()
     for item in data_json["Result"]:
@@ -151,7 +163,13 @@ def news_trade_notify_dividend_baidu(date: str = "20241107") -> pd.DataFrame:
         "pn": 0,
         "finClientType": "pc",
     }
-    r = requests.get(url=url, params=params)
+    headers = {
+        'Host': 'finance.pae.baidu.com',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36',
+        'Connection': 'keep-alive',
+        'Accept': '*/*',
+    }
+    r = requests.get(url=url, params=params, headers=headers)
     data_json = r.json()
     big_df = pd.DataFrame()
     for item in data_json["Result"]["calendarInfo"]:
@@ -222,7 +240,13 @@ def news_report_time_baidu(date: str = "20241107") -> pd.DataFrame:
         "cate": "report_time",
         "finClientType": "pc",
     }
-    r = requests.get(url=url, params=params)
+    headers = {
+        'Host': 'finance.pae.baidu.com',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36',
+        'Connection': 'keep-alive',
+        'Accept': '*/*',
+    }
+    r = requests.get(url=url, params=params, headers=headers)
     data_json = r.json()
     big_df = pd.DataFrame()
     for item in data_json["Result"]:
