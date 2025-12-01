cb86232e481a211a3e49567a4fbf8b1830ab54d3
smartmakeswatch <206515907+smartmakeswatch@users.noreply.github.com>
2025-12-01 22:58:15 +0800

    fix: fix stock_news_em

--- akshare/news/news_stock.py
+++ akshare/news/news_stock.py
@@ -22,20 +22,53 @@ def stock_news_em(symbol: str = "603777") -> pd.DataFrame:
     :rtype: pandas.DataFrame
     """
     url = "https://search-api-web.eastmoney.com/search/jsonp"
-    params = {
-        "cb": "jQuery3510875346244069884_1668256937995",
-        "param": '{"uid":"",'
-                 + f'"keyword":"{symbol}"'
-                 + ',"type":["cmsArticle"],"client":"web","clientType":"web","clientVersion":"curr",'
-                 + '"param":{"cmsArticle":{"searchScope":"default","sort":"default","pageIndex":1,'
-                 + '"pageSize":100,"preTag":"<em>","postTag":"</em>"}}}',
+    inner_param = {
+        "uid": "",
+        "keyword": "603777",
+        "type": ["cmsArticleWebOld"],
+        "client": "web",
+        "clientType": "web",
+        "clientVersion": "curr",
+        "param": {
+            "cmsArticleWebOld": {
+                "searchScope": "default",
+                "sort": "default",
+                "pageIndex": 1,
+                "pageSize": 10,
+                "preTag": "<em>",
+                "postTag": "</em>"
+            }
+        }
     }
-    r = requests.get(url, params=params)
+    params = {
+        "cb": "jQuery35101792940631092459_1764599530165",
+        "param": json.dumps(inner_param, ensure_ascii=False),  # 保留中文,
+        "_": "1764599530176"
+    }
+    headers = {
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "en,zh-CN;q=0.9,zh;q=0.8",
+        "cache-control": "no-cache",
+        "connection": "keep-alive",
+        "cookie": "qgqp_b_id=652bf4c98a74e210088f372a17d4e27b; st_nvi=ulN5JAj9FUocz3p4klMME9f20; emshistory=%5B%22603777%22%5D; nid18=010d039dd427dc4d187090491f47d7ad; nid18_create_time=1764582801999; gviem=gSdeY51VWSuTzM3kWaagtf560; gviem_create_time=1764582801999; st_si=55269775884615; st_pvi=66803244437563; st_sp=2025-11-19%2014%3A19%3A16; st_inirUrl=https%3A%2F%2Fso.eastmoney.com%2Fnews%2Fs; st_sn=2; st_psi=20251201223210488-118000300905-0940816858; st_asi=delete",
+        "host": "search-api-web.eastmoney.com",
+        "pragma": "no-cache",
+        "referer": "https://so.eastmoney.com/news/s?keyword=603777",
+        "sec-ch-ua": "\"Chromium\";v=\"142\", \"Google Chrome\";v=\"142\", \"Not_A Brand\";v=\"99\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "script",
+        "sec-fetch-mode": "no-cors",
+        "sec-fetch-site": "same-site",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"
+    }
+    r = requests.get(url, params=params, headers=headers)
     data_text = r.text
     data_json = json.loads(
-        data_text.strip("jQuery3510875346244069884_1668256937995(")[:-1]
+        data_text.strip("jQuery35101792940631092459_1764599530165(")[:-1]
     )
-    temp_df = pd.DataFrame(data_json["result"]["cmsArticle"])
+    temp_df = pd.DataFrame(data_json["result"]["cmsArticleWebOld"])
     temp_df["url"] = "http://finance.eastmoney.com/a/" + temp_df["code"] + ".html"
     temp_df.rename(
         columns={
