e7b1bb6d65940cbfe04a6ad01ecc76f9476e8afa
ninja <work_ninja@163.com>
2025-12-05 16:19:07 +0800

    feat: temp

---
+++ data_2_local/index_stocks_2_local.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+from data_2_local.common_data_2_local import df_append_2_local
+
+
+def data_2_local():
+    file_path = f'data/index_stocks/tdx_399101.txt'
+    # 定义列名。turnover: 成交额
+    column_names = ['code']
+    dtype_dict = {
+        'code': 'str',
+    }
+    df = pd.read_csv(file_path,
+                     # sep=' ',
+                     skiprows=1,
+                     # comment='#',  # 跳过以#开头的行
+                     skip_blank_lines=True,  # 跳过空行
+                     names=column_names,
+                     dtype=dtype_dict,
+                     encoding='utf-8')
+    df_append_2_local(table_name='temp', df=df)
+
+
+if __name__ == '__main__':
+    data_2_local()
