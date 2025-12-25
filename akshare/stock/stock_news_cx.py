e538bac00e44bbb2389a0dd55fba15581b4929e7
smartmakeswatch <206515907+smartmakeswatch@users.noreply.github.com>
2025-12-25 17:39:50 +0800

    fix: fix stock_news_main_cx

--- akshare/stock/stock_news_cx.py
+++ akshare/stock/stock_news_cx.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2024/11/18 16:00
+Date: 2025/12/25 17:00
 Desc: 财新网-财新数据通
 https://cxdata.caixin.com/pc/
 """
 
 import pandas as pd
-
-from akshare.request import make_request_with_retry_json
+import requests
 
 
 def stock_news_main_cx() -> pd.DataFrame:
@@ -18,19 +17,22 @@ def stock_news_main_cx() -> pd.DataFrame:
     :return: 特定时间表示的字典
     :rtype: pandas.DataFrame
     """
-    url = "https://cxdata.caixin.com/api/dataplus/sjtPc/jxNews"
+    url = "https://cxdata.caixin.com/api/dataplus/sjtPc/news"
     params = {
         "pageNum": "1",
-        "pageSize": "20000",
+        "pageSize": "100",
         "showLabels": "true",
     }
-    data_json = make_request_with_retry_json(url, params=params)
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
+        "referer": "https://cxdata.caixin.com/index/newsTab?tab=latest"
+    }
+    r = requests.get(url, params=params, headers=headers)
+    data_json = r.json()
     temp_df = pd.DataFrame(data_json["data"]["data"])
-    temp_df = temp_df[["tag", "summary", "intervalTime", "pubTime", "url"]]
-    temp_df.columns = ["tag", "summary", "interval_time", "pub_time", "url"]
-    temp_df["pub_time"] = pd.to_datetime(
-        temp_df["pub_time"], errors="coerce", unit="ms"
-    ).astype(str)
+    temp_df = temp_df[["tag", "summary", "url"]]
+    temp_df.columns = ["tag", "summary", "url"]
+    temp_df.dropna(inplace=True)
     return temp_df
 
 
