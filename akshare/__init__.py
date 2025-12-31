62e2f82a526f639ffb3cf868aebe5005705a21d8
smartmakeswatch <206515907+smartmakeswatch@users.noreply.github.com>
2025-12-31 23:09:27 +0800

    feat: add version 1.18.5

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -3210,9 +3210,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.18.2 fix: fix stock_ipo_declare_em interface
 1.18.3 fix: fix stock_ipo_tutor_em interface
 1.18.4 fix: fix stock_zh_a_daily interface
+1.18.5 fix: fix futures_inventory_99 interface
 """
 
-__version__ = "1.18.4"
+__version__ = "1.18.5"
 __author__ = "AKFamily"
 
 import sys
