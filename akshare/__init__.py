b637074fd64d1b2e5cb8b31ac9e86c53b6abdc01
smartmakeswatch <206515907+smartmakeswatch@users.noreply.github.com>
2026-01-05 16:53:46 +0800

    feat: add version 1.18.7

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -3212,7 +3212,7 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.18.4 fix: fix stock_zh_a_daily interface
 1.18.5 fix: fix futures_inventory_99 interface
 1.18.6 fix: fix fund_fee_em interface
-1.18.7 fix: fix stock_us_valuation_baidu interface import error
+1.18.7 fix: fix stock_us_valuation_baidu interface
 """
 
 __version__ = "1.18.7"
