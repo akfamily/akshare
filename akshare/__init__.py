779c0068ad03d48e4239417533e221f85f769275
Albert <18234520+albertandking@users.noreply.github.com>
2026-01-14 16:05:50 +0800

    Dev (#6957)
    * fix: fix _baidu_finance_calendar (#6955)
    
    * fix: fix _baidu_finance_calendar
    
    解决访问百度接口时的403问题
    
    * fix: fix news_trade_notify_dividend_baidu
    
    解决接口报错：['分红'] not in index
    
    * docs: update date
    
    * feat: add version 1.18.12
    
    ---------
    
    Co-authored-by: wids1356 <liucfreedom@sina.com>
    Co-authored-by: smartmakeswatch <206515907+smartmakeswatch@users.noreply.github.com>

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -3217,9 +3217,10 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.18.9 fix: fix fx_quote_baidu interface
 1.18.10 fix: fix futures_comm_info interface
 1.18.11 fix: fix stock_individual_spot_xq interface
+1.18.12 fix: fix news_economic_baidu interface
 """
 
-__version__ = "1.18.11"
+__version__ = "1.18.12"
 __author__ = "AKFamily"
 
 import sys
