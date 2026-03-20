9a94fc9785fa18d2d2701d8034d2f7273e0e047f
smartmakeswatch <206515907+smartmakeswatch@users.noreply.github.com>
2026-03-20 12:40:28 +0800

    fix: fix stock_zh_a_minute

--- akshare/stock/stock_zh_a_sina.py
+++ akshare/stock/stock_zh_a_sina.py
@@ -181,7 +181,7 @@ def stock_zh_a_daily(
         "d", r.text.split("=")[1].split(";")[0].replace('"', "")
     )  # 执行js解密代码
     data_df = pd.DataFrame(dict_list)
-    data_df.index = pd.to_datetime(data_df["date"]).dt.date
+    data_df.index = pd.to_datetime(data_df["date"], errors="coerce").dt.date
     del data_df["date"]
     try:
         del data_df["prevclose"]
@@ -202,15 +202,7 @@ def stock_zh_a_daily(
     temp_df = pd.merge(
         data_df, amount_data_df, left_index=True, right_index=True, how="outer"
     )
-    try:
-        # try for pandas >= 2.1.0
-        temp_df.ffill(inplace=True)
-    except Exception:
-        try:
-            temp_df.fillna(method="ffill", inplace=True)
-        except Exception as e:
-            print("Error:", e)
-
+    temp_df.ffill(inplace=True)
     temp_df = temp_df.astype(float)
     temp_df["outstanding_share"] = temp_df["outstanding_share"] * 10000
     temp_df["turnover"] = temp_df["volume"] / temp_df["outstanding_share"]
@@ -253,15 +245,7 @@ def stock_zh_a_daily(
             right_index=True,
             how="outer",
         )
-        try:
-            # try for pandas >= 2.1.0
-            temp_df.ffill(inplace=True)
-        except Exception:
-            try:
-                # try for pandas < 2.1.0
-                temp_df.fillna(method="ffill", inplace=True)
-            except Exception as e:
-                print("Error:", e)
+        temp_df.ffill(inplace=True)
         temp_df = temp_df.astype(float)
         temp_df.dropna(inplace=True)
         temp_df.drop_duplicates(
@@ -297,15 +281,7 @@ def stock_zh_a_daily(
             right_index=True,
             how="outer",
         )
-        try:
-            # try for pandas >= 2.1.0
-            temp_df.ffill(inplace=True)
-        except Exception:
-            try:
-                # try for pandas < 2.1.0
-                temp_df.fillna(method="ffill", inplace=True)
-            except Exception as e:
-                print("Error:", e)
+        temp_df.ffill(inplace=True)
         temp_df = temp_df.astype(float)
         temp_df.dropna(inplace=True)
         temp_df.drop_duplicates(
