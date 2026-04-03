5faf2acc7bc50f49084a9c5837d9894f93a28823
Albert <18234520+albertandking@users.noreply.github.com>
2026-04-03 17:11:51 +0800

    Dev (#7187)
    * fix: Legu dividend yield field error (#7186)
    
    * fix: fix stock_a_gxl_lg
    
    * docs: update date
    
    * feat: add version 1.18.51
    
    ---------
    
    Co-authored-by: LiuShuPeng <desert_fox@foxmail.com>
    Co-authored-by: smartmakeswatch <206515907+smartmakeswatch@users.noreply.github.com>

--- akshare/stock_feature/stock_gxl_lg.py
+++ akshare/stock_feature/stock_gxl_lg.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2023/4/10 16:05
+Date: 2026/4/3 16:05
 Desc: 乐咕乐股-股息率-A 股股息率
 https://legulegu.com/stockdata/guxilv
 """
@@ -38,8 +38,10 @@ def stock_a_gxl_lg(symbol: str = "上证A股") -> pd.DataFrame:
     data_json = r.json()
     temp_df = pd.DataFrame(data_json[symbol_map[symbol]])
     temp_df["date"] = (
-        pd.to_datetime(temp_df["date"], unit="ms") + pd.Timedelta(hours=8)
-    ).dt.date
+        pd.to_datetime(temp_df["date"], utc=True)
+        .dt.tz_convert("Asia/Shanghai")
+        .dt.date
+    )
     temp_df.rename(columns={"addDvTtm": "股息率", "date": "日期"}, inplace=True)
     temp_df = temp_df[
         [
@@ -69,9 +71,11 @@ def stock_hk_gxl_lg() -> pd.DataFrame:
     data_json = r.json()
     temp_df = pd.DataFrame(data_json)
     temp_df["date"] = (
-        pd.to_datetime(temp_df["date"], unit="ms") + pd.Timedelta(hours=8)
-    ).dt.date
-    temp_df.rename(columns={"addDvRatio": "股息率", "date": "日期"}, inplace=True)
+        pd.to_datetime(temp_df["date"], utc=True)
+        .dt.tz_convert("Asia/Shanghai")
+        .dt.date
+    )
+    temp_df.rename(columns={"dvRatio": "股息率", "date": "日期"}, inplace=True)
     temp_df = temp_df[
         [
             "日期",
--- docs/changelog.md
+++ docs/changelog.md
@@ -89,6 +89,10 @@
 
 ## 更新说明详情
 
+1.18.51 fix: fix stock_a_gxl_lg interface
+
+	1. 修复 stock_a_gxl_lg 接口
+
 1.18.50 fix: fix stock_gpzy_individual_pledge_ratio_detail_em interface
 
 	1. 修复 stock_gpzy_individual_pledge_ratio_detail_em 接口
@@ -6234,6 +6238,8 @@
     3. 修改 air_quality_hist 接口的请求日期格式，从 '2022-01-01' 为 '20220101'
 
 ## 版本更新说明
+
+1.18.51 fix: fix stock_a_gxl_lg interface
 
 1.18.50 fix: fix stock_gpzy_individual_pledge_ratio_detail_em interface
 
