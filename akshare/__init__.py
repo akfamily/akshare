3cf9877a49040b3f1b6487f4ea9d4a1844a9534a
smartmakeswatch <206515907+smartmakeswatch@users.noreply.github.com>
2026-04-08 17:11:57 +0800

    feat: add version 1.18.54

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -3259,6 +3259,7 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.18.51 fix: fix stock_a_gxl_lg interface
 1.18.52 fix: fix fund_scale_daily_szse interface
 1.18.53 fix: fix futures_hist_daily_cffex interface
+1.18.54 fix: fix stock_individual_notice_report interface
 """
 
 from akshare._version import __version__
