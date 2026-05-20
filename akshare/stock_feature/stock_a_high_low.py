4d35a2b0cf6a8e436a0fb7d3ebf5b49ede19b7c4
smartmakeswatch <206515907+smartmakeswatch@users.noreply.github.com>
2026-05-20 17:52:43 +0800

    fix: fix stock_a_high_low_statistics

--- akshare/stock_feature/stock_a_high_low.py
+++ akshare/stock_feature/stock_a_high_low.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Date: 2026/3/24 11:30
+Date: 2026/5/20 17:30
 Desc: 乐咕乐股-创新高、新低的股票数量
 https://www.legulegu.com/stockdata/high-low-statistics
 """
-
-from datetime import date
 
 import pandas as pd
 import requests
@@ -28,7 +26,6 @@ def stock_a_high_low_statistics(symbol: str = "all") -> pd.DataFrame:
     data_json = r.json()
     temp_df = pd.DataFrame(data_json)
     del temp_df["indexCode"]
-    temp_df['date'] = temp_df['date'].apply(lambda x: pd.Timestamp(date(x[0], x[1], x[2])))
     temp_df["date"] = pd.to_datetime(temp_df["date"], errors="coerce").dt.date
     temp_df["close"] = pd.to_numeric(temp_df["close"], errors="coerce")
     temp_df["high20"] = pd.to_numeric(temp_df["high20"], errors="coerce")
